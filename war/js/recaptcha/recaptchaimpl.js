4b604513ab766864b0469ea2eec28ed379656338
matthewhorridge <matthew.horridge@stanford.edu>
2012-06-28 00:28:57 +0000

    Matthew's web protege

---
+++ war/js/recaptcha/recaptchaimpl.js
@@ -0,0 +1,6 @@
+function initRecapture() {
+    Recaptcha.create("6Le8WNISAAAAAB_NG0dp-qoXv7PgfiB1wJJpmaZT", "recaptcha-widget-div", {
+        theme:"clean"
+//        callback:Recaptcha.focus_response_field
+    });
+}
