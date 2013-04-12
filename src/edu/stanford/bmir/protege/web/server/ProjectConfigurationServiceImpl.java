003953f37993bf7db860230f30c40b8717ff6d10
tudorache <tudorache@stanford.edu>
2013-04-12 20:46:13 +0000

    cleaning up imports

--- src/edu/stanford/bmir/protege/web/server/ProjectConfigurationServiceImpl.java
+++ src/edu/stanford/bmir/protege/web/server/ProjectConfigurationServiceImpl.java
@@ -1,23 +1,11 @@
 package edu.stanford.bmir.protege.web.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import com.thoughtworks.xstream.XStream;
-import edu.stanford.bmir.protege.web.client.rpc.ProjectConfigurationService;
-import edu.stanford.bmir.protege.web.client.rpc.data.ProjectType;
-import edu.stanford.bmir.protege.web.client.rpc.data.UserId;
-import edu.stanford.bmir.protege.web.client.rpc.data.layout.PortletConfiguration;
-import edu.stanford.bmir.protege.web.client.rpc.data.layout.ProjectLayoutConfiguration;
-import edu.stanford.bmir.protege.web.client.rpc.data.layout.TabColumnConfiguration;
-import edu.stanford.bmir.protege.web.client.rpc.data.layout.TabConfiguration;
-import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProjectType;
-import edu.stanford.bmir.protege.web.shared.project.ProjectId;
-import edu.stanford.smi.protege.util.Log;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.logging.Level;
+import edu.stanford.bmir.protege.web.client.rpc.ProjectConfigurationService;
+import edu.stanford.bmir.protege.web.client.rpc.data.UserId;
+import edu.stanford.bmir.protege.web.client.rpc.data.layout.ProjectLayoutConfiguration;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 
 public class ProjectConfigurationServiceImpl extends RemoteServiceServlet implements ProjectConfigurationService {
 
