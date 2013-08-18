7e805203e7723c51632558e5da1beb8261d26f6b
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-18 15:05:23 -0700

    Got rid of examples in the WhiteSpaceTreatement enum
    The examples can be generated as needed using the transform method.

--- src/edu/stanford/bmir/protege/web/shared/crud/supplied/WhiteSpaceTreatment.java
+++ src/edu/stanford/bmir/protege/web/shared/crud/supplied/WhiteSpaceTreatment.java
@@ -8,36 +8,29 @@ package edu.stanford.bmir.protege.web.shared.crud.supplied;
  */
 public enum  WhiteSpaceTreatment {
 
-    ESCAPE("Leave intact", "'Ontology engineer' remains as 'Ontology engineer'"),
+    ESCAPE("Leave intact"),
 
-    TRANSFORM_TO_CAMEL_CASE("Collapse and transform to CamelCase", "'Ontology engineer' becomes 'OntologyEngineer'"),
+    TRANSFORM_TO_CAMEL_CASE("Collapse and transform to CamelCase"),
 
-    REPLACE_WITH_UNDERSCORES("Replace with underscores", "'Ontology engineer' becomes 'Ontology_engineer'"),
+    REPLACE_WITH_UNDERSCORES("Replace with underscores"),
 
-    REPLACE_WITH_DASHES("Replace with dashes", "'Ontology engineer' becomes 'Ontology-engineer'");
+    REPLACE_WITH_DASHES("Replace with dashes");
 
     private String displayName;
 
-    private String example;
-
-    private WhiteSpaceTreatment(String displayName, String example) {
+    private WhiteSpaceTreatment(String displayName) {
         this.displayName = displayName;
-        this.example = example;
     }
 
     public String getDisplayName() {
         return displayName;
     }
 
-    public String getExample() {
-        return example;
-    }
-
-    public String transform(String input) {
+    public String transform(final String input) {
         if(input.indexOf(' ') == -1) {
             return input;
         }
-        String trimmedInput = input.trim();
+        final String trimmedInput = input.trim();
         switch (this) {
             case ESCAPE:
                 return trimmedInput.replaceAll("\\s+", "%20");
@@ -50,8 +43,6 @@ public enum  WhiteSpaceTreatment {
         }
         throw new IllegalStateException();
     }
-
-
 
     private static String transformToCamelCase(String input) {
         if(input.indexOf(' ') == -1) {
