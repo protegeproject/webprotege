a2c71db5bd710eb183e087fb2c4248a0c1231930
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-18 15:00:21 -0700

    Added test cases for WhiteSpaceTreatment

--- src/edu/stanford/bmir/protege/web/shared/crud/supplied/WhiteSpaceTreatment.java
+++ src/edu/stanford/bmir/protege/web/shared/crud/supplied/WhiteSpaceTreatment.java
@@ -8,13 +8,13 @@ package edu.stanford.bmir.protege.web.shared.crud.supplied;
  */
 public enum  WhiteSpaceTreatment {
 
-    TRANSFORM_TO_CAMEL_CASE("Transform to camel case (most common)", "'Ontology engineer' becomes 'OntologyEngineer'"),
+    ESCAPE("Leave intact", "'Ontology engineer' remains as 'Ontology engineer'"),
+
+    TRANSFORM_TO_CAMEL_CASE("Collapse and transform to CamelCase", "'Ontology engineer' becomes 'OntologyEngineer'"),
 
     REPLACE_WITH_UNDERSCORES("Replace with underscores", "'Ontology engineer' becomes 'Ontology_engineer'"),
 
-    REPLACE_WITH_DASHES("Replace with dashes", "'Ontology engineer' becomes 'Ontology-engineer'"),
-
-    ESCAPE("Escape (spaces will be shown in the User Interface but escaped with %20 when saved)", "'Ontology engineer' becomes 'Ontology%20engineer'");
+    REPLACE_WITH_DASHES("Replace with dashes", "'Ontology engineer' becomes 'Ontology-engineer'");
 
     private String displayName;
 
@@ -31,5 +31,52 @@ public enum  WhiteSpaceTreatment {
 
     public String getExample() {
         return example;
+    }
+
+    public String transform(String input) {
+        if(input.indexOf(' ') == -1) {
+            return input;
+        }
+        String trimmedInput = input.trim();
+        switch (this) {
+            case ESCAPE:
+                return trimmedInput.replaceAll("\\s+", "%20");
+            case TRANSFORM_TO_CAMEL_CASE:
+                return transformToCamelCase(trimmedInput);
+            case REPLACE_WITH_UNDERSCORES:
+                return trimmedInput.replaceAll("\\s+", "_");
+            case REPLACE_WITH_DASHES:
+                return trimmedInput.replaceAll("\\s+", "-");
+        }
+        throw new IllegalStateException();
+    }
+
+
+
+    private static String transformToCamelCase(String input) {
+        if(input.indexOf(' ') == -1) {
+            return input;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean lastWasWhiteSpace = false;
+        boolean inbetweenWords = false;
+        for(int index = 0; index < input.length(); index++) {
+            char ch = input.charAt(index);
+            if(ch != ' ') {
+                if(lastWasWhiteSpace && inbetweenWords) {
+                    stringBuilder.append(Character.toUpperCase(ch));
+                    lastWasWhiteSpace = false;
+                }
+                else {
+                    stringBuilder.append(ch);
+                }
+                lastWasWhiteSpace = false;
+                inbetweenWords = true;
+            }
+            else {
+                lastWasWhiteSpace = true;
+            }
+        }
+        return stringBuilder.toString();
     }
 }
