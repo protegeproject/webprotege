afd0fad59f19bd52ebb9e1adf6ff4225ed02cc8f
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-20 14:53:00 -0700

    Added some documentation

--- src/edu/stanford/bmir/protege/web/server/app/WebProtegeApplicationContext.java
+++ src/edu/stanford/bmir/protege/web/server/app/WebProtegeApplicationContext.java
@@ -8,8 +8,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Stanford University<br>
  * Bio-Medical Informatics Research Group<br>
  * Date: 8/20/13
+ * <p>
+ *     A reference point for the Spring application context.
+ * </p>
  */
 public class WebProtegeApplicationContext {
+
+    //  I'm not sure whether this is a good idea or not!
 
     private static final ApplicationContext APPLICATION_CONTEXT = new AnnotationConfigApplicationContext(WebProtegeApplicationConfig.class);
 
