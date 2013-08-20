bedd626e6423ca2fd50e97916e8626d62d06c619
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-20 14:13:28 -0700

    Adding converters for value objects

---
+++ src/edu/stanford/bmir/protege/web/server/persistence/EmailAddressWriteConverter.java
@@ -0,0 +1,19 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import edu.stanford.bmir.protege.web.shared.user.EmailAddress;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
+
+/**
+ * Author: Matthew Horridge<br>
+ * Stanford University<br>
+ * Bio-Medical Informatics Research Group<br>
+ * Date: 8/20/13
+ */
+@WritingConverter
+public class EmailAddressWriteConverter implements Converter<EmailAddress, String> {
+
+    public String convert(EmailAddress source) {
+        return source.getEmailAddress();
+    }
+}
