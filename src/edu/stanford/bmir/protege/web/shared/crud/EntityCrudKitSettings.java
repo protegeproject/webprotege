4906ec2333062e0fc94a0303895d0843828201b0
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-21 13:03:10 -0700

    Added documentation, checkNotNull checks, and implementation hashCode() and equals() to EntityCrudKitSettings

--- src/edu/stanford/bmir/protege/web/shared/crud/EntityCrudKitSettings.java
+++ src/edu/stanford/bmir/protege/web/shared/crud/EntityCrudKitSettings.java
@@ -1,8 +1,11 @@
 package edu.stanford.bmir.protege.web.shared.crud;
 
 import com.google.common.base.Objects;
+import org.springframework.data.annotation.TypeAlias;
 
 import java.io.Serializable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Author: Matthew Horridge<br>
@@ -10,6 +13,7 @@ import java.io.Serializable;
  * Bio-Medical Informatics Research Group<br>
  * Date: 14/08/2013
  */
+@TypeAlias("EntityCrudKitSettings")
 public class EntityCrudKitSettings<S extends EntityCrudKitSuffixSettings> implements Serializable {
 
     private EntityCrudKitPrefixSettings prefixSettings;
@@ -19,19 +23,50 @@ public class EntityCrudKitSettings<S extends EntityCrudKitSuffixSettings> implem
     private EntityCrudKitSettings() {
     }
 
+    /**
+     * Constructs an {@link EntityCrudKitSettings} object for the specified {@link EntityCrudKitPrefixSettings} and
+     * {@link EntityCrudKitSuffixSettings}.
+     * @param prefixSettings The {@link EntityCrudKitPrefixSettings}. Not {@code null}.
+     * @param suffixSettings The {@link EntityCrudKitSuffixSettings}. Not {@code null}.
+     * @throws NullPointerException if any argument is {@code null}.
+     */
     public EntityCrudKitSettings(EntityCrudKitPrefixSettings prefixSettings, S suffixSettings) {
-        this.prefixSettings = prefixSettings;
-        this.suffixSettings = suffixSettings;
+        this.prefixSettings = checkNotNull(prefixSettings);
+        this.suffixSettings = checkNotNull(suffixSettings);
     }
 
+    /**
+     * Gets the prefix settings for this settings object.
+     * @return The prefix settings.  Not {@code null}.
+     */
     public EntityCrudKitPrefixSettings getPrefixSettings() {
         return prefixSettings;
     }
 
+    /**
+     * Gets the suffix settings for this object.
+     * @return The suffix settings. Not {@code null}.
+     */
     public S getSuffixSettings() {
         return suffixSettings;
     }
 
+    @Override
+    public int hashCode() {
+        return "EntityCrudKitSettings".hashCode() + prefixSettings.hashCode() + suffixSettings.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) {
+            return true;
+        }
+        if(!(o instanceof EntityCrudKitSettings)) {
+            return false;
+        }
+        EntityCrudKitSettings other = (EntityCrudKitSettings) o;
+        return this.prefixSettings.equals(other.prefixSettings) && this.suffixSettings.equals(other.suffixSettings);
+    }
 
     @Override
     public String toString() {
