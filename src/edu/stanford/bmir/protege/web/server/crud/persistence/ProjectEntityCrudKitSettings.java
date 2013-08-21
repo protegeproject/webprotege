1a3de64368dfa73af565ccb9342d62b7f97086d4
Matthew Horridge <matthew.horridge@stanford.edu>
2013-08-21 13:05:35 -0700

    Added ProjectEntityCrudKitSettings to bind EntityCrudKitSettings with a ProjectId
    Added TypeAlias annotation
    checkNotNull checks for constructor args
    Implementation of hashCode and equals
    Documentation

--- src/edu/stanford/bmir/protege/web/server/crud/persistence/ProjectEntityCrudKitSettings.java
+++ src/edu/stanford/bmir/protege/web/server/crud/persistence/ProjectEntityCrudKitSettings.java
@@ -3,6 +3,9 @@ package edu.stanford.bmir.protege.web.server.crud.persistence;
 import edu.stanford.bmir.protege.web.shared.crud.EntityCrudKitSettings;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Author: Matthew Horridge<br>
@@ -13,6 +16,7 @@ import org.springframework.data.annotation.Id;
  *     Captures the {@link EntityCrudKitSettings} for a project.
  * </p>
  */
+@TypeAlias("ProjectEntityCrudKitSettings")
 public class ProjectEntityCrudKitSettings {
 
     @Id
@@ -20,16 +24,49 @@ public class ProjectEntityCrudKitSettings {
 
     private EntityCrudKitSettings<?> settings;
 
+    /**
+     * Constructs a {@link ProjectEntityCrudKitSettings}, which specifies the {@link EntityCrudKitSettings} for a
+     * specified project.
+     * @param projectId The {@link ProjectId} that identifies the settings that apply to the project.  Not {@code null}.
+     * @param settings The settings that apply to the project.  Not {@code null}.
+     * @throws NullPointerException if any parameters are {@code null}.
+     */
     public ProjectEntityCrudKitSettings(ProjectId projectId, EntityCrudKitSettings<?> settings) {
-        this.projectId = projectId;
-        this.settings = settings;
+        this.projectId = checkNotNull(projectId);
+        this.settings = checkNotNull(settings);
     }
 
+    /**
+     * Gets the {@link ProjectId}, which specifies the project that these settings apply to.
+     * @return The {@link ProjectId}.  Not {@code null}.
+     */
     public ProjectId getProjectId() {
         return projectId;
     }
 
+    /**
+     * Gets the {@link EntityCrudKitSettings} for the project.
+     * @return The settings.  Not {@code null}.
+     */
     public EntityCrudKitSettings<?> getSettings() {
         return settings;
     }
+
+    @Override
+    public int hashCode() {
+        return "ProjectEntityCrudKitSettings".hashCode() + projectId.hashCode() + settings.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) {
+            return true;
+        }
+        if(!(o instanceof ProjectEntityCrudKitSettings)) {
+            return false;
+        }
+        ProjectEntityCrudKitSettings other = (ProjectEntityCrudKitSettings) o;
+        return this.projectId.equals(other.projectId) && this.settings.equals(other.settings);
+    }
+
 }
