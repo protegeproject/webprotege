3fa9e03f41901550aadd52b31bc4978677b6676b
Matthew Horridge <matthew.horridge@stanford.edu>
2013-09-05 12:40:28 +0100

    Tided the documentation on the OWLLiteralParser interface

--- src/edu/stanford/bmir/protege/web/client/primitive/OWLLiteralParser.java
+++ src/edu/stanford/bmir/protege/web/client/primitive/OWLLiteralParser.java
@@ -13,10 +13,11 @@ public interface OWLLiteralParser {
 
     /**
      * Parses the specified text and optional language into a literal.
-     * @param text
-     * @param language
-     * @return
-     * @throws OWLLiteralParseException
+     * @param text The text to be parsed.  Not {@code null}.
+     * @param language The language.  Not {@code null}.
+     * @return The {@link OWLLiteral} that {@code text} was parsed into.
+     * @throws OWLLiteralParseException If the literal text is malformed.
+     * @throws NullPointerException if {@code text} is {@code null} or {@code language} is {@code null}.
      */
     OWLLiteral parseLiteral(String text, Optional<String> language);
 }
