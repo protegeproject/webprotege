d0b06d5d41f01ce2c05859be781be21b1eaed15d
Matthew Horridge <matthew.horridge@stanford.edu>
2013-09-05 12:19:57 +0100

    Tidied the interface and added documentation

--- src/edu/stanford/bmir/protege/web/client/ui/frame/PrimitiveDataEditor.java
+++ src/edu/stanford/bmir/protege/web/client/ui/frame/PrimitiveDataEditor.java
@@ -21,14 +21,190 @@ import java.util.SortedSet;
  * Stanford University<br>
  * Bio-Medical Informatics Research Group<br>
  * Date: 04/12/2012
+ * <p>
+ *     An interface to an editor that can edit primitive values (classes, properties, individuals, datatypes, literals
+ *     and IRIs).  The editor can be controlled to restrict the type of primitives that can be edited.  This interface
+ *     contains various boolean setters and getters to do this so that it may be used with UIBinder.
+ * </p>
  */
 public interface PrimitiveDataEditor extends IsWidget, HasEnabled, ValueEditor<OWLPrimitiveData>, HasFocusHandlers, HasKeyUpHandlers, HasTextRendering, HasPlaceholder {
 
+    /**
+     * Gets the language editor for this primitive data editor
+     * @return The language editor.  Not {@code null}.
+     */
     LanguageEditor getLanguageEditor();
-    
+
+    /**
+     * Specifies whether or not the editor should be a single or multiple line editor.
+     * @param mode The mode of the editor.  Not {@code null}.
+     */
+    void setMode(ExpandingTextBoxMode mode);
+
+    /**
+     * Specifies whether or not a link should be shown when the content of the editor is an entity.
+     * @param showLinkForEntities {@code true} if a link should be shown when the content of the editor is an entity,
+     *                                        otherwise {@code false}.
+     */
+    void setShowLinkForEntities(boolean showLinkForEntities);
+
+    /**
+     * Sets the primitive types that this editor can edit.
+     * @param primitiveTypes The set of primitive types that this editor can edit.  May be empty (although this doesn't
+     *                       make much sense!).  Not {@code null}.
+     */
     void setAllowedTypes(SortedSet<PrimitiveType> primitiveTypes);
 
-    void setShowLinkForEntities(boolean showLinkForEntities);
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLClass}
+     * entities to be edited.
+     * @param classesAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLClass}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setClassesAllowed(boolean classesAllowed);
+
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLAnnotationProperty} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLAnnotationProperty} entities,
+     * otherwise {@code false}.
+     */
+    boolean isAnnotationPropertiesAllowed();
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLAnnotationProperty}
+     * entities to be edited.
+     * @param annotationPropertiesAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLAnnotationProperty}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setAnnotationPropertiesAllowed(boolean annotationPropertiesAllowed);
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLDataProperty} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLDataProperty} entities,
+     * otherwise {@code false}.
+     */
+    boolean isDataPropertiesAllowed();
+
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLDataProperty}
+     * entities to be edited.
+     * @param dataPropertiesAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLDataProperty}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setDataPropertiesAllowed(boolean dataPropertiesAllowed);
+
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLObjectProperty} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLObjectProperty} entities,
+     * otherwise {@code false}.
+     */
+    boolean isObjectPropertiesAllowed();
+
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLObjectProperty}
+     * entities to be edited.
+     * @param objectPropertiesAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLObjectProperty}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setObjectPropertiesAllowed(boolean objectPropertiesAllowed);
+
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLClass} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLClass} entities,
+     * otherwise {@code false}.
+     */
+    boolean isClassesAllowed();
+
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLDatatype} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLDatatype} entities,
+     * otherwise {@code false}.
+     */
+    boolean isDatatypesAllowed();
+
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLDatatype}
+     * entities to be edited.
+     * @param datatypesAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLDatatype}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setDatatypesAllowed(boolean datatypesAllowed);
+
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLNamedIndividual} entities.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLNamedIndividual} entities,
+     * otherwise {@code false}.
+     */
+    boolean isNamedIndividualsAllowed();
+
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLNamedIndividual}
+     * entities to be edited.
+     * @param namedIndividualsAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLNamedIndividual}
+     * entities to be edited, otherwise {@code false}.
+     */
+    void setNamedIndividualsAllowed(boolean namedIndividualsAllowed);
+
+    /**
+     * Determines whether or not this editor can edit {@link org.semanticweb.owlapi.model.OWLLiteral}s.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.OWLLiteral}s,
+     * otherwise {@code false}.
+     */
+    boolean isLiteralAllowed();
+
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.OWLLiteral}s to be edited.
+     * @param literalsAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.OWLLiteral}s
+     *                                   to be edited, otherwise {@code false}.
+     */
+    void setLiteralAllowed(boolean literalsAllowed);
+
+
+    /**
+     * Determines whether or not this editor can edit raw {@link org.semanticweb.owlapi.model.IRI}s.
+     * @return {@code true} if this editor can edit {@link org.semanticweb.owlapi.model.IRI}s,
+     * otherwise {@code false}.
+     */
+    boolean isIRIAllowed();
+
+    /**
+     * Specifies whether or not the editor should allow {@link org.semanticweb.owlapi.model.IRI}s to be edited.
+     * @param irisAllowed {@code true} if the editor should allow {@link org.semanticweb.owlapi.model.IRI}s
+     *                                   to be edited, otherwise {@code false}.
+     */
+    void setIRIAllowed(boolean irisAllowed);
+
+    /**
+     * Specifies that the editor can edit the specified {@link PrimitiveType}.
+     * @param type The type.  Not {@code null}.
+     * @param allowed {@code true} if editing the specified type is allowed, otherwise {@code false}.
+     */
+    void setAllowedType(PrimitiveType type, boolean allowed);
+
+    /**
+     * Coerces the value in the editor to the specified entity type.
+     * @param type The type to coerce to.  Not {@code null}.
+     */
+    void coerceToEntityType(EntityType<?> type);
+
+    /**
+     * Specifies whether or not the editor should offer the possibility of creating new types of entities for unrecognised
+     * names, or whether the editor should only allow entities for recognised names.
+     * @param mode The mode.  Not {@code null}.
+     * @see PrimitiveDataEditorSuggestOracleMode
+     */
+    void setSuggestMode(PrimitiveDataEditorSuggestOracleMode mode);
+
 
     /**
      * Sets the FreshEntitiesHandler for this editor.
@@ -36,53 +212,6 @@ public interface PrimitiveDataEditor extends IsWidget, HasEnabled, ValueEditor<O
      * @throws NullPointerException if {@code handler} is {@code null}.
      */
     void setFreshEntitiesHandler(FreshEntitiesHandler handler);
-
-
-
-
-
-
-
-    void setMode(ExpandingTextBoxMode mode);
-
-    boolean isAnnotationPropertiesAllowed();
-
-    void setAnnotationPropertiesAllowed(boolean annotationPropertiesAllowed);
-
-    boolean isDataPropertiesAllowed();
-
-    void setDataPropertiesAllowed(boolean dataPropertiesAllowed);
-
-    boolean isObjectPropertiesAllowed();
-
-    void setObjectPropertiesAllowed(boolean objectPropertiesAllowed);
-
-    boolean isClassesAllowed();
-
-    void setClassesAllowed(boolean classesAllowed);
-
-    boolean isDatatypesAllowed();
-
-    void setDatatypesAllowed(boolean datatypesAllowed);
-
-    boolean isNamedIndividualsAllowed();
-
-    void setNamedIndividualsAllowed(boolean namedIndividualsAllowed);
-
-    boolean isLiteralAllowed();
-
-    void setLiteralAllowed(boolean literalAllowed);
-
-    boolean isIRIAllowed();
-
-    void setIRIAllowed(boolean iriAllowed);
-
-    void setAllowedType(PrimitiveType type, boolean allowed);
-
-    void coerceToEntityType(EntityType<?> type);
-
-    void setSuggestMode(PrimitiveDataEditorSuggestOracleMode mode);
-
 
 
 }
