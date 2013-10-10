822bb4b79be51f4087fd400e19853f551dfe1641
Matthew Horridge <matthew.horridge@stanford.edu>
2013-10-10 11:24:19 -0700

    Fixes #62
    Moved the OBO parser to the end of the list to fix Issue 62.

--- src/edu/stanford/bmir/protege/web/server/owlapi/manager/WebProtegeOWLManager.java
+++ src/edu/stanford/bmir/protege/web/server/owlapi/manager/WebProtegeOWLManager.java
@@ -1,11 +1,16 @@
 package edu.stanford.bmir.protege.web.server.owlapi.manager;
 
 import edu.stanford.bmir.protege.web.server.obo.WebProtegeOBOFormatParserFactory;
+import org.coode.owlapi.obo.parser.OBOParserFactory;
 import org.semanticweb.binaryowl.owlapi.BinaryOWLOntologyDocumentParserFactory;
 import org.semanticweb.binaryowl.owlapi.BinaryOWLOntologyDocumentStorer;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.OWLParserFactory;
 import org.semanticweb.owlapi.io.OWLParserFactoryRegistry;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: Matthew Horridge<br>
@@ -18,8 +23,20 @@ public class WebProtegeOWLManager {
     static {
         // Some HORRIBLE HORRIBLE staticness requires this
         OWLManager.getOWLDataFactory();
-        OWLParserFactoryRegistry.getInstance().registerParserFactory(new WebProtegeOBOFormatParserFactory());
-        OWLParserFactoryRegistry.getInstance().registerParserFactory(new BinaryOWLOntologyDocumentParserFactory());
+
+        // Set up parser factories.  We want to replace the OBO one with the reference OBO parser.  This OBO parser
+        // must be chosen last (i.e. be added first here) - see https://github.com/protegeproject/webprotege/issues/62
+        OWLParserFactoryRegistry parserFactoryRegistry = OWLParserFactoryRegistry.getInstance();
+        List<OWLParserFactory> parserFactoryList = new ArrayList<OWLParserFactory>(parserFactoryRegistry.getParserFactories());
+        parserFactoryRegistry.clearParserFactories();
+        parserFactoryRegistry.registerParserFactory(new WebProtegeOBOFormatParserFactory());
+        for(OWLParserFactory parserFactory : parserFactoryList) {
+            if (!(parserFactory instanceof OBOParserFactory)) {
+                // Replace the OBO one with the one above
+                parserFactoryRegistry.registerParserFactory(parserFactory);
+            }
+        }
+        parserFactoryRegistry.registerParserFactory(new BinaryOWLOntologyDocumentParserFactory());
     }
 
     public static OWLOntologyManager createOWLOntologyManager() {
