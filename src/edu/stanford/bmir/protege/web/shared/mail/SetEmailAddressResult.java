0a28b00d9e2e3e547f69d949733bb63f659a4163
Matthew Horridge <matthew.horridge@stanford.edu>
2013-11-06 14:24:01 -0800

    Adding skeleton classes for setting an email address for a user

---
+++ src/edu/stanford/bmir/protege/web/shared/mail/SetEmailAddressResult.java
@@ -0,0 +1,13 @@
+package edu.stanford.bmir.protege.web.shared.mail;
+
+import edu.stanford.bmir.protege.web.shared.dispatch.Result;
+
+/**
+ * Author: Matthew Horridge<br>
+ * Stanford University<br>
+ * Bio-Medical Informatics Research Group<br>
+ * Date: 06/11/2013
+ */
+public class SetEmailAddressResult implements Result {
+
+}
