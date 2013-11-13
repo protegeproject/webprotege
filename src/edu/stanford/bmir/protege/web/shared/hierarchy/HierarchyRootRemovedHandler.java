66064c590ac19e134dfde243cc8d950dcb5fa0c8
Matthew Horridge <matthew.horridge@stanford.edu>
2013-11-13 13:54:48 -0800

    Changes to generics on hierarchy events

--- src/edu/stanford/bmir/protege/web/shared/hierarchy/HierarchyRootRemovedHandler.java
+++ src/edu/stanford/bmir/protege/web/shared/hierarchy/HierarchyRootRemovedHandler.java
@@ -2,13 +2,15 @@ package edu.stanford.bmir.protege.web.shared.hierarchy;
 
 import com.google.gwt.event.shared.EventHandler;
 
+import java.io.Serializable;
+
 /**
  * Author: Matthew Horridge<br>
  * Stanford University<br>
  * Bio-Medical Informatics Research Group<br>
  * Date: 25/03/2013
  */
-public interface HierarchyRootRemovedHandler<T> extends EventHandler {
+public interface HierarchyRootRemovedHandler<T extends Serializable> extends EventHandler {
 
     void handleHierarchyRootRemoved(HierarchyRootRemovedEvent<T> event);
 }
