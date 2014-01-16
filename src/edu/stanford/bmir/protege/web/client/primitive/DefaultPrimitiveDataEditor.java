e01a56a514b33f5b5028c20363138d69956d892e
Matthew Horridge <matthew.horridge@stanford.edu>
2014-01-16 15:16:27 -0800

    Rearranged code

--- src/edu/stanford/bmir/protege/web/client/primitive/DefaultPrimitiveDataEditor.java
+++ src/edu/stanford/bmir/protege/web/client/primitive/DefaultPrimitiveDataEditor.java
@@ -124,11 +124,6 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
         entitySuggestOracle.setMode(checkNotNull(mode));
     }
 
-    private void handleEdit() {
-        reparsePrimitiveData();
-        dirty = true;
-    }
-
     @Override
     public void setFreshEntitiesHandler(FreshEntitiesHandler handler) {
         checkNotNull(handler);
@@ -149,6 +144,251 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
     @Override
     public void setShowLinksForEntities(boolean showLinksForEntities) {
         setEntityLinkMode(showLinksForEntities ? EntityLinkMode.SHOW_LINKS_FOR_ENTITIES : EntityLinkMode.DO_NOT_SHOW_LINKS_FOR_ENTITIES);
+    }
+
+    /**
+     * Returns true if the widget is enabled, false if not.
+     */
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Sets whether this widget is enabled.
+     *
+     * @param enabled <code>true</code> to enable the widget, <code>false</code>
+     *                to disable it
+     */
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        editor.setEnabled(enabled);
+        languageEditor.setEnabled(enabled);
+    }
+
+    @Override
+    public LanguageEditor getLanguageEditor() {
+        return languageEditor;
+    }
+
+    @Override
+    public String getPlaceholder() {
+        return editor.getPlaceholder();
+    }
+
+    @Override
+    public void setPlaceholder(String placeholder) {
+        editor.setPlaceholder(placeholder);
+    }
+
+    /**
+     * Gets this object's text.
+     *
+     * @return the object's text
+     */
+//    @Override
+    public String getText() {
+        return getTrimmedText();
+    }
+
+    /**
+     * Adds a {@link com.google.gwt.event.dom.client.FocusEvent} handler.
+     *
+     * @param handler the focus handler
+     * @return {@link com.google.gwt.event.shared.HandlerRegistration} used to remove this handler
+     */
+    @Override
+    public HandlerRegistration addFocusHandler(FocusHandler handler) {
+        return editor.addFocusHandler(handler);
+    }
+
+    /**
+     * Adds a {@link com.google.gwt.event.dom.client.KeyUpEvent} handler.
+     *
+     * @param handler the key up handler
+     * @return {@link com.google.gwt.event.shared.HandlerRegistration} used to remove this handler
+     */
+    @Override
+    public HandlerRegistration addKeyUpHandler(KeyUpHandler handler) {
+        return editor.addKeyUpHandler(handler);
+    }
+
+    @Override
+    public boolean isAnnotationPropertiesAllowed() {
+        return allowedTypes.contains(PrimitiveType.ANNOTATION_PROPERTY);
+    }
+
+    @Override
+    public void setAnnotationPropertiesAllowed(boolean annotationPropertiesAllowed) {
+        setAllowedType(PrimitiveType.ANNOTATION_PROPERTY, annotationPropertiesAllowed);
+    }
+
+    @Override
+    public boolean isDataPropertiesAllowed() {
+        return allowedTypes.contains(PrimitiveType.DATA_PROPERTY);
+    }
+
+    @Override
+    public void setDataPropertiesAllowed(boolean dataPropertiesAllowed) {
+        setAllowedType(PrimitiveType.DATA_PROPERTY, dataPropertiesAllowed);
+    }
+
+    @Override
+    public boolean isObjectPropertiesAllowed() {
+        return allowedTypes.contains(PrimitiveType.OBJECT_PROPERTY);
+    }
+
+    @Override
+    public void setObjectPropertiesAllowed(boolean objectPropertiesAllowed) {
+        setAllowedType(PrimitiveType.OBJECT_PROPERTY, objectPropertiesAllowed);
+    }
+
+    @Override
+    public boolean isClassesAllowed() {
+        return allowedTypes.contains(PrimitiveType.CLASS);
+    }
+
+    @Override
+    public void setClassesAllowed(boolean classesAllowed) {
+        setAllowedType(PrimitiveType.CLASS, classesAllowed);
+    }
+
+    @Override
+    public boolean isDatatypesAllowed() {
+        return allowedTypes.contains(PrimitiveType.DATA_TYPE);
+    }
+
+    @Override
+    public void setDatatypesAllowed(boolean datatypesAllowed) {
+        setAllowedType(PrimitiveType.DATA_TYPE, datatypesAllowed);
+    }
+
+    @Override
+    public boolean isNamedIndividualsAllowed() {
+        return allowedTypes.contains(PrimitiveType.NAMED_INDIVIDUAL);
+    }
+
+    @Override
+    public void setNamedIndividualsAllowed(boolean namedIndividualsAllowed) {
+        setAllowedType(PrimitiveType.NAMED_INDIVIDUAL, namedIndividualsAllowed);
+    }
+
+    @Override
+    public boolean isLiteralAllowed() {
+        return allowedTypes.contains(PrimitiveType.LITERAL);
+    }
+
+    @Override
+    public void setLiteralAllowed(boolean literalAllowed) {
+        setAllowedType(PrimitiveType.LITERAL, literalAllowed);
+    }
+
+    @Override
+    public boolean isIRIAllowed() {
+        return allowedTypes.contains(PrimitiveType.IRI);
+    }
+
+    @Override
+    public void setIRIAllowed(boolean iriAllowed) {
+        setAllowedType(PrimitiveType.IRI, iriAllowed);
+    }
+
+    @Override
+    public void setAllowedType(PrimitiveType type, boolean allowed) {
+        boolean revalidate;
+        if (allowed) {
+            revalidate = allowedTypes.add(type);
+        } else {
+            revalidate = allowedTypes.remove(type);
+        }
+        if (revalidate) {
+            if (type.getEntityType() != null) {
+                updateOracle();
+            }
+            validateCurrentEntityTypeAgainstAllowedTypes();
+        }
+    }
+
+    @Override
+    public void coerceToEntityType(EntityType<?> entityType) {
+        String text = getTrimmedText();
+        OWLEntity entity = freshEntitiesHandler.getFreshEntity(text, entityType);
+        OWLPrimitiveData coercedData = DataFactory.getOWLEntityData(entity, text);
+        setCurrentData(Optional.of(coercedData), EventStrategy.FIRE_EVENTS);
+        updateDisplayForCurrentData();
+    }
+
+    /**
+     * Adds a {@link com.google.gwt.event.logical.shared.ValueChangeEvent} handler.
+     *
+     * @param handler the handler
+     * @return the registration for the event
+     */
+    @Override
+    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Optional<OWLPrimitiveData>> handler) {
+        return addHandler(handler, ValueChangeEvent.getType());
+    }
+
+    @Override
+    public Optional<OWLPrimitiveData> getValue() {
+        return currentData;
+    }
+
+    @Override
+    public void setValue(OWLPrimitiveData object) {
+        checkNotNull(object);
+        setCurrentData(Optional.of(object), EventStrategy.DO_NOT_FIRE_EVENTS);
+    }
+
+    @Override
+    public void clearValue() {
+        editor.setValue("");
+        languageEditor.setValue("");
+        setCurrentData(Optional.<OWLPrimitiveData>absent(), EventStrategy.DO_NOT_FIRE_EVENTS);
+    }
+
+    @Override
+    public boolean isWellFormed() {
+        return currentData.isPresent();
+    }
+
+    /**
+     * Determines if this object is dirty.
+     *
+     * @return {@code true} if the object is dirty, otherwise {@code false}.
+     */
+    @Override
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    @Override
+    public void setAllowedTypes(SortedSet<PrimitiveType> primitiveTypes) {
+        if (primitiveTypes.equals(this.allowedTypes)) {
+            return;
+        }
+        this.allowedTypes.clear();
+        this.allowedTypes.addAll(primitiveTypes);
+        if (!allowedTypes.contains(PrimitiveType.LITERAL)) {
+            setMode(ExpandingTextBoxMode.SINGLE_LINE);
+        } else {
+            setMode(ExpandingTextBoxMode.MULTI_LINE);
+        }
+        updateOracle();
+        if (!isCurrentEntityTypeAllowed()) {
+            reparsePrimitiveData();
+        }
+    }
+
+    @Override
+    public HandlerRegistration addDirtyChangedHandler(DirtyChangedHandler handler) {
+        return addHandler(handler, DirtyChangedEvent.TYPE);
+    }
+
+    private void handleEdit() {
+        reparsePrimitiveData();
+        dirty = true;
     }
 
     private void handleAnchorClick(ClickEvent event) {
@@ -205,32 +445,6 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
                 return null;
             }
         });
-    }
-
-    /**
-     * Returns true if the widget is enabled, false if not.
-     */
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    /**
-     * Sets whether this widget is enabled.
-     *
-     * @param enabled <code>true</code> to enable the widget, <code>false</code>
-     *                to disable it
-     */
-    @Override
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-        editor.setEnabled(enabled);
-        languageEditor.setEnabled(enabled);
-    }
-
-    @Override
-    public LanguageEditor getLanguageEditor() {
-        return languageEditor;
     }
 
     private void showErrorLabel() {
@@ -294,16 +508,6 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
         return true;
     }
 
-    @Override
-    public void setPlaceholder(String placeholder) {
-        editor.setPlaceholder(placeholder);
-    }
-
-    @Override
-    public String getPlaceholder() {
-        return editor.getPlaceholder();
-    }
-
     private void updateDisplayForCurrentData() {
         setIconInsetStyleNameForEntityData();
         validateCurrentEntityTypeAgainstAllowedTypes();
@@ -345,6 +549,7 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
             lastIconInsetStyleName = null;
         }
     }
+    ////////////////////////////////////////////////////////////////////////////////////////
 
     private void setIconInsetStyleNameForEntityData() {
         if (!currentData.isPresent()) {
@@ -483,134 +688,6 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
         reparsePrimitiveData();
     }
 
-    /**
-     * Gets this object's text.
-     *
-     * @return the object's text
-     */
-//    @Override
-    public String getText() {
-        return getTrimmedText();
-    }
-
-    /**
-     * Adds a {@link com.google.gwt.event.dom.client.FocusEvent} handler.
-     *
-     * @param handler the focus handler
-     * @return {@link com.google.gwt.event.shared.HandlerRegistration} used to remove this handler
-     */
-    @Override
-    public HandlerRegistration addFocusHandler(FocusHandler handler) {
-        return editor.addFocusHandler(handler);
-    }
-
-    /**
-     * Adds a {@link com.google.gwt.event.dom.client.KeyUpEvent} handler.
-     *
-     * @param handler the key up handler
-     * @return {@link com.google.gwt.event.shared.HandlerRegistration} used to remove this handler
-     */
-    @Override
-    public HandlerRegistration addKeyUpHandler(KeyUpHandler handler) {
-        return editor.addKeyUpHandler(handler);
-    }
-
-    @Override
-    public boolean isAnnotationPropertiesAllowed() {
-        return allowedTypes.contains(PrimitiveType.ANNOTATION_PROPERTY);
-    }
-
-    @Override
-    public void setAnnotationPropertiesAllowed(boolean annotationPropertiesAllowed) {
-        setAllowedType(PrimitiveType.ANNOTATION_PROPERTY, annotationPropertiesAllowed);
-    }
-
-    @Override
-    public boolean isDataPropertiesAllowed() {
-        return allowedTypes.contains(PrimitiveType.DATA_PROPERTY);
-    }
-
-    @Override
-    public void setDataPropertiesAllowed(boolean dataPropertiesAllowed) {
-        setAllowedType(PrimitiveType.DATA_PROPERTY, dataPropertiesAllowed);
-    }
-
-    @Override
-    public boolean isObjectPropertiesAllowed() {
-        return allowedTypes.contains(PrimitiveType.OBJECT_PROPERTY);
-    }
-
-    @Override
-    public void setObjectPropertiesAllowed(boolean objectPropertiesAllowed) {
-        setAllowedType(PrimitiveType.OBJECT_PROPERTY, objectPropertiesAllowed);
-    }
-
-    @Override
-    public boolean isClassesAllowed() {
-        return allowedTypes.contains(PrimitiveType.CLASS);
-    }
-
-    @Override
-    public void setClassesAllowed(boolean classesAllowed) {
-        setAllowedType(PrimitiveType.CLASS, classesAllowed);
-    }
-
-    @Override
-    public boolean isDatatypesAllowed() {
-        return allowedTypes.contains(PrimitiveType.DATA_TYPE);
-    }
-
-    @Override
-    public void setDatatypesAllowed(boolean datatypesAllowed) {
-        setAllowedType(PrimitiveType.DATA_TYPE, datatypesAllowed);
-    }
-
-    @Override
-    public boolean isNamedIndividualsAllowed() {
-        return allowedTypes.contains(PrimitiveType.NAMED_INDIVIDUAL);
-    }
-
-    @Override
-    public void setNamedIndividualsAllowed(boolean namedIndividualsAllowed) {
-        setAllowedType(PrimitiveType.NAMED_INDIVIDUAL, namedIndividualsAllowed);
-    }
-
-    @Override
-    public boolean isLiteralAllowed() {
-        return allowedTypes.contains(PrimitiveType.LITERAL);
-    }
-
-    @Override
-    public void setLiteralAllowed(boolean literalAllowed) {
-        setAllowedType(PrimitiveType.LITERAL, literalAllowed);
-    }
-
-    @Override
-    public boolean isIRIAllowed() {
-        return allowedTypes.contains(PrimitiveType.IRI);
-    }
-
-    @Override
-    public void setIRIAllowed(boolean iriAllowed) {
-        setAllowedType(PrimitiveType.IRI, iriAllowed);
-    }
-
-    @Override
-    public void setAllowedType(PrimitiveType type, boolean allowed) {
-        boolean revalidate;
-        if (allowed) {
-            revalidate = allowedTypes.add(type);
-        } else {
-            revalidate = allowedTypes.remove(type);
-        }
-        if (revalidate) {
-            if (type.getEntityType() != null) {
-                updateOracle();
-            }
-            validateCurrentEntityTypeAgainstAllowedTypes();
-        }
-    }
-
     private void updateOracle() {
         Set<EntityType<?>> types = getMatchTypes();
         entitySuggestOracle.setEntityTypes(types);
@@ -651,62 +728,8 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
         }
     }
 
-    @Override
-    public void coerceToEntityType(EntityType<?> entityType) {
-        String text = getTrimmedText();
-        OWLEntity entity = freshEntitiesHandler.getFreshEntity(text, entityType);
-        OWLPrimitiveData coercedData = DataFactory.getOWLEntityData(entity, text);
-        setCurrentData(Optional.of(coercedData), EventStrategy.FIRE_EVENTS);
-        updateDisplayForCurrentData();
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////
-
-    /**
-     * Adds a {@link com.google.gwt.event.logical.shared.ValueChangeEvent} handler.
-     *
-     * @param handler the handler
-     * @return the registration for the event
-     */
-    @Override
-    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Optional<OWLPrimitiveData>> handler) {
-        return addHandler(handler, ValueChangeEvent.getType());
-    }
-
     private void fireValueChangedEvent() {
         ValueChangeEvent.fire(this, getValue());
-    }
-
-    @Override
-    public void setValue(OWLPrimitiveData object) {
-        checkNotNull(object);
-        setCurrentData(Optional.of(object), EventStrategy.DO_NOT_FIRE_EVENTS);
-    }
-
-    @Override
-    public Optional<OWLPrimitiveData> getValue() {
-        return currentData;
-    }
-
-    @Override
-    public void clearValue() {
-        editor.setValue("");
-        languageEditor.setValue("");
-        setCurrentData(Optional.<OWLPrimitiveData>absent(), EventStrategy.DO_NOT_FIRE_EVENTS);
-    }
-
-    @Override
-    public boolean isWellFormed() {
-        return currentData.isPresent();
-    }
-
-    /**
-     * Determines if this object is dirty.
-     *
-     * @return {@code true} if the object is dirty, otherwise {@code false}.
-     */
-    @Override
-    public boolean isDirty() {
-        return dirty;
     }
 
     private void setCurrentData(Optional<OWLPrimitiveData> nextCurrentData, EventStrategy eventStrategy) {
@@ -728,28 +751,5 @@ public class DefaultPrimitiveDataEditor extends Composite implements PrimitiveDa
         if (eventStrategy == EventStrategy.FIRE_EVENTS) {
             fireValueChangedEvent();
         }
-    }
-
-    @Override
-    public void setAllowedTypes(SortedSet<PrimitiveType> primitiveTypes) {
-        if (primitiveTypes.equals(this.allowedTypes)) {
-            return;
-        }
-        this.allowedTypes.clear();
-        this.allowedTypes.addAll(primitiveTypes);
-        if (!allowedTypes.contains(PrimitiveType.LITERAL)) {
-            setMode(ExpandingTextBoxMode.SINGLE_LINE);
-        } else {
-            setMode(ExpandingTextBoxMode.MULTI_LINE);
-        }
-        updateOracle();
-        if (!isCurrentEntityTypeAllowed()) {
-            reparsePrimitiveData();
-        }
-    }
-
-    @Override
-    public HandlerRegistration addDirtyChangedHandler(DirtyChangedHandler handler) {
-        return addHandler(handler, DirtyChangedEvent.TYPE);
     }
 }
