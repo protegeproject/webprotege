12599f42299e76f9628c20050f9f74dce0a8f0b6
Matthew Horridge <matthew.horridge@stanford.edu>
2014-02-18 19:33:42 -0800

    Added some util classes

---
+++ test/edu/stanford/bmir/protege/web/server/project/ZipInputStreamChecker_TestCase.java
@@ -0,0 +1,46 @@
+package edu.stanford.bmir.protege.web.server.project;
+
+import edu.stanford.bmir.protege.web.server.owlapi.ZipInputStreamChecker;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Matthew Horridge,
+ *         Stanford University,
+ *         Bio-Medical Informatics Research Group
+ *         Date: 18/02/2014
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ZipInputStreamChecker_TestCase {
+
+    @Mock
+    private BufferedInputStream bufferedInputStream;
+
+    @Test
+    public void shouldTestZipInputStream() throws IOException {
+        when(bufferedInputStream.read())
+                .thenReturn((int) 'P')
+                .thenReturn((int) 'K');
+        ZipInputStreamChecker checker = new ZipInputStreamChecker();
+        assertTrue(checker.isZipInputStream(bufferedInputStream));
+    }
+
+    @Test
+    public void shouldTestNonZipInputStream() throws IOException {
+        when(bufferedInputStream.read())
+                .thenReturn((int) 'X')
+                .thenReturn((int) 'Y');
+        ZipInputStreamChecker checker = new ZipInputStreamChecker();
+        assertFalse(checker.isZipInputStream(bufferedInputStream));
+    }
+}
