7e873e447647b88b6126356c1106f6a360391afe
Matthew Horridge <matthew.horridge@stanford.edu>
2014-04-08 14:08:39 -0700

    Initial mavenisation

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/BioPortalAPIServiceImpl.java
@@ -0,0 +1,57 @@
+package edu.stanford.bmir.protege.web.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import edu.stanford.bmir.protege.web.client.rpc.bioportal.*;
+import edu.stanford.bmir.protege.web.shared.revision.RevisionNumber;
+import edu.stanford.bmir.protege.web.server.bioportal.BioPortalConfigurationManager;
+import edu.stanford.bmir.protege.web.server.bioportal.BioPortalMetadataCache;
+import edu.stanford.bmir.protege.web.server.bioportal.BioPortalRestAPI;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Matthew Horridge<br>
+ * Stanford University<br>
+ * Bio-Medical Informatics Research Group<br>
+ * Date: 09/10/2012
+ */
+public class BioPortalAPIServiceImpl extends WebProtegeRemoteServiceServlet implements BioPortalAPIService {
+
+
+
+    private BioPortalRestAPI getAPI() {
+        String restBase = BioPortalConfigurationManager.getManager().getRestBase();
+        return new BioPortalRestAPI(restBase);
+    }
+
+    public BioPortalUserInfo getBioPortalUserInfo(String bioportalAccountName, String bioportalPassword) throws CannotValidateBioPortalCredentials {
+        BioPortalRestAPI api = getAPI();
+        return api.getUserInfo(bioportalAccountName, bioportalPassword);
+    }
+
+    public void uploadProjectToBioPortal(ProjectId projectId, RevisionNumber revisionNumber, PublishToBioPortalInfo publishInfo) throws IOException {
+        BioPortalRestAPI api = getAPI();
+        api.uploadOntologyToBioPortal(projectId, revisionNumber, publishInfo);
+    }
+
+
+
+    public BioPortalOntologyInfo getBioPortalOntologyInfoForDisplayName(String displayName) {
+        return BioPortalMetadataCache.getCache().getOntologyWithDisplayName(displayName);
+    }
+
+    public List<BioPortalOntologyInfo> getOwnedOntologies(BioPortalUserId bioPortalUserId) {
+        List<BioPortalOntologyInfo> result = new ArrayList<BioPortalOntologyInfo>();
+        List<BioPortalOntologyInfo> onts = BioPortalMetadataCache.getCache().getOntologies();
+        for(BioPortalOntologyInfo info : onts) {
+            if(info.getOwners().contains(bioPortalUserId)) {
+                result.add(info);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/edu/stanford/bmir/protege/web/server/project/ZipArchiveProjectSourcesExtractor_TestCase.java
@@ -0,0 +1,89 @@
+package edu.stanford.bmir.protege.web.server.project;
+
+import edu.stanford.bmir.protege.web.server.owlapi.RawProjectSources;
+import edu.stanford.bmir.protege.web.server.owlapi.RootOntologyDocumentFileMatcher;
+import edu.stanford.bmir.protege.web.server.util.TempFileFactory;
+import edu.stanford.bmir.protege.web.server.owlapi.ZipArchiveProjectSourcesExtractor;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static edu.stanford.bmir.protege.web.server.project.FileDocumentSourceMatcher.isFileDocumentSourceForFile;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Matthew Horridge,
+ *         Stanford University,
+ *         Bio-Medical Informatics Research Group
+ *         Date: 19/02/2014
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ZipArchiveProjectSourcesExtractor_TestCase {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Mock
+    private TempFileFactory tempFileFactory;
+
+    @Mock
+    private RootOntologyDocumentFileMatcher rootOntologyDocumentFileMatcher;
+
+    private File outputFolder;
+
+    @Before
+    public void setUp() throws IOException {
+        outputFolder = temporaryFolder.newFolder("ziptest");
+        when(tempFileFactory.createTempDirectory()).thenReturn(outputFolder);
+
+    }
+
+    @Test
+    public void shouldExtractZipFile() throws IOException {
+        String document = "/ontologies/root-ontology.owl";
+        File expectedDocumentFile =  new File(outputFolder, document);
+        when(rootOntologyDocumentFileMatcher.isRootOntologyDocument(expectedDocumentFile)).thenReturn(true);
+        File zipFile = createZipFile(document);
+        ZipArchiveProjectSourcesExtractor extractor = new ZipArchiveProjectSourcesExtractor(tempFileFactory, rootOntologyDocumentFileMatcher);
+        RawProjectSources projectSources = extractor.extractProjectSources(zipFile);
+        Collection<OWLOntologyDocumentSource> documentSources = projectSources.getDocumentSources();
+        assertThat(documentSources, hasSize(1));
+        assertThat(documentSources, hasItem(isFileDocumentSourceForFile(expectedDocumentFile)));
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void shouldThrowFileNotFoundExceptionForMissingRootOntology() throws IOException {
+        String document = "/ontologies/ont.owl";
+        File zipFile = createZipFile(document);
+        ZipArchiveProjectSourcesExtractor extractor = new ZipArchiveProjectSourcesExtractor(tempFileFactory, rootOntologyDocumentFileMatcher);
+        extractor.extractProjectSources(zipFile);
+    }
+
+
+    public File createZipFile(String document) throws IOException {
+        File zipFile = temporaryFolder.newFile("ziptest");
+        OutputStream out = new FileOutputStream(zipFile);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(out);
+        ZipEntry entryA = new ZipEntry(document);
+        entryA.setSize(1);
+        zipOutputStream.putNextEntry(entryA);
+        zipOutputStream.write(1);
+        zipOutputStream.close();
+        return zipFile;
+    }
+
+}
