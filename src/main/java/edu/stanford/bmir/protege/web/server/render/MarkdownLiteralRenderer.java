119438b7e8503fb51c6be60ecb360d73eb938aef
Matthew Horridge <matthew.horridge@stanford.edu>
2014-04-15 08:54:28 -0700

    Removed markdown rendering as the library required Java 7

--- src/main/java/edu/stanford/bmir/protege/web/server/render/MarkdownLiteralRenderer.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/render/MarkdownLiteralRenderer.java
@@ -1,9 +1,5 @@
 package edu.stanford.bmir.protege.web.server.render;
 
-import org.markdown4j.Markdown4jProcessor;
-import org.semanticweb.owlapi.model.OWLLiteral;
-
-import java.io.IOException;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 25/02/2014
@@ -12,12 +8,15 @@ public class MarkdownLiteralRenderer implements LiteralRenderer {
 
     @Override
     public void renderLiteral(String literal, StringBuilder stringBuilder) {
-        try {
-            Markdown4jProcessor markdown4jProcessor = new Markdown4jProcessor();
-            String rendering = markdown4jProcessor.process(literal);
-            stringBuilder.append(rendering.trim());
-        } catch (IOException e) {
-            stringBuilder.append(literal);
-        }
+        // TODO:  Won't work until we move to Java 7
+        stringBuilder.append(literal);
+//        try {
+//            stringBuilder.append(Processor.process(literal));
+//            Markdown4jProcessor markdown4jProcessor = new Markdown4jProcessor();
+//            String rendering = markdown4jProcessor.process(literal);
+//            stringBuilder.append(rendering.trim());
+//        } catch (IOException e) {
+//            stringBuilder.append(literal);
+//        }
     }
 }
