ad840566260b2314bfbc4a2ba3c091c06de9c4b5
Matthew Horridge <matthew.horridge@stanford.edu>
2014-04-20 14:09:20 +0100

    Removed some legacy entity creation stuff

--- src/test/java/edu/stanford/bmir/protege/web/server/OWLDeclarationAxiomMatcher.java
+++ src/test/java/edu/stanford/bmir/protege/web/server/OWLDeclarationAxiomMatcher.java
@@ -1,27 +1,25 @@
 package edu.stanford.bmir.protege.web.server;
 
 import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
 import org.semanticweb.owlapi.model.OWLEntity;
 
-import static org.hamcrest.core.Is.is;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 19/04/2014
  */
 public class OWLDeclarationAxiomMatcher extends TypeSafeMatcher<OWLDeclarationAxiom> {
 
-    private Matcher<OWLEntity> entityMatcher;
+    private OWLEntity entity;
 
-    public OWLDeclarationAxiomMatcher(Matcher<OWLEntity> entityMatcher) {
-        this.entityMatcher = entityMatcher;
+    public OWLDeclarationAxiomMatcher(OWLEntity entity) {
+        this.entity = entity;
     }
 
     @Override
     protected boolean matchesSafely(OWLDeclarationAxiom item) {
-        return entityMatcher.matches(item.getEntity());
+        return entity.equals(item.getEntity());
     }
 
     @Override
@@ -30,6 +28,6 @@ public class OWLDeclarationAxiomMatcher extends TypeSafeMatcher<OWLDeclarationAx
     }
 
     public static OWLDeclarationAxiomMatcher declarationFor(OWLEntity entity) {
-        return new OWLDeclarationAxiomMatcher(is(entity));
+        return new OWLDeclarationAxiomMatcher(entity);
     }
 }
