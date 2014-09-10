01e235f93d078c7706c023e77846f7a81e0a0657
Matthew Horridge <matthew.horridge@stanford.edu>
2014-09-10 09:49:56 +0100

    Fixes #173

--- src/main/java/edu/stanford/bmir/protege/web/server/crud/obo/OBOIdSuffixEntityCrudKitHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/crud/obo/OBOIdSuffixEntityCrudKitHandler.java
@@ -1,5 +1,7 @@
 package edu.stanford.bmir.protege.web.server.crud.obo;
 
+import com.beust.jcommander.internal.Maps;
+import com.google.common.collect.ImmutableMap;
 import edu.stanford.bmir.protege.web.server.change.OntologyChangeList;
 import edu.stanford.bmir.protege.web.server.crud.EntityCrudContext;
 import edu.stanford.bmir.protege.web.server.crud.EntityCrudKitHandler;
@@ -9,10 +11,13 @@ import edu.stanford.bmir.protege.web.shared.crud.EntityCrudKitSettings;
 import edu.stanford.bmir.protege.web.shared.crud.EntityShortForm;
 import edu.stanford.bmir.protege.web.shared.crud.oboid.OBOIdSuffixKit;
 import edu.stanford.bmir.protege.web.shared.crud.oboid.OBOIdSuffixSettings;
+import edu.stanford.bmir.protege.web.shared.crud.oboid.UserIdRange;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
 import org.semanticweb.owlapi.model.*;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.Map;
 
 /**
  * Author: Matthew Horridge<br>
@@ -22,14 +27,27 @@ import java.text.NumberFormat;
  */
 public class OBOIdSuffixEntityCrudKitHandler implements EntityCrudKitHandler<OBOIdSuffixSettings> {
 
+
+    private long currentId = 0;
+
     private EntityCrudKitPrefixSettings prefixSettings;
 
     private OBOIdSuffixSettings suffixSettings;
 
+    private final Map<UserId, Long> userId2CurrentIdMap = Maps.newHashMap();
+
+    private final Map<UserId, UserIdRange> userId2RangeEndMap;
 
     public OBOIdSuffixEntityCrudKitHandler(EntityCrudKitPrefixSettings prefixSettings, OBOIdSuffixSettings suffixSettings) {
         this.prefixSettings = prefixSettings;
         this.suffixSettings = suffixSettings;
+
+        ImmutableMap.Builder<UserId, UserIdRange> builder = ImmutableMap.builder();
+        for(UserIdRange range : suffixSettings.getUserIdRanges()) {
+            userId2CurrentIdMap.put(range.getUserId(), range.getStart());
+            builder.put(range.getUserId(), range);
+        }
+        userId2RangeEndMap = builder.build();
     }
 
     @Override
@@ -57,7 +75,7 @@ public class OBOIdSuffixEntityCrudKitHandler implements EntityCrudKitHandler<OBO
     public <E extends OWLEntity> E create(EntityType<E> entityType, EntityShortForm shortForm, EntityCrudContext context, OntologyChangeList.Builder<E> builder) {
         OWLDataFactory dataFactory = context.getDataFactory();
         final OWLOntology targetOntology = context.getTargetOntology();
-        final IRI iri = getNextIRI(targetOntology);
+        final IRI iri = getNextIRI(targetOntology, context.getUserId());
         final E entity = dataFactory.getOWLEntity(entityType, iri);
         builder.addAxiom(targetOntology, dataFactory.getOWLDeclarationAxiom(entity));
         final OWLLiteral labellingLiteral = getLabellingLiteral(shortForm, context);
@@ -67,21 +85,46 @@ public class OBOIdSuffixEntityCrudKitHandler implements EntityCrudKitHandler<OBO
     }
 
 
-    private int currentId = 0;
 
-    private synchronized IRI getNextIRI(OWLOntology rootOntology) {
+    private synchronized IRI getNextIRI(OWLOntology rootOntology, UserId userId) {
         StringBuilder formatStringBuilder = new StringBuilder();
         for (int i = 0; i < suffixSettings.getTotalDigits(); i++) {
             formatStringBuilder.append("0");
         }
         NumberFormat numberFormat = new DecimalFormat(formatStringBuilder.toString());
+        long currentId = getCurrentId(userId);
         while (true) {
             currentId++;
             String shortName = numberFormat.format(currentId);
             IRI iri = IRI.create(prefixSettings.getIRIPrefix() + shortName);
             if (!rootOntology.containsEntityInSignature(iri, true)) {
+                setCurrentId(userId, currentId);
                 return iri;
             }
+        }
+    }
+
+    private long getCurrentId(UserId userId) {
+        Long currentIdForUser = userId2CurrentIdMap.get(userId);
+        if(currentIdForUser != null) {
+            return currentIdForUser;
+        }
+        else {
+            return currentId;
+        }
+    }
+
+    private void setCurrentId(UserId userId, long currentId) {
+        if (userId2CurrentIdMap.containsKey(userId)) {
+            UserIdRange userIdRange = userId2RangeEndMap.get(userId);
+            long limitForUser = userIdRange.getEnd();
+            if(currentId > limitForUser) {
+                throw new CannotGenerateFreshEntityIdForUserException(userIdRange);
+            }
+            userId2CurrentIdMap.put(userId, currentId);
+        }
+        else {
+            this.currentId = currentId;
         }
     }
 
