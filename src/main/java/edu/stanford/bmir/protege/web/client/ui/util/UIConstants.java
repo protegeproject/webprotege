1146912d880b20526584f0b9bf26e3f63ecdc6f6
Matthew Horridge <matthew.horridge@stanford.edu>
2015-01-09 13:09:54 +0000

    Removed UIConstants
    To be replaced with client bundle

--- src/main/java/edu/stanford/bmir/protege/web/client/ui/util/UIConstants.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.bmir.protege.web.client.ui.util;
-
-public class UIConstants {
-
-    public static final String ICON_INVISIBLE_12PX = "images/invisible12.png";
-
-    public static final String ICON_LOADING_PLACEHOLDER = ICON_INVISIBLE_12PX;
-}
