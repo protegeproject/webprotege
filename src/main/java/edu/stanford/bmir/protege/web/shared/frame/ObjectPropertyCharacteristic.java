d67d034e2221d884894a0fe83c0433983ba95c21
Matthew Horridge <matthew.horridge@stanford.edu>
2015-01-12 12:23:43 +0000

    Added ObjectPropertyCharacteristic enum

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/frame/ObjectPropertyCharacteristic.java
@@ -0,0 +1,63 @@
+package edu.stanford.bmir.protege.web.shared.frame;
+
+import org.semanticweb.owlapi.model.*;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 12/01/15
+ */
+public enum ObjectPropertyCharacteristic {
+
+    FUNCTIONAL("Functional"),
+
+    INVERSE_FUNCTIONAL("Inverse functional"),
+
+    TRANSITIVE("Transitive"),
+
+    SYMMETRIC("Symmetric"),
+
+    ASYMMETRIC("Asymmetric"),
+
+    REFLEXIVE("Reflexive"),
+
+    IRREFLEXIVE("Irreflexive");
+
+    ObjectPropertyCharacteristic(String displayName) {
+        this.displayName = displayName;
+    }
+
+    private String displayName;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public <A extends OWLAxiom> A createAxiom(OWLObjectPropertyExpression property, OWLDataFactory dataFactory) {
+        return createAxiom(property, Collections.<OWLAnnotation>emptySet(), dataFactory);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <A extends OWLAxiom> A createAxiom(OWLObjectPropertyExpression property, Set<? extends OWLAnnotation> annotations, OWLDataFactory dataFactory) {
+        switch(this) {
+            case FUNCTIONAL:
+                return (A) dataFactory.getOWLFunctionalObjectPropertyAxiom(property, annotations);
+            case INVERSE_FUNCTIONAL:
+                return (A) dataFactory.getOWLInverseFunctionalObjectPropertyAxiom(property, annotations);
+            case SYMMETRIC:
+                return (A) dataFactory.getOWLSymmetricObjectPropertyAxiom(property, annotations);
+            case ASYMMETRIC:
+                return (A) dataFactory.getOWLAsymmetricObjectPropertyAxiom(property, annotations);
+            case REFLEXIVE:
+                return (A) dataFactory.getOWLReflexiveObjectPropertyAxiom(property, annotations);
+            case IRREFLEXIVE:
+                return (A) dataFactory.getOWLIrreflexiveObjectPropertyAxiom(property, annotations);
+            case TRANSITIVE:
+                return (A) dataFactory.getOWLTransitiveObjectPropertyAxiom(property, annotations);
+        }
+        throw new IllegalStateException("Missing case for property: " + this);
+    }
+}
