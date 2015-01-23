647c7c01e67006885adbb8137d385e6d1d725bc5
Matthew Horridge <matthew.horridge@stanford.edu>
2015-01-23 15:21:54 -0800

    Deprecated ValueType

--- src/main/java/edu/stanford/bmir/protege/web/client/rpc/data/ValueType.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/rpc/data/ValueType.java
@@ -1,5 +1,6 @@
 package edu.stanford.bmir.protege.web.client.rpc.data;
 
+@Deprecated
 public enum ValueType { //TODO: fix cases - problem with mapping bw value type protege and value type webpro
 	String,
 	Symbol,
