a778fb558678821b57732d80f91040c5f8b3f953
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-04 12:31:46 -0800

    DefaultAxiomComparator and test case

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/axiom/DefaultAxiomComparator.java
@@ -0,0 +1,32 @@
+package edu.stanford.bmir.protege.web.shared.axiom;
+
+import com.google.common.collect.Ordering;
+import org.semanticweb.owlapi.model.OWLAxiom;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 04/02/15
+ */
+public class DefaultAxiomComparator implements Comparator<OWLAxiom> {
+
+    private Comparator<OWLAxiom> compoundComparator;
+
+    public DefaultAxiomComparator(AxiomBySubjectComparator axiomBySubjectComparator, AxiomByTypeComparator axiomByTypeComparator, AxiomByRenderingComparator axiomByRenderingComparator) {
+        compoundComparator = Ordering.compound(
+                Arrays.asList(
+                        checkNotNull(axiomBySubjectComparator),
+                        checkNotNull(axiomByTypeComparator),
+                        checkNotNull(axiomByRenderingComparator)));
+    }
+
+    @Override
+    public int compare(OWLAxiom o1, OWLAxiom o2) {
+        return compoundComparator.compare(o1, o2);
+    }
+}
