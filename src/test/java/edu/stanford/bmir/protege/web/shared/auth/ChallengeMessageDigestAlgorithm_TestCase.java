cb5b026ef280ffdb36a7d4dc87e804a9abf92b9d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 13:29:49 +0000

    Added clean ChallengeMessageDigestAlgorithm
    - Clean isolation of challenge message digest code
    - Tests

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/auth/ChallengeMessageDigestAlgorithm_TestCase.java
@@ -0,0 +1,74 @@
+package edu.stanford.bmir.protege.web.shared.auth;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.inject.Provider;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17/02/15
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ChallengeMessageDigestAlgorithm_TestCase {
+
+    private ChallengeMessageDigestAlgorithm algorithm;
+
+    @Mock
+    private Provider<MessageDigestAlgorithm> digestAlgorithmProvider;
+
+    @Mock
+    private MessageDigestAlgorithm messageDigestAlgorithm;
+
+    private byte [] digestOfSaltedPassword = {4, 4, 4, 4, 4};
+
+    private byte [] challengeMessage = {5, 5, 5, 5, 5};
+
+    private byte [] result = {6, 6, 6, 6, 6};
+
+    @Before
+    public void setUp() throws Exception {
+        when(digestAlgorithmProvider.get()).thenReturn(messageDigestAlgorithm);
+        algorithm = new ChallengeMessageDigestAlgorithm(digestAlgorithmProvider);
+        when(messageDigestAlgorithm.computeDigest()).thenReturn(result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_Provider_IsNull() {
+        new ChallengeMessageDigestAlgorithm(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_Message_IsNull() {
+        algorithm.getDigestOfMessageAndPassword(null, digestOfSaltedPassword);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_Password_IsNull() {
+        algorithm.getDigestOfMessageAndPassword(challengeMessage, null);
+    }
+
+    @Test
+    public void shouldComputeDigestWithChallengeFirstAndPasswordSecond() throws Exception {
+        algorithm.getDigestOfMessageAndPassword(challengeMessage, digestOfSaltedPassword);
+        InOrder inOrder = inOrder(messageDigestAlgorithm);
+        inOrder.verify(messageDigestAlgorithm, times(1)).update(challengeMessage);
+        inOrder.verify(messageDigestAlgorithm, times(1)).update(digestOfSaltedPassword);
+    }
+
+    @Test
+    public void shouldReturnResult() {
+        assertThat(algorithm.getDigestOfMessageAndPassword(challengeMessage, digestOfSaltedPassword), is(result));
+    }
+}
