8b954c683bc380396ca2c1071daa0e7b40cc6c35
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 12:30:33 +0000

    Added clean support for computing message digests
    - Added MessageDigestAlgorithm interface
    - Added MD5 digest implementation.  This uses the Java MD5
    implementation under the hood (which is available for client side code).
    - Added a module dependency for the guava io module (contains encoding
    and decoding utils)
    - Added tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/auth/Md5MessageDigestAlgorithm.java
@@ -0,0 +1,54 @@
+package edu.stanford.bmir.protege.web.shared.auth;
+
+import com.google.common.io.BaseEncoding;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 14/02/15
+ */
+public class Md5MessageDigestAlgorithm implements MessageDigestAlgorithm {
+
+    private static final String UTF_8 = "UTF-8";
+
+    private MessageDigest messageDigest;
+
+    public Md5MessageDigestAlgorithm() {
+        try {
+            this.messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(byte[] bytes) {
+        messageDigest.update(checkNotNull(bytes));
+    }
+
+    @Override
+    public void updateWithBytesFromUtf8String(String utf8String) {
+        try {
+            messageDigest.update(checkNotNull(utf8String).getBytes(UTF_8));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Broken JVM.  UTF-8 Encoding is not available.");
+        }
+    }
+
+    @Override
+    public byte[] computeDigest() {
+        return messageDigest.digest();
+    }
+
+    @Override
+    public String computeDigestAsBase16Encoding() {
+        byte [] digest = messageDigest.digest();
+        return BaseEncoding.base16().encode(digest);
+    }
+}
