3574bc6746021417c3306b86028ac22baeb41591
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 17:55:07 +0000

    Updated guava dependency

--- src/main/java/edu/stanford/bmir/protege/web/shared/auth/AuthenticationResponse.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/auth/AuthenticationResponse.java
@@ -3,7 +3,11 @@ package edu.stanford.bmir.protege.web.shared.auth;
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
- * 17/02/15
+ * 14/02/15
  */
-public class PasswordDigestAlgorithm_ITestCase {
+public enum AuthenticationResponse {
+
+    FAIL,
+
+    SUCCESS
 }
