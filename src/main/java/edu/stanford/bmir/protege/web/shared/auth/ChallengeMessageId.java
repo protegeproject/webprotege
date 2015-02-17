7da7ac08188d0f195ec71040987c805dda275a7d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 11:42:29 +0000

    Fix for partial commit.
    Complete implementation for ChallengeMessageId

--- src/main/java/edu/stanford/bmir/protege/web/shared/auth/ChallengeMessageId.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/auth/ChallengeMessageId.java
@@ -1,6 +1,10 @@
 package edu.stanford.bmir.protege.web.shared.auth;
 
+import com.google.common.base.Objects;
 import com.google.gwt.user.client.rpc.IsSerializable;
+
+import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Matthew Horridge
@@ -15,10 +19,35 @@ public class ChallengeMessageId implements IsSerializable {
     }
 
     public ChallengeMessageId(String id) {
-        this.id = id;
+        this.id = checkNotNull(id);
     }
 
     public String getId() {
         return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof ChallengeMessageId)) {
+            return false;
+        }
+        ChallengeMessageId other = (ChallengeMessageId) obj;
+        return this.id.equals(other.id);
+    }
+
+
+    @Override
+    public String toString() {
+        return toStringHelper("ChallengeMessageId")
+                .addValue(id)
+                .toString();
     }
 }
