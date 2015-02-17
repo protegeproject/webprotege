817368b4d9e50d3d6830b58a69a8cf6ab3632a9d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 15:00:48 +0000

    Added value object for salted password digests

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/auth/SaltedPasswordDigest.java
@@ -0,0 +1,60 @@
+package edu.stanford.bmir.protege.web.shared.auth;
+
+import com.google.common.base.Objects;
+import com.google.common.io.BaseEncoding;
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.Arrays;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17/02/15
+ *
+ * Represents the MD5 digest of a salted password.
+ */
+public class SaltedPasswordDigest implements IsSerializable {
+
+    private byte [] digest;
+
+    /**
+     * For serialization purposes only
+     */
+    private SaltedPasswordDigest() {
+    }
+
+    public SaltedPasswordDigest(byte[] digest) {
+        this.digest = checkNotNull(digest);
+    }
+
+    public byte[] getDigest() {
+        return Arrays.copyOf(digest, digest.length);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(digest);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof SaltedPasswordDigest)) {
+            return false;
+        }
+        SaltedPasswordDigest other = (SaltedPasswordDigest) obj;
+        return Arrays.equals(this.digest, other.digest);
+    }
+
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper("SaltedPasswordDigest")
+                .addValue(BaseEncoding.base16().lowerCase().encode(digest))
+                .toString();
+    }
+}
