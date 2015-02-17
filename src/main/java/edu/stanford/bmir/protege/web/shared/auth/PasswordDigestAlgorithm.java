244dcd8f4972cf22385fa50d5c65e856d3b9a0f4
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 14:14:11 +0000

    Updated PasswordDigestAlgorithm to use Salt value object

--- src/main/java/edu/stanford/bmir/protege/web/shared/auth/PasswordDigestAlgorithm.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/auth/PasswordDigestAlgorithm.java
@@ -27,18 +27,15 @@ public class PasswordDigestAlgorithm {
     /**
      * Gets the digested salted password for the given clear text password and salt.
      * @param clearTextPassword The clear text password.  Not {@code null}.
-     * @param salt The salt.  Not {@code null}.  Not empty.
+     * @param salt The salt.  Not {@code null}.
      * @return The digest of the salted password.
      * @throws java.lang.IllegalArgumentException if the salt is empty.
      * @throws java.lang.NullPointerException if any parameters are {@code null}.
      */
-    public byte [] getDigestOfSaltedPassword(String clearTextPassword, byte[] salt) {
+    public byte [] getDigestOfSaltedPassword(String clearTextPassword, Salt salt) {
         try {
             MessageDigestAlgorithm messageDigestAlgorithm = messageDigestAlgorithmProvider.get();
-            messageDigestAlgorithm.update(checkNotNull(salt));
-            if(salt.length == 0) {
-                throw new IllegalArgumentException("Salt is empty.  This is probably unintentional.");
-            }
+            messageDigestAlgorithm.update(checkNotNull(salt).getBytes());
             byte [] utf8EncodedPassword = checkNotNull(clearTextPassword).getBytes("UTF-8");
             messageDigestAlgorithm.update(utf8EncodedPassword);
             return messageDigestAlgorithm.computeDigest();
