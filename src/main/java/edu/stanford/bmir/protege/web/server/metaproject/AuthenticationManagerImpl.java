1fe3b7d7708c35dd2dfb9e7169d45de85fd7f8f9
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 14:18:52 +0000

    Updated AuthenticationManager and implementation to use Salt value object

--- src/main/java/edu/stanford/bmir/protege/web/server/metaproject/AuthenticationManagerImpl.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/metaproject/AuthenticationManagerImpl.java
@@ -1,15 +1,21 @@
 package edu.stanford.bmir.protege.web.server.metaproject;
 
+import com.google.common.base.Optional;
+import com.google.common.io.BaseEncoding;
 import com.google.inject.Inject;
 import edu.stanford.bmir.protege.web.client.rpc.data.UserData;
 import edu.stanford.bmir.protege.web.server.AuthenticationUtil;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIMetaProjectStore;
+import edu.stanford.bmir.protege.web.shared.auth.Salt;
 import edu.stanford.bmir.protege.web.shared.user.UserEmailAlreadyExistsException;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
 import edu.stanford.bmir.protege.web.shared.user.UserNameAlreadyExistsException;
 import edu.stanford.bmir.protege.web.shared.user.UserRegistrationException;
 import edu.stanford.smi.protege.server.metaproject.MetaProject;
 import edu.stanford.smi.protege.server.metaproject.User;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -36,8 +42,8 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
         if (existingUser != null) {
             throw new UserNameAlreadyExistsException(userName);
         }
-        for(User user : metaProject.getUsers()) {
-            if(email.equals(user.getEmail())) {
+        for (User user : metaProject.getUsers()) {
+            if (email.equals(user.getEmail())) {
                 throw new UserEmailAlreadyExistsException(email);
             }
         }
@@ -58,7 +64,7 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
 
     @Override
     public void setDigestedPassword(UserId userId, String encryptedPassword, String salt) {
-        if(userId.isGuest()) {
+        if (userId.isGuest()) {
             return;
         }
         User user = metaProject.getUser(userId.getUserName());
@@ -68,4 +74,27 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
         user.setDigestedPassword(encryptedPassword, salt);
         OWLAPIMetaProjectStore.getStore().saveMetaProject(metaProject);
     }
+
+    @Override
+    public Optional<Salt> getSalt(UserId userId) {
+        User user = metaProject.getUser(userId.getUserName());
+        if (user == null) {
+            return Optional.absent();
+        }
+        String salt = user.getSalt();
+        byte[] saltBytes = BaseEncoding.base16().decode(salt);
+        return Optional.fromNullable(new Salt(saltBytes));
+    }
+
+    @Override
+    public Optional<byte[]> getDigestOfSaltedPassword(UserId userId) {
+        User user = metaProject.getUser(userId.getUserName());
+        if (user == null) {
+            return Optional.absent();
+        }
+        String pwd = user.getDigestedPassword();
+        byte[] pwdBytes = BaseEncoding.base16().decode(pwd);
+        return Optional.of(pwdBytes);
+    }
+
 }
