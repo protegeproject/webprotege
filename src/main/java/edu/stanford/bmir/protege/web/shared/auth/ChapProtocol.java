cb1a453f28c59ad871f7298417f4c188e213bae7
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-17 17:55:07 +0000

    Updated guava dependency

--- src/main/java/edu/stanford/bmir/protege/web/shared/auth/ChapProtocol.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.bmir.protege.web.shared.auth;
-
-/**
- * Matthew Horridge
- * Stanford Center for Biomedical Informatics Research
- * 17/02/15
- */
-public class ChapProtocol {
-}
