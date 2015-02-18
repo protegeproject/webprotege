0681b6c78b9feb2a4d1dede6c65c5d4e89c9c198
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-18 19:26:44 +0000

    Added logging module

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/inject/LoggingModule.java
@@ -0,0 +1,18 @@
+package edu.stanford.bmir.protege.web.server.inject;
+
+import com.google.inject.AbstractModule;
+import edu.stanford.bmir.protege.web.server.logging.DefaultLogger;
+import edu.stanford.bmir.protege.web.server.logging.WebProtegeLogger;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17/02/15
+ */
+public class LoggingModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(WebProtegeLogger.class).toInstance(new DefaultLogger(LoggingModule.class));
+    }
+}
