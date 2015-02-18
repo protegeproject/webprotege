a5161e5def9dde5bcb700cc8bb2458449c5cf98f
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-18 19:27:20 +0000

    Added metaproject password digest tests

---
+++ src/test/java/edu/stanford/bmir/protege/web/server/metaproject/DigestedPassword_TestCase.java
@@ -0,0 +1,62 @@
+package edu.stanford.bmir.protege.web.server.metaproject;
+
+import com.google.common.io.BaseEncoding;
+import edu.stanford.bmir.protege.web.shared.auth.Md5DigestAlgorithmProvider;
+import edu.stanford.bmir.protege.web.shared.auth.PasswordDigestAlgorithm;
+import edu.stanford.bmir.protege.web.shared.auth.Salt;
+import edu.stanford.bmir.protege.web.shared.auth.SaltedPasswordDigest;
+import edu.stanford.smi.protege.server.metaproject.MetaProject;
+import edu.stanford.smi.protege.server.metaproject.User;
+import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17/02/15
+ */
+public class DigestedPassword_TestCase {
+
+    public static final String DIGESTED_PASSWORD = "bcc1ae0ca0834b41787c8c1ecd8feba8";
+    public static final String SALT = "54d4bff73b7df8df";
+
+    private User userX;
+
+    private MetaProject metaProject;
+
+    @Before
+    public void setUp() throws Exception {
+        URI metaProjectURI = getClass().getResource("NullUserNameMetaproject.pprj").toURI();
+        metaProject = new MetaProjectImpl(metaProjectURI);
+        userX = metaProject.getUser("UserX");
+    }
+
+    @Test
+    public void shouldFindUserX() {
+        assertThat(userX != null, is(true));
+    }
+
+    @Test
+    public void shouldHaveDigestedPassword() {
+        assertThat(userX.getDigestedPassword(), is(DIGESTED_PASSWORD));
+    }
+
+    @Test
+    public void shouldHaveSalt() {
+        assertThat(userX.getSalt(), is(SALT));
+    }
+
+    @Test
+    public void shouldGenerateSameDigestedPassword() {
+        PasswordDigestAlgorithm passwordDigestAlgorithm = new PasswordDigestAlgorithm(new Md5DigestAlgorithmProvider());
+        Salt salt = new Salt(BaseEncoding.base16().lowerCase().decode(SALT));
+        SaltedPasswordDigest digest = passwordDigestAlgorithm.getDigestOfSaltedPassword("password", salt);
+        assertThat(digest.getBytes(), is(BaseEncoding.base16().lowerCase().decode(DIGESTED_PASSWORD)));
+    }
+}
