62d0e5481f208f160984783ac41e2d15f857ed92
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-18 19:22:54 +0000

    Adding a provider implementation fo ChapSessionId
    ChapSessionIdProvider and tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/auth/ChapSessionIdProvider.java
@@ -0,0 +1,23 @@
+package edu.stanford.bmir.protege.web.server.auth;
+
+import edu.stanford.bmir.protege.web.shared.auth.ChapSessionId;
+
+import javax.inject.Provider;
+import java.util.UUID;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 14/02/15
+ */
+public class ChapSessionIdProvider implements Provider<ChapSessionId> {
+
+    @Override
+    public ChapSessionId get() {
+        return new ChapSessionId(getUuid());
+    }
+
+    private String getUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
