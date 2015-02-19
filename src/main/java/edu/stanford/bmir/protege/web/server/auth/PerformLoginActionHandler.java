c1f67b8c7f952643bdffc8306caf8e9516cc650b
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-19 12:04:12 +0000

    Made authenticated action handle code reusable

--- src/main/java/edu/stanford/bmir/protege/web/server/auth/PerformLoginActionHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/auth/PerformLoginActionHandler.java
@@ -1,7 +1,5 @@
 package edu.stanford.bmir.protege.web.server.auth;
 
-import com.google.common.base.Optional;
-import edu.stanford.bmir.protege.web.server.dispatch.ActionHandler;
 import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
 import edu.stanford.bmir.protege.web.server.dispatch.RequestContext;
 import edu.stanford.bmir.protege.web.server.dispatch.RequestValidator;
@@ -10,7 +8,6 @@ import edu.stanford.bmir.protege.web.server.logging.WebProtegeLogger;
 import edu.stanford.bmir.protege.web.server.metaproject.AuthenticationManager;
 import edu.stanford.bmir.protege.web.server.session.WebProtegeSession;
 import edu.stanford.bmir.protege.web.shared.auth.*;
-import edu.stanford.bmir.protege.web.shared.user.UserId;
 
 import javax.inject.Inject;
 
@@ -19,22 +16,11 @@ import javax.inject.Inject;
  * Stanford Center for Biomedical Informatics Research
  * 14/02/15
  */
-public class PerformLoginActionHandler implements ActionHandler<PerformLoginAction, PerformLoginResult> {
-
-    private ChapSessionManager chapSessionManager;
-
-    private AuthenticationManager authenticationManager;
-
-    private ChapResponseChecker chapResponseChecker;
-
-    private WebProtegeLogger logger;
+public class PerformLoginActionHandler extends AuthenticatedActionHandler<PerformLoginAction, PerformLoginResult> {
 
     @Inject
     public PerformLoginActionHandler(ChapSessionManager chapSessionManager, AuthenticationManager authenticationManager, ChapResponseChecker chapResponseChecker, WebProtegeLogger logger) {
-        this.chapSessionManager = chapSessionManager;
-        this.authenticationManager = authenticationManager;
-        this.chapResponseChecker = chapResponseChecker;
-        this.logger = logger;
+        super(chapSessionManager, authenticationManager, chapResponseChecker, logger);
     }
 
     @Override
@@ -48,33 +34,14 @@ public class PerformLoginActionHandler implements ActionHandler<PerformLoginActi
     }
 
     @Override
-    public PerformLoginResult execute(PerformLoginAction action, ExecutionContext executionContext) {
-        UserId userId = action.getUserId();
-        Optional<SaltedPasswordDigest> passwordDigest = authenticationManager.getSaltedPasswordDigest(userId);
-        if (!passwordDigest.isPresent()) {
-            logger.info("Authentication attempt, but no digestOfSaltedPassword set for user %s", userId);
-            return new PerformLoginResult(AuthenticationResponse.FAIL);
-        }
-        Optional<ChapSession> chapDataOptional = chapSessionManager.retrieveChallengeMessage(action.getChapSessionId());
-        if (!chapDataOptional.isPresent()) {
-            logger.info("Challenge expired for user %s", userId);
-            return new PerformLoginResult(AuthenticationResponse.FAIL);
-        }
-        ChapSession chapSession = chapDataOptional.get();
-        ChallengeMessage challenge = chapSession.getChallengeMessage();
-        ChapResponse chapResponse = action.getChapResponse();
+    protected PerformLoginResult executeAuthenticatedAction(PerformLoginAction action, ExecutionContext executionContext) {
+        WebProtegeSession session = executionContext.getSession();
+        session.setUserInSession(action.getUserId());
+        return new PerformLoginResult(AuthenticationResponse.SUCCESS);
+    }
 
-        boolean expectedResponse = chapResponseChecker.isExpectedResponse(
-                chapResponse,
-                challenge,
-                passwordDigest.get());
-        if (expectedResponse) {
-            WebProtegeSession session = executionContext.getSession();
-            session.setUserInSession(userId);
-            return new PerformLoginResult(AuthenticationResponse.SUCCESS);
-        } else {
-            return new PerformLoginResult(AuthenticationResponse.FAIL);
-        }
-
+    @Override
+    protected PerformLoginResult createAuthenticationFailedResult() {
+        return new PerformLoginResult(AuthenticationResponse.FAIL);
     }
 }
