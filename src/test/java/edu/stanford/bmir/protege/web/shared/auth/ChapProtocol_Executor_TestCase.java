6d662bf74ca73904c15a0c9b8d43c500abbdfa86
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-19 09:28:06 +0000

    Added executor for the Chap protocol on the client side
    - ChapProtocolExecutor and tests

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/auth/ChapProtocol_Executor_TestCase.java
@@ -0,0 +1,157 @@
+package edu.stanford.bmir.protege.web.shared.auth;
+
+import com.google.common.base.Optional;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
+import edu.stanford.bmir.protege.web.shared.dispatch.Action;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 19/02/15
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ChapProtocol_Executor_TestCase {
+
+    @Mock
+    private DispatchServiceManager dispatchServiceManager;
+
+    @Mock
+    private PasswordDigestAlgorithm passwordDigestAlgorithm;
+
+    @Mock
+    private ChapResponseDigestAlgorithm chapResponseDigestAlgorithm;
+
+
+    private ChapProtocolExecutor protocol;
+
+    @Mock
+    private UserId userId;
+
+    @Mock
+    private AsyncCallback<AuthenticationResponse> callback;
+
+    private String clearTextPassword = "ThePassword";
+
+    @Mock
+    private GetChapSessionResult chapSessionResult;
+
+    @Mock
+    private ChapResponse chapResponse;
+
+    @Mock
+    private SaltedPasswordDigest saltedPassword;
+
+    @Mock
+    private ChapSession chapSession;
+
+    @Mock
+    private ChapSessionId chapSessionId;
+
+    @Mock
+    private Salt salt;
+
+    @Mock
+    private ChallengeMessage challengeMessage;
+
+    @Mock
+    private PerformLoginResult performLoginResult;
+
+
+    @Before
+    public void setUp() throws Exception {
+        protocol = new ChapProtocolExecutor(dispatchServiceManager, passwordDigestAlgorithm, chapResponseDigestAlgorithm);
+
+        when(passwordDigestAlgorithm
+                .getDigestOfSaltedPassword(anyString(), any(Salt.class)))
+                .thenReturn(saltedPassword);
+
+        when(chapResponseDigestAlgorithm
+                .getChapResponseDigest(any(ChallengeMessage.class), any(SaltedPasswordDigest.class)))
+                .thenReturn(chapResponse);
+
+        when(chapSessionResult.getChapSession()).thenReturn(Optional.of(chapSession));
+        when(chapSession.getId()).thenReturn(chapSessionId);
+        when(chapSession.getSalt()).thenReturn(salt);
+        when(chapSession.getChallengeMessage()).thenReturn(challengeMessage);
+
+        doAnswer(new Answer() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Object[] args = invocationOnMock.getArguments();
+                if (args[0] instanceof GetChapSessionAction) {
+                    AsyncCallback<GetChapSessionResult> cb = (AsyncCallback<GetChapSessionResult>) args[1];
+                    cb.onSuccess(chapSessionResult);
+                }
+                else if(args[1] instanceof PerformLoginAction) {
+                    AsyncCallback<PerformLoginResult> cb = (AsyncCallback<PerformLoginResult>) args[1];
+                    cb.onSuccess(performLoginResult);
+                }
+                return null;
+            }
+        }).when(dispatchServiceManager).execute(any(Action.class), any(AsyncCallback.class));
+    }
+
+    @Test
+    public void shouldExecute_GetChapSession_ForUserId() {
+        protocol.execute(userId, clearTextPassword, callback);
+        verify(dispatchServiceManager, atLeastOnce()).execute(argThat(isAGetChapSessionAction()), anyGetChapSessionResultCallback());
+    }
+
+    @Test
+    public void shouldExecute_PerformLogin_ForChapResponse() {
+        protocol.execute(userId, clearTextPassword, callback);
+        verify(dispatchServiceManager).execute(argThat(isAPerformLoginAction()), Matchers.<AsyncCallback<PerformLoginResult>>any());
+    }
+
+    private Matcher<GetChapSessionAction> isAGetChapSessionAction() {
+        return new TypeSafeMatcher<GetChapSessionAction>() {
+            @Override
+            protected boolean matchesSafely(GetChapSessionAction item) {
+                return item.getUserId().equals(userId);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("GetChapSessionAction with userId " + userId);
+            }
+        };
+    }
+
+    private Matcher<PerformLoginAction> isAPerformLoginAction() {
+        return new TypeSafeMatcher<PerformLoginAction>() {
+            @Override
+            protected boolean matchesSafely(PerformLoginAction item) {
+                return item.getUserId().equals(userId)
+                        && item.getChapResponse().equals(chapResponse)
+                        && item.getChapSessionId().equals(chapSessionId);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("A PerformLoginAction " + new PerformLoginAction(userId, chapSessionId, chapResponse));
+            }
+        };
+    }
+
+
+    private AsyncCallback<GetChapSessionResult> anyGetChapSessionResultCallback() {
+        return Matchers.any();
+    }
+}
