e74d477c7f28e456cc9eb97244626599139d2bd5
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-21 14:09:12 +0000

    Action and Result for creating projects
    CreateNewProjectAction
    CreateNewProjectResult
    Tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/project/CreateNewProjectAction.java
@@ -0,0 +1,56 @@
+package edu.stanford.bmir.protege.web.shared.project;
+
+import com.google.common.base.Objects;
+import edu.stanford.bmir.protege.web.client.rpc.data.NewProjectSettings;
+import edu.stanford.bmir.protege.web.shared.dispatch.Action;
+
+import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 21/02/15
+ */
+public class CreateNewProjectAction implements Action<CreateNewProjectResult> {
+
+    private NewProjectSettings newProjectSettings;
+
+    /**
+     * For serialization purposes only
+     */
+    private CreateNewProjectAction() {
+    }
+
+    public CreateNewProjectAction(NewProjectSettings newProjectSettings) {
+        this.newProjectSettings = checkNotNull(newProjectSettings);
+    }
+
+    public NewProjectSettings getNewProjectSettings() {
+        return newProjectSettings;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(newProjectSettings);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof CreateNewProjectAction)) {
+            return false;
+        }
+        CreateNewProjectAction other = (CreateNewProjectAction) obj;
+        return this.newProjectSettings.equals(other.newProjectSettings);
+    }
+
+    @Override
+    public String toString() {
+        return toStringHelper("CreateNewProjectAction")
+                .addValue(newProjectSettings)
+                .toString();
+    }
+}
---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/project/CreateNewProjectResult_TestCase.java
@@ -0,0 +1,72 @@
+package edu.stanford.bmir.protege.web.shared.project;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 21/02/15
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CreateNewProjectResult_TestCase {
+
+
+    private CreateNewProjectResult result;
+
+    private CreateNewProjectResult otherResult;
+
+    @Mock
+    private ProjectDetails projectDetails;
+
+
+    @Before
+    public void setUp() throws Exception {
+        result = new CreateNewProjectResult(projectDetails);
+        otherResult = new CreateNewProjectResult(projectDetails);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerException() {
+        new CreateNewProjectResult(null);
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(result, is(equalTo(result)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToNull() {
+        assertThat(result, is(not(equalTo(null))));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(result, is(equalTo(otherResult)));
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeAsOther() {
+        assertThat(result.hashCode(), is(otherResult.hashCode()));
+    }
+
+    @Test
+    public void shouldGenerateToString() {
+        assertThat(result.toString(), startsWith("CreateNewProjectResult"));
+    }
+
+    @Test
+    public void shouldReturnSuppliedProjectDetails() {
+        assertThat(result.getProjectDetails(), is(projectDetails));
+    }
+}
