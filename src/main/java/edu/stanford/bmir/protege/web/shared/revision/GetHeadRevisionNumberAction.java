4d6ea72e5db17de43a2ebaae38d2687e9aeebab1
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-21 13:26:53 +0000

    Added action and result to get the head revision number
    GetHeadRevisionNumberAction
    GetHeadRevisionNumberResult
    Tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/revision/GetHeadRevisionNumberAction.java
@@ -0,0 +1,56 @@
+package edu.stanford.bmir.protege.web.shared.revision;
+
+import com.google.common.base.Objects;
+import edu.stanford.bmir.protege.web.shared.HasProjectId;
+import edu.stanford.bmir.protege.web.shared.dispatch.Action;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+
+import static com.google.common.base.Objects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 21/02/15
+ */
+public class GetHeadRevisionNumberAction implements Action<GetHeadRevisionNumberResult>, HasProjectId {
+
+    private ProjectId projectId;
+
+    private GetHeadRevisionNumberAction() {
+    }
+
+    public GetHeadRevisionNumberAction(ProjectId projectId) {
+        this.projectId = checkNotNull(projectId);
+    }
+
+    @Override
+    public ProjectId getProjectId() {
+        return projectId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(projectId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof GetHeadRevisionNumberAction)) {
+            return false;
+        }
+        GetHeadRevisionNumberAction other = (GetHeadRevisionNumberAction) obj;
+        return this.projectId.equals(other.projectId);
+    }
+
+
+    @Override
+    public String toString() {
+        return toStringHelper("GetHeadRevisionNumberAction")
+                .addValue(projectId)
+                .toString();
+    }
+}
