2684fc453981a45b917a868136a65ddd1843a3eb
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-21 15:14:08 +0000

    Removed ProjectManager service
    Replaced ProjectManagerService calls with DispatchService calls

--- src/main/java/edu/stanford/bmir/protege/web/client/ui/ontology/home/NewProjectDialogController.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/ui/ontology/home/NewProjectDialogController.java
@@ -1,22 +1,18 @@
 package edu.stanford.bmir.protege.web.client.ui.ontology.home;
 
 import com.google.common.base.Optional;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Focusable;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtext.client.widgets.MessageBox;
 import edu.stanford.bmir.protege.web.client.Application;
-import edu.stanford.bmir.protege.web.client.rpc.ProjectManagerService;
-import edu.stanford.bmir.protege.web.client.rpc.ProjectManagerServiceAsync;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceCallbackWithProgressDisplay;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
 import edu.stanford.bmir.protege.web.client.rpc.data.NewProjectSettings;
 import edu.stanford.bmir.protege.web.client.rpc.data.NotSignedInException;
 import edu.stanford.bmir.protege.web.client.ui.library.dlg.*;
 import edu.stanford.bmir.protege.web.client.ui.projectmanager.ProjectCreatedEvent;
 import edu.stanford.bmir.protege.web.shared.event.EventBusManager;
-import edu.stanford.bmir.protege.web.shared.project.ProjectAlreadyRegisteredException;
-import edu.stanford.bmir.protege.web.shared.project.ProjectDetails;
-import edu.stanford.bmir.protege.web.shared.project.ProjectDocumentExistsException;
+import edu.stanford.bmir.protege.web.shared.project.*;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
 
 /**
@@ -51,39 +47,42 @@ public class NewProjectDialogController extends WebProtegeOKCancelDialogControll
             throw new RuntimeException("User is guest.  Guest users are not allowed to create projects.");
         }
         NewProjectSettings newProjectSettings = new NewProjectSettings(userId, data.getProjectName(), data.getProjectDescription(), data.getProjectType());
-        ProjectManagerServiceAsync projectManagerService = GWT.create(ProjectManagerService.class);
-        projectManagerService.createNewProject(newProjectSettings, new AsyncCallback<ProjectDetails>() {
-            public void onFailure(Throwable caught) {
-                handleCreateProjectFailure(caught);
+        DispatchServiceManager.get().execute(new CreateNewProjectAction(newProjectSettings), new DispatchServiceCallbackWithProgressDisplay<CreateNewProjectResult>() {
+            @Override
+            public String getProgressDisplayTitle() {
+                return "Creating project";
             }
 
-            public void onSuccess(ProjectDetails result) {
-                handleCreateProjectSuccess(result);
+            @Override
+            public String getProgressDisplayMessage() {
+                return "Please wait.";
+            }
+
+            @Override
+            public void handleSuccess(CreateNewProjectResult result) {
+                EventBusManager.getManager().postEvent(new ProjectCreatedEvent(result.getProjectDetails()));
+            }
+
+            @Override
+            public void handleExecutionException(Throwable cause) {
+                if(cause instanceof NotSignedInException) {
+                    MessageBox.alert("You must be signed in to create new projects");
+                }
+                else if(cause instanceof ProjectAlreadyRegisteredException) {
+                    ProjectAlreadyRegisteredException ex = (ProjectAlreadyRegisteredException) cause;
+                    String projectName = ex.getProjectId().getId();
+                    MessageBox.alert("The project name " + projectName + " is already registered.  Please try a different name.");
+                }
+                else if(cause instanceof ProjectDocumentExistsException) {
+                    ProjectDocumentExistsException ex = (ProjectDocumentExistsException) cause;
+                    String projectName = ex.getProjectId().getId();
+                    MessageBox.alert("There is already a non-empty project on the server with the id " + projectName + ".  This project has NOT been overwritten.  Please contact the administrator to resolve this issue.");
+                }
+                else {
+                    MessageBox.alert(cause.getMessage());
+                }
             }
         });
-    }
-
-    private void handleCreateProjectFailure(Throwable caught) {
-        if(caught instanceof NotSignedInException) {
-            MessageBox.alert("You must be signed in to create new projects");
-        }
-        else if(caught instanceof ProjectAlreadyRegisteredException) {
-            ProjectAlreadyRegisteredException ex = (ProjectAlreadyRegisteredException) caught;
-            String projectName = ex.getProjectId().getId();
-            MessageBox.alert("The project name " + projectName + " is already registered.  Please try a different name.");
-        }
-        else if(caught instanceof ProjectDocumentExistsException) {
-            ProjectDocumentExistsException ex = (ProjectDocumentExistsException) caught;
-            String projectName = ex.getProjectId().getId();
-            MessageBox.alert("There is already a non-empty project on the server with the id " + projectName + ".  This project has NOT been overwritten.  Please contact the administrator to resolve this issue.");
-        }
-        else {
-            MessageBox.alert(caught.getMessage());
-        }
-    }
-
-    private void handleCreateProjectSuccess(ProjectDetails projectDetails) {
-        EventBusManager.getManager().postEvent(new ProjectCreatedEvent(projectDetails));
     }
 
 
