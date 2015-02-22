b7f03a2c9362b8eb699ff345d0b3e7a59e432262
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-22 09:26:45 +0000

    Added some documentation to DispatchErrorMessageDisplay

--- src/main/java/edu/stanford/bmir/protege/web/client/dispatch/DispatchErrorMessageDisplay.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/dispatch/DispatchErrorMessageDisplay.java
@@ -7,11 +7,27 @@ package edu.stanford.bmir.protege.web.client.dispatch;
  */
 public interface DispatchErrorMessageDisplay {
 
+    /**
+     * Display an error message indicating that the submitted action could not be executed because the user does not
+     * have permission to execute it.
+     */
     void displayPermissionDeniedErrorMessage();
 
-    void displayGeneralErrorMessage(String title, String message);
-
+    /**
+     * Display an error message for an {@link com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException}
+     */
     void displayIncompatibleRemoteServiceExceptionErrorMessage();
 
+    /**
+     * Display an error message for an {@link com.google.gwt.user.client.rpc.InvocationException}
+     */
     void displayInvocationExceptionErrorMessage();
+
+    /**
+     * Display a general error message.
+     * @param title The error message title.  Not {@code null}.
+     * @param message The error message.  Not {@code null}.
+     */
+    void displayGeneralErrorMessage(String title, String message);
+
 }
