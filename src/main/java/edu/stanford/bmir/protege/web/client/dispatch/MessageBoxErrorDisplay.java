dab3e6ae17b89d18aa78e435dd50d09fdf03954e
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-22 09:28:18 +0000

    Cleaned up error messages

--- src/main/java/edu/stanford/bmir/protege/web/client/dispatch/MessageBoxErrorDisplay.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/dispatch/MessageBoxErrorDisplay.java
@@ -11,7 +11,9 @@ public class MessageBoxErrorDisplay implements DispatchErrorMessageDisplay {
 
     @Override
     public void displayPermissionDeniedErrorMessage() {
-        MessageBox.showAlert("You do not have permission to carry out the specified action");
+        MessageBox.showAlert(
+                "Permission denied",
+                "You do not have permission to carry out the specified action.");
     }
 
     @Override
@@ -21,11 +23,16 @@ public class MessageBoxErrorDisplay implements DispatchErrorMessageDisplay {
 
     @Override
     public void displayIncompatibleRemoteServiceExceptionErrorMessage() {
-        MessageBox.showMessage("WebProtégé has been upgraded.  Please refresh your browser.");
+        MessageBox.showMessage(
+                "Please refresh your browser",
+                "WebProtégé has been upgraded.  Please refresh your browser.");
     }
 
     @Override
     public void displayInvocationExceptionErrorMessage() {
-        MessageBox.showMessage("There was a problem communicating with WebProtégé.  Please check your network connection and try again.");
+        MessageBox.showMessage(
+                "Error",
+                "There was a problem communicating with WebProtégé.  " +
+                        "Please check your network connection and try again.");
     }
 }
