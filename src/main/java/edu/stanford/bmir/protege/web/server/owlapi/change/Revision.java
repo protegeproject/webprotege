e382aea3346af70b5a21a15cac6f15e9d890dd4d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-27 09:03:39 +0000

    Updated Revision
    - Removed detailed rendering (now done elsewhere)
    - Return an ImmutableSet for entities
    - Switch AxiomSubjectProvider to WebProtege version
    - Removed commented out code

--- src/main/java/edu/stanford/bmir/protege/web/server/owlapi/change/Revision.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/owlapi/change/Revision.java
@@ -1,15 +1,16 @@
 package edu.stanford.bmir.protege.web.server.owlapi.change;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
+import edu.stanford.bmir.protege.web.shared.DataFactory;
+import edu.stanford.bmir.protege.web.shared.axiom.AxiomSubjectProvider;
 import edu.stanford.bmir.protege.web.shared.revision.RevisionNumber;
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProject;
 import edu.stanford.bmir.protege.web.server.owlapi.RenderingManager;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
 import org.semanticweb.owlapi.change.*;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLObject;
-import org.semanticweb.owlapi.util.AxiomSubjectProvider;
+import org.semanticweb.owlapi.model.*;
+
 
 import java.util.*;
 
@@ -38,7 +39,7 @@ public class Revision implements Iterable<OWLOntologyChangeRecord>, Comparable<R
 
     private RevisionType revisionType;
 
-    private Set<OWLEntity> cachedEntities = null;
+    private ImmutableSet<OWLEntity> cachedEntities = null;
 
     public Revision(UserId userId, RevisionNumber revision, List<OWLOntologyChangeRecord> changes, long timestamp, String highLevelDescription, RevisionType revisionType) {
         this.changes = checkNotNull(new OWLOntologyChangeRecordList(changes));
@@ -72,33 +73,44 @@ public class Revision implements Iterable<OWLOntologyChangeRecord>, Comparable<R
         return revision;
     }
 
-    public Set<OWLEntity> getEntities(OWLAPIProject project) {
+    public boolean containsEntity(OWLAPIProject project, OWLEntity entity) {
+        return getEntities(project).contains(entity);
+    }
+
+    public ImmutableSet<OWLEntity> getEntities(OWLAPIProject project) {
         if (cachedEntities == null) {
             cachedEntities = getEntitiesInternal(project);
         }
         return cachedEntities;
     }
 
-    private Set<OWLEntity> getEntitiesInternal(OWLAPIProject project) {
-        Set<OWLEntity> result = new HashSet<OWLEntity>();
-        Set<IRI> iris = new HashSet<IRI>();
+    private ImmutableSet<OWLEntity> getEntitiesInternal(OWLAPIProject project) {
+        ImmutableSet.Builder<OWLEntity> result = ImmutableSet.builder();
+        Set<IRI> iris = new HashSet<>();
+        AxiomSubjectProvider axiomSubjectProvider = project.getAxiomSubjectProvider();
         for (OWLOntologyChangeRecord change : changes) {
             if (change.getData() instanceof AxiomChangeData) {
                 OWLAxiom ax = ((AxiomChangeData) change.getData()).getAxiom();
-                AxiomSubjectProvider axiomSubjectProvider = new AxiomSubjectProvider();
-                OWLObject object = axiomSubjectProvider.getSubject(ax);
-                if (object instanceof OWLEntity) {
-                    result.add((OWLEntity) object);
-                }
-                else if (object instanceof IRI) {
-                    iris.add((IRI) object);
+                Optional<? extends OWLObject> subject = axiomSubjectProvider.getSubject(ax);
+                if (subject.isPresent()) {
+                    if (subject.get() instanceof OWLEntity) {
+                        result.add((OWLEntity) subject.get());
+                    }
+                    else if (subject.get() instanceof IRI) {
+                        iris.add((IRI) subject.get());
+                    }
                 }
             }
         }
         for (IRI iri : iris) {
-            result.addAll(project.getRootOntology().getEntitiesInSignature(iri));
+            result.add(project.getDataFactory().getOWLClass(iri));
+            result.add(project.getDataFactory().getOWLObjectProperty(iri));
+            result.add(project.getDataFactory().getOWLDataProperty(iri));
+            result.add(project.getDataFactory().getOWLAnnotationProperty(iri));
+            result.add(project.getDataFactory().getOWLNamedIndividual(iri));
+            result.add(project.getDataFactory().getOWLDatatype(iri));
         }
-        return result;
+        return result.build();
     }
 
 
@@ -123,91 +135,13 @@ public class Revision implements Iterable<OWLOntologyChangeRecord>, Comparable<R
     }
 
     public String getHighLevelDescription(final OWLAPIProject project, OWLEntity entity) {
-        StringBuilder sb = new StringBuilder();
-        if (highLevelDescription != null) {
-            sb.append(highLevelDescription);
-        }
-        sb.append("<div style=\"width: 100%;\">");
-        sb.append("<div><b>Details:</b></div>");
-        sb.append("<div style=\"margin-left: 20px\">");
-        int counter = 0;
-        for (final OWLOntologyChangeRecord changeRecord : changes) {
-            if (changeRecord.getData() instanceof AxiomChangeData) {
-                AxiomChangeData info = (AxiomChangeData) changeRecord.getData();
-                if (entity == null || isEntitySubjectOfChange(entity, info)) {
-                    sb.append("<div style=\"overflow: hidden;\">");
-                    String ren = changeRecord.getData().accept(new OWLOntologyChangeDataVisitor<String, RuntimeException>() {
-
-                        public String visit(AddAxiomData addAxiom) {
-                            final RenderingManager rm = project.getRenderingManager();
-                            return "<b>Added: </b> " + rm.getBrowserText(addAxiom.getAxiom());
-                        }
-
-                        public String visit(RemoveAxiomData removeAxiom) {
-                            final RenderingManager rm = project.getRenderingManager();
-                            return new StringBuilder().append("<b>Removed: </b> ").append(rm.getBrowserText(removeAxiom.getAxiom())).toString();
-                        }
-
-                        public String visit(SetOntologyIDData setOntologyID) {
-                            return new StringBuilder().append("Changed ontology id from ").append(changeRecord.getOntologyID()).append(" to ").append(setOntologyID.getNewId()).toString();
-                        }
-
-                        public String visit(AddImportData addImport) {
-                            return new StringBuilder().append("Added import: ").append(addImport.getDeclaration().getIRI().toQuotedString()).toString();
-                        }
-
-                        public String visit(RemoveImportData removeImport) {
-                            return new StringBuilder().append("Removed import: ").append(removeImport.getDeclaration().getIRI().toQuotedString()).toString();
-                        }
-
-                        public String visit(AddOntologyAnnotationData addOntologyAnnotation) {
-                            return new StringBuilder().append("Added annotation to ontology: ").append(project.getRenderingManager().getBrowserText(addOntologyAnnotation.getAnnotation())).toString();
-                        }
-
-                        public String visit(RemoveOntologyAnnotationData removeOntologyAnnotation) {
-                            return new StringBuilder().append("Removed annotation from ontology: ").append(project.getRenderingManager().getBrowserText(removeOntologyAnnotation.getAnnotation())).toString();
-                        }
-                    });
-                    sb.append(ren);
-                    sb.append("</div>");
-
-                }
-                if (counter == DESCRIPTION_MAX_CHANGE_COUNT && getSize() > DESCRIPTION_MAX_CHANGE_COUNT) {
-                    sb.append("<div>");
-                    sb.append(" + ");
-                    sb.append(getSize() - DESCRIPTION_MAX_CHANGE_COUNT);
-                    sb.append(" more");
-                    sb.append("</div>");
-                    break;
-                }
-                counter++;
-            }
-        }
-
-        sb.append("</div>");
-        sb.append("</div>");
-
-        return sb.toString();
+        return highLevelDescription != null ? highLevelDescription : "";
     }
 
-    private boolean isEntitySubjectOfChange(OWLEntity entity, AxiomChangeData change) {
-        OWLObject changeSubject = getChangeSubject(change);
-        return entity.equals(changeSubject) || entity.getIRI().equals(changeSubject);
-    }
-
-    private OWLObject getChangeSubject(AxiomChangeData change) {
-        AxiomSubjectProvider subjectProvider = new AxiomSubjectProvider();
-
-        return subjectProvider.getSubject(change.getAxiom());
-    }
 
     public String getHighLevelDescription(final OWLAPIProject project) {
         return getHighLevelDescription(project, null);
     }
-
-//    public List<OWLOntologyChangeRecord> getChanges() {
-//        return changes;
-//    }
 
     public Iterator<OWLOntologyChangeRecord> iterator() {
         return changes.iterator();
