649b386beba2f7d92ebfe8afd15edee8a460ffbe
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-27 08:57:57 +0000

    Tweaked diff line element style
    - Wrap line element
    - Made source document rendering a bit lighter

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/diff/DiffElementRenderer.java
@@ -0,0 +1,9 @@
+package edu.stanford.bmir.protege.web.server.diff;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 26/02/15
+ */
+public class DiffElementRenderer {
+}
