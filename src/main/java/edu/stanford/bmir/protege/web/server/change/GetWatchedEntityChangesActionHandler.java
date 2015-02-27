fd4c57ad7e98421e41565cda5a4ef00ec768c736
Matthew Horridge <matthew.horridge@stanford.edu>
2015-02-27 12:04:09 +0000

    Added handler for getting changes for watches
    - GetWatchedEntityChangesActionHandler
    - Tests
    - Added to handlers module

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/change/GetWatchedEntityChangesActionHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.bmir.protege.web.server.change;
+
+import com.google.common.collect.ImmutableList;
+import edu.stanford.bmir.protege.web.server.dispatch.AbstractHasProjectActionHandler;
+import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
+import edu.stanford.bmir.protege.web.server.dispatch.RequestContext;
+import edu.stanford.bmir.protege.web.server.dispatch.RequestValidator;
+import edu.stanford.bmir.protege.web.server.dispatch.validators.UserHasProjectReadPermissionValidator;
+import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProject;
+import edu.stanford.bmir.protege.web.shared.change.GetWatchedEntityChangesAction;
+import edu.stanford.bmir.protege.web.shared.change.GetWatchedEntityChangesResult;
+import edu.stanford.bmir.protege.web.shared.change.ProjectChange;
+import edu.stanford.bmir.protege.web.shared.watches.Watch;
+
+import java.util.Set;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 27/02/15
+ */
+public class GetWatchedEntityChangesActionHandler extends AbstractHasProjectActionHandler<GetWatchedEntityChangesAction, GetWatchedEntityChangesResult> {
+
+    @Override
+    protected RequestValidator<GetWatchedEntityChangesAction> getAdditionalRequestValidator(GetWatchedEntityChangesAction action, RequestContext requestContext) {
+        return UserHasProjectReadPermissionValidator.get();
+    }
+
+    @Override
+    protected GetWatchedEntityChangesResult execute(GetWatchedEntityChangesAction action, OWLAPIProject project, ExecutionContext executionContext) {
+        Set<Watch<?>> watches = project.getWatchManager().getWatches(action.getUserId());
+        ImmutableList<ProjectChange> changes = project.getChangeManager().getProjectChangesForWatches(watches);
+        return new GetWatchedEntityChangesResult(changes);
+    }
+
+    @Override
+    public Class<GetWatchedEntityChangesAction> getActionClass() {
+        return GetWatchedEntityChangesAction.class;
+    }
+}
