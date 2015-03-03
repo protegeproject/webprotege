54ec651054a883679f49d1cc66f1f449cf12259c
Matthew Horridge <matthew.horridge@stanford.edu>
2015-03-02 17:34:39 -0800

    Extracted computing changes from diffs

--- src/main/java/edu/stanford/bmir/protege/web/server/merge/MergeUploadedProjectActionHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/merge/MergeUploadedProjectActionHandler.java
@@ -1,11 +1,13 @@
 package edu.stanford.bmir.protege.web.server.merge;
 
+import com.google.common.collect.ImmutableSet;
 import edu.stanford.bmir.protege.web.client.dispatch.ActionExecutionException;
 import edu.stanford.bmir.protege.web.client.rpc.data.DocumentId;
 import edu.stanford.bmir.protege.web.server.change.ChangeGenerationContext;
 import edu.stanford.bmir.protege.web.server.change.ChangeListGenerator;
 import edu.stanford.bmir.protege.web.server.change.FixedMessageChangeDescriptionGenerator;
 import edu.stanford.bmir.protege.web.server.change.OntologyChangeList;
+import edu.stanford.bmir.protege.web.server.diff.OntologyDiff2OntologyChanges;
 import edu.stanford.bmir.protege.web.server.dispatch.AbstractHasProjectActionHandler;
 import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
 import edu.stanford.bmir.protege.web.server.dispatch.RequestContext;
@@ -16,12 +18,17 @@ import edu.stanford.bmir.protege.web.server.owlapi.*;
 import edu.stanford.bmir.protege.web.server.owlapi.manager.WebProtegeOWLManager;
 import edu.stanford.bmir.protege.web.server.util.TempFileFactoryImpl;
 import edu.stanford.bmir.protege.web.server.util.ZipInputStreamChecker;
+import edu.stanford.bmir.protege.web.shared.merge.Diff;
 import edu.stanford.bmir.protege.web.shared.merge.MergeUploadedProjectAction;
 import edu.stanford.bmir.protege.web.shared.merge.MergeUploadedProjectResult;
+import edu.stanford.bmir.protege.web.shared.merge.OntologyDiff;
 import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Matthew Horridge
@@ -41,31 +48,32 @@ public class MergeUploadedProjectActionHandler extends AbstractHasProjectActionH
             DocumentId documentId = action.getUploadedDocumentId();
             final OWLOntology uploadedRootOntology = loadUploadedOntology(documentId);
             final OWLOntology projectRootOntology = project.getRootOntology();
-            if(isUploadedOntologyMergableIntoRootOntology(uploadedRootOntology, projectRootOntology)) {
-                generateAndApplyChanges(action.getCommitMessage(), project, projectRootOntology, uploadedRootOntology, executionContext);
+            ModifiedProjectOntologiesCalculator calculator = new ModifiedProjectOntologiesCalculator(
+                    ImmutableSet.copyOf(projectRootOntology.getImportsClosure()),
+                    ImmutableSet.copyOf(uploadedRootOntology.getImportsClosure()),
+                    new OntologyDiffCalculator(new AnnotationDiffCalculator(), new AxiomDiffCalculator()));
+
+            Set<OntologyDiff> ontologyDiffSet = calculator.getModifiedOntologyDiffs();
+            List<OWLOntologyChange> changeList = new ArrayList<>();
+            HasGetOntologyById man = project.getRootOntology().getOWLOntologyManager();
+            for(OntologyDiff diff : ontologyDiffSet) {
+                OntologyDiff2OntologyChanges diff2Changes = new OntologyDiff2OntologyChanges();
+                List<OWLOntologyChange> changes = diff2Changes.getOntologyChangesFromDiff(diff, man);
+                changeList.addAll(changes);
             }
+            applyChanges(action.getCommitMessage(), project, changeList, executionContext);
         } catch (IOException | OWLOntologyCreationException e) {
             throw new ActionExecutionException(e);
         }
         return new MergeUploadedProjectResult();
     }
 
-    private void generateAndApplyChanges(String commitMessage, OWLAPIProject project, final OWLOntology projectRootOntology, final OWLOntology uploadedRootOntology, ExecutionContext executionContext) {
+    private void applyChanges(String commitMessage, OWLAPIProject project, final List<OWLOntologyChange> changes, ExecutionContext executionContext) {
         project.applyChanges(executionContext.getUserId(), new ChangeListGenerator<Void>() {
             @Override
             public OntologyChangeList<Void> generateChanges(OWLAPIProject project, ChangeGenerationContext context) {
                 OntologyChangeList.Builder<Void> builder = OntologyChangeList.builder();
-
-                for(OWLAxiom ax : uploadedRootOntology.getAxioms()) {
-                    if(!projectRootOntology.containsAxiom(ax)) {
-                        builder.addAxiom(projectRootOntology, ax);
-                    }
-                }
-                for(OWLAxiom ax : projectRootOntology.getAxioms()) {
-                    if(!uploadedRootOntology.containsAxiom(ax)) {
-                        builder.removeAxiom(projectRootOntology, ax);
-                    }
-                }
+                builder.addAll(changes);
                 return builder.build();
             }
 
@@ -76,9 +84,7 @@ public class MergeUploadedProjectActionHandler extends AbstractHasProjectActionH
         }, new FixedMessageChangeDescriptionGenerator<Void>(commitMessage + " [Merged from external edit]"));
     }
 
-    private boolean isUploadedOntologyMergableIntoRootOntology(OWLOntology uploadedRootOntology, OWLOntology projectRootOntology) {
-        return projectRootOntology.getOntologyID().equals(uploadedRootOntology.getOntologyID());
-    }
+
 
     @Override
     public Class<MergeUploadedProjectAction> getActionClass() {
