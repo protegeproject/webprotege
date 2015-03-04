f01183afb41803da72669c90cd60a9696c437604
Matthew Horridge <matthew.horridge@stanford.edu>
2015-03-04 09:27:35 -0800

    Cache missing projects

--- src/main/java/edu/stanford/bmir/protege/web/server/metaproject/ProjectExistsFilterImpl.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/metaproject/ProjectExistsFilterImpl.java
@@ -2,6 +2,12 @@ package edu.stanford.bmir.protege.web.server.metaproject;
 
 import edu.stanford.bmir.protege.web.server.owlapi.OWLAPIProjectDocumentStore;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import edu.stanford.smi.protege.server.metaproject.MetaProject;
+import edu.stanford.smi.protege.server.metaproject.ProjectInstance;
+
+import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Matthew Horridge
@@ -10,9 +16,30 @@ import edu.stanford.bmir.protege.web.shared.project.ProjectId;
  */
 public class ProjectExistsFilterImpl implements ProjectExistsFilter {
 
+    private MetaProject metaProject;
+
+    private final Set<ProjectId> missingProjects = new HashSet<>();
+
+
+    @Inject
+    public ProjectExistsFilterImpl(MetaProject metaProject) {
+        this.metaProject = metaProject;
+        findMissingProjects();
+    }
+
+    private void findMissingProjects() {
+        for(ProjectInstance project : metaProject.getProjects()) {
+            if(ProjectId.isWelFormedProjectId(project.getName())) {
+                ProjectId projectId = ProjectId.get(project.getName());
+                if(!OWLAPIProjectDocumentStore.getProjectDocumentStore(projectId).exists()) {
+                    missingProjects.add(projectId);
+                }
+            }
+        }
+    }
+
     @Override
     public boolean isProjectPresent(ProjectId projectId) {
-        OWLAPIProjectDocumentStore ds = OWLAPIProjectDocumentStore.getProjectDocumentStore(projectId);
-        return ds.exists();
+        return !missingProjects.contains(projectId);
     }
 }
