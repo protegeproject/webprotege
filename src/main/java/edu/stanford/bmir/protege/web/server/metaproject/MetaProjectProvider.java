5ddf51675ea9cb6c854a6d57f9fe8b008002ccc3
Matthew Horridge <matthew.horridge@stanford.edu>
2015-04-16 11:31:38 -0700

    Fixes #259
    This was caused by a change in the way that dependencies get injected
    into WebProtege, with the CheckMetaProjectExists task being run after
    the injector tried to inject the metaproject.
    
    - Changed the Provider for the metaproject so that it creates it if it
    does not exist.
    - Moved the metaproject into the resources directory so that it gets
    copied to the class path.  This way the ServletContext is not needed.

--- src/main/java/edu/stanford/bmir/protege/web/server/metaproject/MetaProjectProvider.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/metaproject/MetaProjectProvider.java
@@ -1,12 +1,19 @@
 package edu.stanford.bmir.protege.web.server.metaproject;
 
 import com.google.inject.Provider;
+import edu.stanford.bmir.protege.web.server.init.WebProtegeConfigurationException;
 import edu.stanford.smi.protege.server.metaproject.MetaProject;
 import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl;
+import org.apache.commons.io.FileUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Logger;
 
 /**
  * Matthew Horridge
@@ -16,15 +23,43 @@ import java.net.URI;
 @Singleton
 public class MetaProjectProvider implements Provider<MetaProject> {
 
+    private static final String DEFAULT_PROJECTS_DIRECTORY_NAME = "default-projects";
+
     private MetaProjectImpl metaProject;
 
+    private Logger logger;
+
     @Inject
-    public MetaProjectProvider(@MetaProjectURI URI metaProjectURI) {
+    public MetaProjectProvider(@MetaProjectURI URI metaProjectURI, Logger logger) {
+        this.logger = logger;
+        File metaProjectFile = new File(metaProjectURI);
+        if(!metaProjectFile.exists()) {
+            createFreshMetaProject(metaProjectFile);
+        }
         metaProject = new MetaProjectImpl(metaProjectURI);
     }
 
     @Override
     public MetaProject get() {
         return metaProject;
+    }
+
+    private void createFreshMetaProject(File metaProjectFile) {
+        try {
+            logger.info("The meta-project does not exist.  Creating a fresh meta-project.");
+            URL defaultProjectsDirectoryURL = Thread.currentThread()
+                    .getContextClassLoader().getResource(DEFAULT_PROJECTS_DIRECTORY_NAME);
+            if (defaultProjectsDirectoryURL == null) {
+                throw new WebProtegeConfigurationException("Could not find default projects directory");
+            }
+            File templateProjectsDirectory = new File(defaultProjectsDirectoryURL.toURI());
+            File templateMetaProjectDirectory = new File(templateProjectsDirectory, "metaproject");
+            final File metaProjectDirectory = metaProjectFile.getParentFile();
+            metaProjectDirectory.mkdirs();
+            FileUtils.copyDirectory(templateMetaProjectDirectory, metaProjectDirectory);
+            logger.info("Meta-project created at " + metaProjectDirectory);
+        } catch (IOException | URISyntaxException e) {
+            throw new WebProtegeConfigurationException("There was a problem initializing the metaproject.  Details: " + e.getMessage());
+        }
     }
 }
