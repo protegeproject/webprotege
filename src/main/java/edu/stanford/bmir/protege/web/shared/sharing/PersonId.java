68ec6ade0d785815e38f67e2085dadaf908792b6
Matthew Horridge <matthew.horridge@stanford.edu>
2015-05-11 12:20:11 -0700

    Added PersonId and tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/sharing/PersonId.java
@@ -0,0 +1,56 @@
+package edu.stanford.bmir.protege.web.shared.sharing;
+
+import com.google.common.base.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 11/05/15
+ *
+ * Identifies a Person in a sharing setting.  The person may or may not be a user in webprotege.
+ */
+public class PersonId {
+
+    private String id;
+
+    /**
+     * For serialization purposes only
+     */
+    private PersonId() {
+    }
+
+    public PersonId(String id) {
+        this.id = checkNotNull(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper("PersonId")
+                .addValue(id)
+                .toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof PersonId)) {
+            return false;
+        }
+        PersonId other = (PersonId) obj;
+        return this.id.equals(other.id);
+    }
+}
