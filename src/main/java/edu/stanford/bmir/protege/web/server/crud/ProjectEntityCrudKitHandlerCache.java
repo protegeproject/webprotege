90ea0bbaff709c7e6d9331bd078cc10eb142abf7
Matthew Horridge <matthew.horridge@stanford.edu>
2015-05-14 10:46:42 -0700

    Changes to inject repositories
    Added a Guice module that binds repositories as singleton instances.
    Removed singleton repository managers.
    Inject the repository into the entity CRUD kit handler cache.

--- src/main/java/edu/stanford/bmir/protege/web/server/crud/ProjectEntityCrudKitHandlerCache.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/crud/ProjectEntityCrudKitHandlerCache.java
@@ -2,12 +2,13 @@ package edu.stanford.bmir.protege.web.server.crud;
 
 import edu.stanford.bmir.protege.web.server.crud.persistence.ProjectEntityCrudKitSettings;
 import edu.stanford.bmir.protege.web.server.crud.persistence.ProjectEntityCrudKitSettingsRepository;
-import edu.stanford.bmir.protege.web.server.crud.persistence.ProjectEntityCrudKitSettingsRepositoryManager;
 import edu.stanford.bmir.protege.web.shared.crud.EntityCrudKitPrefixSettings;
 import edu.stanford.bmir.protege.web.shared.crud.EntityCrudKitSettings;
 import edu.stanford.bmir.protege.web.shared.crud.EntityCrudKitSuffixSettings;
 import edu.stanford.bmir.protege.web.shared.crud.uuid.UUIDSuffixSettings;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Author: Matthew Horridge<br>
@@ -17,12 +18,15 @@ import edu.stanford.bmir.protege.web.shared.project.ProjectId;
  */
 public class ProjectEntityCrudKitHandlerCache {
 
-    private ProjectId projectId;
+    private final ProjectId projectId;
+
+    private final ProjectEntityCrudKitSettingsRepository repository;
 
     private EntityCrudKitHandler<?, ?> cachedHandler;
 
-    public ProjectEntityCrudKitHandlerCache(ProjectId projectId) {
-        this.projectId = projectId;
+    public ProjectEntityCrudKitHandlerCache(ProjectEntityCrudKitSettingsRepository repository, ProjectId projectId) {
+        this.repository = checkNotNull(repository);
+        this.projectId = checkNotNull(projectId);
     }
 
     /**
@@ -38,7 +42,6 @@ public class ProjectEntityCrudKitHandlerCache {
     }
 
     private EntityCrudKitSettings<?> getCurrentSettings() {
-        ProjectEntityCrudKitSettingsRepository repository = ProjectEntityCrudKitSettingsRepositoryManager.getRepository();
         ProjectEntityCrudKitSettings projectSettings = repository.findOne(projectId);
         if (projectSettings == null) {
             projectSettings = new ProjectEntityCrudKitSettings(projectId, getDefaultSettings());
