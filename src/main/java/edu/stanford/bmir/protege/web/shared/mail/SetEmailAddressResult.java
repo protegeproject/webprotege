20464df9331123b07c591acfb1b05eb90e1b4b6d
Matthew Horridge <matthew.horridge@stanford.edu>
2015-09-23 11:28:41 -0700

    Closes #297

--- src/main/java/edu/stanford/bmir/protege/web/shared/mail/SetEmailAddressResult.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/mail/SetEmailAddressResult.java
@@ -1,6 +1,10 @@
 package edu.stanford.bmir.protege.web.shared.mail;
 
+import com.google.common.base.Objects;
 import edu.stanford.bmir.protege.web.shared.dispatch.Result;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Author: Matthew Horridge<br>
@@ -10,4 +14,58 @@ import edu.stanford.bmir.protege.web.shared.dispatch.Result;
  */
 public class SetEmailAddressResult implements Result {
 
+    public static enum Result {
+        /**
+         * The address was changed.
+         */
+        ADDRESS_CHANGED,
+        /**
+         * The user and the address are the same, therefore the address has not been changed.
+         */
+        ADDRESS_UNCHANGED,
+        /**
+         * A different user with the specified address exists and the address was not changed.
+         */
+        ADDRESS_ALREADY_EXISTS
+    }
+
+    private Result result;
+
+    /**
+     * For serialization only
+     */
+    private SetEmailAddressResult() {
+    }
+
+    public SetEmailAddressResult(Result result) {
+        this.result = checkNotNull(result);
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(result);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof SetEmailAddressResult)) {
+            return false;
+        }
+        SetEmailAddressResult other = (SetEmailAddressResult) obj;
+        return this.result == other.result;
+    }
+
+    @Override
+    public String toString() {
+        return toStringHelper("SetEmailAddressResult")
+                .addValue(result)
+                .toString();
+    }
 }
