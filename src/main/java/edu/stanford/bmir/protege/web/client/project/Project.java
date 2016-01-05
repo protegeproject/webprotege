d58840dedf0d80d710c1e7980b9c1abc945d2f76
Matthew Horridge <matthew.horridge@stanford.edu>
2016-01-04 23:03:48 -0800

    Removed defunct LayoutManager

--- src/main/java/edu/stanford/bmir/protege/web/client/project/Project.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/project/Project.java
@@ -4,37 +4,21 @@ import com.google.web.bindery.event.shared.EventBus;
 import edu.stanford.bmir.protege.web.client.LoggedInUserProvider;
 import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
 import edu.stanford.bmir.protege.web.client.events.EventPollingManager;
-import edu.stanford.bmir.protege.web.client.permissions.ProjectPermissionManager;
 import edu.stanford.bmir.protege.web.client.rpc.data.layout.ProjectLayoutConfiguration;
-import edu.stanford.bmir.protege.web.client.ui.LayoutManager;
 import edu.stanford.bmir.protege.web.shared.HasDispose;
 import edu.stanford.bmir.protege.web.shared.HasProjectId;
-import edu.stanford.bmir.protege.web.shared.permissions.Permission;
-import edu.stanford.bmir.protege.web.shared.permissions.PermissionsSet;
 import edu.stanford.bmir.protege.web.shared.project.ProjectDetails;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
-import edu.stanford.bmir.protege.web.shared.user.UserId;
 
 import javax.inject.Inject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-/**
- * A project on the client side. A project has information about:
- * <ul>
- * <li>project data (owner, description, etc.)</li>
- * <li>ontology event manager</li>
- * <li>project configuration (layout, portlets configuration, etc.)</li>
- * </ul>
- * @author Tania Tudorache <tudorache@stanford.edu>
- */
 public class Project implements HasProjectId, HasDispose {
 
     private final ProjectDetails projectDetails;
 
     private ProjectLayoutConfiguration projectLayoutConfiguration;
-
-    private LayoutManager layoutManager;
 
     private EventPollingManager eventPollingManager;
 
@@ -46,7 +30,6 @@ public class Project implements HasProjectId, HasDispose {
     @Inject
     public Project(ProjectDetails projectDetails, EventBus eventBus, DispatchServiceManager dispatchServiceManager, LoggedInUserProvider loggedInUserProvider) {
         this.projectDetails = checkNotNull(projectDetails);
-        this.layoutManager = new LayoutManager(projectDetails.getProjectId(), new ProjectLayoutConfiguration());
         this.eventPollingManager = EventPollingManager.get(10 * 1000, projectDetails.getProjectId(), eventBus, dispatchServiceManager, loggedInUserProvider);
         eventPollingManager.start();
     }
@@ -69,10 +52,6 @@ public class Project implements HasProjectId, HasDispose {
 
     public ProjectLayoutConfiguration getProjectLayoutConfiguration() {
         return projectLayoutConfiguration;
-    }
-
-    public LayoutManager getLayoutManager() {
-        return layoutManager;
     }
 
     public void dispose() {
