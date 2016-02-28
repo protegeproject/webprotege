4d044791e93ae55560ab2e70767cc3458f67fd99
Matthew Horridge <matthew.horridge@stanford.edu>
2016-02-28 11:19:36 -0800

    Action and tests to set perspective

---
+++ src/main/java/edu/stanford/bmir/protege/web/shared/perspective/SetPerspectiveLayoutResult.java
@@ -0,0 +1,12 @@
+package edu.stanford.bmir.protege.web.shared.perspective;
+
+import edu.stanford.bmir.protege.web.shared.dispatch.Result;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 28/02/16
+ */
+public class SetPerspectiveLayoutResult implements Result {
+
+}
