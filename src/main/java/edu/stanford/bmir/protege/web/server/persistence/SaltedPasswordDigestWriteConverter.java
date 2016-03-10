54828ea623305b1d909ea0bd7a978cd68b23abe1
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-10 09:25:16 -0800

    Converters for salted password digest and test cases

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/persistence/SaltedPasswordDigestWriteConverter.java
@@ -0,0 +1,22 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import com.google.common.io.BaseEncoding;
+import edu.stanford.bmir.protege.web.shared.auth.SaltedPasswordDigest;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
+
+import java.util.Locale;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 09/03/16
+ */
+@WritingConverter
+public class SaltedPasswordDigestWriteConverter implements Converter<SaltedPasswordDigest, String> {
+
+    @Override
+    public String convert(SaltedPasswordDigest saltedPasswordDigest) {
+        return BaseEncoding.base16().encode(saltedPasswordDigest.getBytes()).toLowerCase(Locale.ENGLISH);
+    }
+}
---
+++ src/test/java/edu/stanford/bmir/protege/web/server/persistence/SaltedPasswordDigestReadConverterTestCase.java
@@ -0,0 +1,45 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import com.google.common.io.BaseEncoding;
+import edu.stanford.bmir.protege.web.shared.auth.SaltedPasswordDigest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Locale;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 09/03/16
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SaltedPasswordDigestReadConverterTestCase {
+
+    private byte [] bytes = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16};
+
+    private final String encodedString = "000102030405060708090a0b0d0e0f10";
+
+    private SaltedPasswordDigestReadConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new SaltedPasswordDigestReadConverter();
+    }
+
+    @Test
+    public void shouldConvertLowerCaseBase16StringToSaltedPasswordDigest() {
+        SaltedPasswordDigest digest = converter.convert(encodedString.toLowerCase(Locale.ENGLISH));
+        assertThat(digest.getBytes(), is(bytes));
+    }
+
+    @Test
+    public void shouldConvertUpperCaseBase16StringToSaltedPasswordDigest() {
+        SaltedPasswordDigest digest = converter.convert(encodedString.toUpperCase(Locale.ENGLISH));
+        assertThat(digest.getBytes(), is(bytes));
+    }
+}
