ed6b7dc2e7b4e989b1a3ae5121ef5c23d34c516b
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-10 09:22:11 -0800

    Added Letter Icons for Users

---
+++ src/main/java/edu/stanford/bmir/protege/web/client/user/UserIcon.java
@@ -0,0 +1,112 @@
+package edu.stanford.bmir.protege.web.client.user;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import edu.stanford.bmir.protege.web.shared.auth.Md5DigestAlgorithmProvider;
+import edu.stanford.bmir.protege.web.shared.auth.MessageDigestAlgorithm;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
+
+import java.math.BigInteger;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 09/03/16
+ */
+public class UserIcon {
+
+    private UserIcon() {
+    }
+
+    public static IsWidget get(UserId userId) {
+        String text = getText(userId);
+        String color = getColor(userId);
+        UserIconView icon = new UserIconView();
+        icon.setText(text);
+        icon.setColor(color);
+        return icon;
+    }
+
+    private static String getText(UserId userId) {
+        String result;
+        String [] initials = userId.getUserName().split("\\s|\\.");
+        if(initials.length == 1) {
+            result = getCharacter(initials[0]);
+        }
+        else {
+            result = getCharacter(initials[0]) + getCharacter(initials[initials.length - 1]);
+        }
+        return result.toUpperCase();
+    }
+
+    private static String getCharacter(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(Character.isLetter(ch)) {
+                return String.valueOf(ch);
+            }
+        }
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)) {
+                return String.valueOf(ch);
+            }
+        }
+        return s.substring(0, 1);
+    }
+
+    private static String getColor(UserId userId) {
+        float hue = getHue(userId.getUserName());
+        float sat = 0.4f;
+        float brt = 0.9f;
+        return hsvToRgb(hue, sat, brt);
+    }
+
+    private static float getHue(String name) {
+        MessageDigestAlgorithm alg = new Md5DigestAlgorithmProvider().get();
+        alg.update(name.getBytes());
+        byte[] hash = alg.computeDigest();
+        BigInteger bi = new BigInteger(hash);
+        double percentageHue = Math.abs(bi.longValue() * 1.0 / Long.MAX_VALUE);
+        // Bound the value so that it's not a Red colour
+        double minHue = 25;
+        double maxHue = 340;
+        return (float) ((minHue + percentageHue * (maxHue - minHue))  / 360.0);
+    }
+
+    private static String hsvToRgb(float hue, float saturation, float value) {
+
+        int h = (int)(hue * 6);
+        float f = hue * 6 - h;
+        float p = value * (1 - saturation);
+        float q = value * (1 - f * saturation);
+        float t = value * (1 - (1 - f) * saturation);
+
+        switch (h) {
+            case 0: return rgbToString(value, t, p);
+            case 1: return rgbToString(q, value, p);
+            case 2: return rgbToString(p, value, t);
+            case 3: return rgbToString(p, q, value);
+            case 4: return rgbToString(t, p, value);
+            case 5: return rgbToString(value, p, q);
+            default: throw new RuntimeException();
+        }
+    }
+
+    private static String rgbToString(float r, float g, float b) {
+        String rs = Integer.toHexString((int)(r * 256));
+        if(rs.length() != 2) {
+            rs = "0" + rs;
+        }
+        String gs = Integer.toHexString((int)(g * 256));
+        if(gs.length() != 2) {
+            gs = "0" + gs;
+        }
+        String bs = Integer.toHexString((int)(b * 256));
+        if(bs.length() != 2) {
+            bs = "0" + bs;
+        }
+
+        return "#" + rs + gs + bs;
+    }
+
+}
