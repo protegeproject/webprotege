075aef1a87cd6c2c09f43af2a539ce0289851d9b
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-10 09:28:22 -0800

    Initial UserRecordRepository interface

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/user/UserRecordRepository.java
@@ -0,0 +1,14 @@
+package edu.stanford.bmir.protege.web.server.user;
+
+import edu.stanford.bmir.protege.web.shared.user.UserDetails;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 09/03/16
+ */
+public interface UserRecordRepository extends CrudRepository<UserDetails, UserId> {
+
+}
