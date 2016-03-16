87d87729e22187db6382bc7e38f7857b2a40a03e
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-15 18:14:41 -0700

    Removed unused see package

--- src/main/java/edu/stanford/bmir/protege/web/client/sel/RootEntityChangedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.bmir.protege.web.client.sel;
-
-/**
- * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 03/10/2014
- */
-public interface RootEntityChangedEventHandler {
-
-    void handleRootEntityChanged(RootEntityChangedEvent event);
-}
