4cc0e3c6fea82eec3662d99d5dd2da049d122743
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-20 21:30:45 -0700

    Added a description generator for subclass of

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/change/matcher/SubClassOfAxiomMatcher.java
@@ -0,0 +1,34 @@
+package edu.stanford.bmir.protege.web.server.change.matcher;
+
+import com.google.inject.TypeLiteral;
+import edu.stanford.bmir.protege.web.server.owlapi.OWLObjectStringFormatter;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 20/03/16
+ */
+public class SubClassOfAxiomMatcher extends AbstractAxiomMatcher<OWLSubClassOfAxiom> {
+
+    private final OWLObjectStringFormatter formatter;
+
+    @Inject
+    public SubClassOfAxiomMatcher(OWLObjectStringFormatter formatter) {
+        super(new TypeLiteral<OWLSubClassOfAxiom>(){});
+        this.formatter = formatter;
+    }
+
+    @Override
+    protected Optional<String> getDescriptionForAddAxiomChange(OWLSubClassOfAxiom axiom) {
+        return formatter.format("Made %s a subclass of %s", axiom.getSubClass(), axiom.getSuperClass());
+    }
+
+    @Override
+    protected Optional<String> getDescriptionForRemoveAxiomChange(OWLSubClassOfAxiom axiom) {
+        return formatter.format("Removed %s as a subclass of %s", axiom.getSubClass(), axiom.getSuperClass());
+    }
+}
