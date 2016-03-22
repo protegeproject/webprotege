c5211213d1443825cd4ab3f31a72a8e860afcf27
Matthew Horridge <matthew.horridge@stanford.edu>
2016-03-22 10:55:45 -0700

    Fixed reset password handler

--- src/main/java/edu/stanford/bmir/protege/web/server/chgpwd/ResetPasswordActionHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/chgpwd/ResetPasswordActionHandler.java
@@ -1,29 +1,60 @@
 package edu.stanford.bmir.protege.web.server.chgpwd;
 
+import edu.stanford.bmir.protege.web.server.IdUtil;
+import edu.stanford.bmir.protege.web.server.auth.AuthenticationManager;
 import edu.stanford.bmir.protege.web.server.dispatch.ActionHandler;
 import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
 import edu.stanford.bmir.protege.web.server.dispatch.RequestContext;
 import edu.stanford.bmir.protege.web.server.dispatch.RequestValidator;
 import edu.stanford.bmir.protege.web.server.dispatch.validators.NullValidator;
 import edu.stanford.bmir.protege.web.server.logging.WebProtegeLogger;
+import edu.stanford.bmir.protege.web.server.user.UserDetailsManager;
+import edu.stanford.bmir.protege.web.shared.auth.PasswordDigestAlgorithm;
+import edu.stanford.bmir.protege.web.shared.auth.Salt;
+import edu.stanford.bmir.protege.web.shared.auth.SaltProvider;
+import edu.stanford.bmir.protege.web.shared.auth.SaltedPasswordDigest;
 import edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordAction;
 import edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordResult;
+import edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordResultCode;
+import edu.stanford.bmir.protege.web.shared.user.UserDetails;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
 
 import javax.inject.Inject;
+import java.util.Optional;
+
+import static edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordResultCode.*;
+import static edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordResultCode.INVALID_EMAIL_ADDRESS;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 01/10/2014
  */
 public class ResetPasswordActionHandler implements ActionHandler<ResetPasswordAction, ResetPasswordResult> {
 
-    private final WebProtegeLogger logger;
+
+    private final UserDetailsManager userDetailsManager;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final SaltProvider saltProvider;
+
+    private final PasswordDigestAlgorithm passwordDigestAlgorithm;
 
     private final ResetPasswordMailer mailer;
 
+    private final WebProtegeLogger logger;
+
+
     @Inject
-    public ResetPasswordActionHandler(
-            ResetPasswordMailer mailer,
-            WebProtegeLogger logger) {
+    public ResetPasswordActionHandler(UserDetailsManager userDetailsManager,
+                                      AuthenticationManager authenticationManager,
+                                      SaltProvider saltProvider,
+                                      PasswordDigestAlgorithm passwordDigestAlgorithm,
+                                      ResetPasswordMailer mailer,
+                                      WebProtegeLogger logger) {
+        this.userDetailsManager = userDetailsManager;
+        this.authenticationManager = authenticationManager;
+        this.saltProvider = saltProvider;
+        this.passwordDigestAlgorithm = passwordDigestAlgorithm;
         this.mailer = mailer;
         this.logger = logger;
     }
@@ -42,34 +73,39 @@ public class ResetPasswordActionHandler implements ActionHandler<ResetPasswordAc
     @Override
     public ResetPasswordResult execute(
             ResetPasswordAction action, ExecutionContext executionContext) {
-//        final String emailAddress = action.getResetPasswordData().getEmailAddress();
-//        try {
-//            Optional<User> user = userDetailsManager.getUserByUserIdOrEmail(emailAddress);
-//            if(!user.isPresent()) {
-//                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
-//            }
-//            if(user.get().getEmail() == null) {
-//                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
-//            }
-//            if(user.get().getEmail().compareToIgnoreCase(emailAddress) != 0) {
-//                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
-//            }
-//            String pwd = IdUtil.getBase62UUID();
-//            user.get().setPassword(pwd);
-//            mailer.sendEmail(executionContext.getUserId(), emailAddress, pwd);
-//            logger.info("The password for %s has been reset.  " +
-//                            "An email has been sent to %s that contains the new password.",
-//                    executionContext.getUserId().getUserName(),
-//                    emailAddress
-//            );
-//            return new ResetPasswordResult(SUCCESS);
-//        } catch (Exception e) {
-//            logger.info("Could not reset the user password " +
-//                                "associated with the email " +
-//                                "address %s.  The following " +
-//                                "error occurred: %s.", emailAddress, e.getMessage());
-//            return new ResetPasswordResult(INTERNAL_ERROR);
-//        }
-        throw new RuntimeException();
+        final String emailAddress = action.getResetPasswordData().getEmailAddress();
+        try {
+            Optional<UserId> userId = userDetailsManager.getUserByUserIdOrEmail(emailAddress);
+            if(!userId.isPresent()) {
+                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
+            }
+            Optional<UserDetails> userDetails = userDetailsManager.getUserDetails(userId.get());
+            if(!userDetails.isPresent()) {
+                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
+            }
+            if(!userDetails.get().getEmailAddress().isPresent()) {
+                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
+            }
+            if(!userDetails.get().getEmailAddress().get().equalsIgnoreCase(emailAddress)) {
+                return new ResetPasswordResult(INVALID_EMAIL_ADDRESS);
+            }
+            String pwd = IdUtil.getBase62UUID();
+            Salt salt = saltProvider.get();
+            SaltedPasswordDigest saltedPasswordDigest = passwordDigestAlgorithm.getDigestOfSaltedPassword(pwd, salt);
+            authenticationManager.setDigestedPassword(userId.get(), saltedPasswordDigest, salt);
+            mailer.sendEmail(executionContext.getUserId(), emailAddress, pwd);
+            logger.info("The password for %s has been reset.  " +
+                            "An email has been sent to %s that contains the new password.",
+                    executionContext.getUserId().getUserName(),
+                    emailAddress
+            );
+            return new ResetPasswordResult(SUCCESS);
+        } catch (Exception e) {
+            logger.info("Could not reset the user password " +
+                                "associated with the email " +
+                                "address %s.  The following " +
+                                "error occurred: %s.", emailAddress, e.getMessage());
+            return new ResetPasswordResult(INTERNAL_ERROR);
+        }
     }
 }
