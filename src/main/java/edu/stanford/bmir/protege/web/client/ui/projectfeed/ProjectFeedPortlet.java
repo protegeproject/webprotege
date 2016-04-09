1839e2484fee93f244e5a0d4f48071e1fba5ef07
Matthew Horridge <matthew.horridge@stanford.edu>
2016-04-09 13:34:10 -0700

    Some renaming to use presenter and view names for the project feed stuff

--- src/main/java/edu/stanford/bmir/protege/web/client/ui/projectfeed/ProjectFeedPortlet.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/ui/projectfeed/ProjectFeedPortlet.java
@@ -1,16 +1,10 @@
 package edu.stanford.bmir.protege.web.client.ui.projectfeed;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.web.bindery.event.shared.EventBus;
 import edu.stanford.bmir.protege.web.client.LoggedInUserProvider;
 import edu.stanford.bmir.protege.web.client.filter.FilterView;
 import edu.stanford.bmir.protege.web.client.filter.FilterViewImpl;
 import edu.stanford.bmir.protege.web.client.portlet.AbstractOWLEntityPortlet;
-import edu.stanford.bmir.protege.web.client.portlet.PortletAction;
-import edu.stanford.bmir.protege.web.client.portlet.PortletActionHandler;
-import edu.stanford.bmir.protege.web.shared.filter.Filter;
 import edu.stanford.bmir.protege.web.shared.filter.FilterId;
 import edu.stanford.bmir.protege.web.shared.filter.FilterSet;
 import edu.stanford.bmir.protege.web.shared.filter.FilterSetting;
@@ -18,7 +12,6 @@ import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import edu.stanford.bmir.protege.web.shared.selection.SelectionModel;
 
 import javax.inject.Inject;
-import java.util.List;
 
 /**
  * Author: Matthew Horridge<br>
@@ -34,15 +27,15 @@ public class ProjectFeedPortlet extends AbstractOWLEntityPortlet {
 
     private final LoggedInUserProvider loggedInUserProvider;
 
-    private final ProjectFeedBasePanel basePanel;
+    private final ProjectFeedPresenter presenter;
 
     @Inject
     public ProjectFeedPortlet(SelectionModel selectionModel, EventBus eventBus, ProjectId projectId, LoggedInUserProvider loggedInUserManager) {
         super(selectionModel, eventBus, projectId, loggedInUserManager);
         this.loggedInUserProvider = loggedInUserManager;
-        basePanel = new ProjectFeedBasePanel(getProjectId(), this, selectionModel);
+        presenter = new ProjectFeedPresenter(getProjectId(), this, selectionModel);
         setTitle("Project feed");
-        getContentHolder().setWidget(basePanel);
+        getContentHolder().setWidget(presenter.getView());
 
         FilterView filterView = new FilterViewImpl();
         filterView.addFilterGroup("Project feed settings");
@@ -55,10 +48,10 @@ public class ProjectFeedPortlet extends AbstractOWLEntityPortlet {
 
     private void applyFilters(FilterSet filterSet) {
         FilterSetting showMyActivity = filterSet.getFilterSetting(SHOW_MY_ACTIVITY_FILTER, FilterSetting.ON);
-        basePanel.setUserActivityVisible(loggedInUserProvider.getCurrentUserId(), showMyActivity == FilterSetting.ON);
+        presenter.setUserActivityVisible(loggedInUserProvider.getCurrentUserId(), showMyActivity == FilterSetting.ON);
 
         FilterSetting showProjectChanges = filterSet.getFilterSetting(SHOW_PROJECT_CHANGES_FILTER, FilterSetting.ON);
-        basePanel.setUserActivityVisible(loggedInUserProvider.getCurrentUserId(), showProjectChanges == FilterSetting.ON);
+        presenter.setUserActivityVisible(loggedInUserProvider.getCurrentUserId(), showProjectChanges == FilterSetting.ON);
 
     }
 
