99401cb9ecf9d993e7147290c1c57b5820f1b755
Matthew Horridge <matthew.horridge@stanford.edu>
2016-04-12 13:29:02 -0700

    Added drop support to image field editor

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/form/FormElementDescriptorSerializer.java
@@ -0,0 +1,14 @@
+package edu.stanford.bmir.protege.web.server.form;
+
+import com.google.gson.JsonSerializer;
+import edu.stanford.bmir.protege.web.shared.form.FormDescriptor;
+import edu.stanford.bmir.protege.web.shared.form.field.FormElementDescriptor;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 11/04/16
+ */
+public class FormElementDescriptorSerializer implements JsonSerializer<FormElementDescriptor> {
+
+}
