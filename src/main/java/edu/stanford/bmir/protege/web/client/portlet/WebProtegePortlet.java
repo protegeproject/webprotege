066349f724ccffdc65b38ae94d6e5c14f76765e9
Matthew Horridge <matthew.horridge@stanford.edu>
2016-04-18 14:46:34 -0700

    Added some documentation to WebProtegePortlet

--- src/main/java/edu/stanford/bmir/protege/web/client/portlet/WebProtegePortlet.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/portlet/WebProtegePortlet.java
@@ -6,6 +6,11 @@ import edu.stanford.bmir.protege.web.shared.HasDispose;
 import edu.stanford.protege.widgetmap.client.view.HasViewTitle;
 import edu.stanford.protege.widgetmap.client.view.HasViewTitleChangedHandlers;
 
+/**
+ * A WebProtegePortlet is basically a presenter (as in Model View Presenter) that contains the logic for displaying
+ * some part of a project in the WebProtege user interface.  The displayed content is typically tied to the selection
+ * with different content being displayed for different selected entities.
+ */
 public interface WebProtegePortlet extends IsWidget, HasDispose, HasViewTitleChangedHandlers, HasViewTitle, HasPortletActions {
 
     AcceptsOneWidget getContentHolder();
