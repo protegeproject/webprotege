883487a833c994ac66f7b8c393159c5d6ef5564d
Matthew Horridge <matthew.horridge@stanford.edu>
2016-04-22 17:30:08 -0700

    Tidying

--- src/main/java/edu/stanford/bmir/protege/web/shared/selection/EntitySelectionChangedHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/selection/EntitySelectionChangedHandler.java
@@ -1,13 +1,11 @@
 package edu.stanford.bmir.protege.web.shared.selection;
 
-import com.google.gwt.event.shared.EventHandler;
-
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 15/05/15
  */
-public interface EntitySelectionChangedHandler extends EventHandler {
+public interface EntitySelectionChangedHandler {
 
     void handleSelectionChanged(EntitySelectionChangedEvent event);
 
