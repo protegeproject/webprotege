9de07d986fe29c84c26dc4e5ed3a14a139c63d2e
Matthew Horridge <matthew.horridge@stanford.edu>
2016-04-26 17:24:36 -0700

    Added functionality to get portlet descriptiors from the generated portlet factory

--- src/main/java/edu/stanford/bmir/protege/web/client/portlet/PortletFactory.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/portlet/PortletFactory.java
@@ -3,12 +3,16 @@ package edu.stanford.bmir.protege.web.client.portlet;
 import com.google.common.base.Optional;
 import edu.stanford.bmir.protege.web.shared.PortletId;
 
+import java.util.List;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 18/04/16
  */
 public interface PortletFactory {
+
+    List<PortletDescriptor> getAvailablePortletDescriptors();
 
     Optional<WebProtegePortlet> createPortlet(PortletId portletId);
 }
