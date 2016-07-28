aa4940851bec53c4cc7ec62d5f374509dbed85fe
Matthew Horridge <matthew.horridge@stanford.edu>
2016-07-28 16:37:23 -0700

    OWLEntity read and write converters and tests
    - Added OWLEntityReadConverter
    - Added OWLEntityWriteConverter
    - Added tests

---
+++ src/main/java/edu/stanford/bmir/protege/web/server/persistence/OWLEntityReadConverter.java
@@ -0,0 +1,48 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import com.mongodb.DBObject;
+import org.semanticweb.owlapi.model.*;
+import org.springframework.core.convert.converter.Converter;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryInternalsImplNoCache;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 27 Jul 16
+ */
+public class OWLEntityReadConverter implements Converter<DBObject, OWLEntity> {
+
+    private OWLDataFactory dataFactory = new OWLDataFactoryImpl(new OWLDataFactoryInternalsImplNoCache(false));
+
+    @Override
+    public OWLEntity convert(DBObject object) {
+        Object typeObject = object.get("type");
+        if(typeObject == null) {
+            throw new IllegalArgumentException("Missing type property value");
+        }
+        Object iriObject = object.get("iri");
+        if(iriObject == null) {
+            throw new IllegalArgumentException("Missing iri property value");
+        }
+
+        String typeString = typeObject.toString();
+        IRI iri = IRI.create(iriObject.toString());
+        switch (typeString) {
+            case "Class":
+                return dataFactory.getOWLClass(iri);
+            case "ObjectProperty":
+                return dataFactory.getOWLObjectProperty(iri);
+            case "DataProperty":
+                return dataFactory.getOWLDataProperty(iri);
+            case "AnnotationProperty":
+                return dataFactory.getOWLAnnotationProperty(iri);
+            case "NamedIndividual":
+                return dataFactory.getOWLNamedIndividual(iri);
+            case "Datatype":
+                return dataFactory.getOWLDatatype(iri);
+            default:
+                throw new RuntimeException("Unknown entity type");
+        }
+    }
+}
