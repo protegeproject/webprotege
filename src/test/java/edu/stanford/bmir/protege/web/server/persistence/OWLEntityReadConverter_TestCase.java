079b0ed2a14a03339b717637a35b410e981c1b2b
Matthew Horridge <matthew.horridge@stanford.edu>
2016-07-28 16:37:23 -0700

    OWLEntity read and write converters and tests
    - Added OWLEntityReadConverter
    - Added OWLEntityWriteConverter
    - Added tests

---
+++ src/test/java/edu/stanford/bmir/protege/web/server/persistence/OWLEntityReadConverter_TestCase.java
@@ -0,0 +1,97 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import com.mongodb.DBObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.EntityType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 27 Jul 16
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OWLEntityReadConverter_TestCase {
+
+    private OWLEntityReadConverter converter;
+
+    private IRI iri = IRI.create("http://the.entity.iri");
+
+    @Mock
+    private DBObject dbObject;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new OWLEntityReadConverter();
+        when(dbObject.get("iri")).thenReturn(iri.toString());
+    }
+
+    @Test
+    public void shouldReadOWLClass() {
+        when(dbObject.get("type")).thenReturn(EntityType.CLASS.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLClass(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test
+    public void shouldReadOWLObjectProperty() {
+        when(dbObject.get("type")).thenReturn(EntityType.OBJECT_PROPERTY.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLObjectProperty(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test
+    public void shouldReadOWLDataProperty() {
+        when(dbObject.get("type")).thenReturn(EntityType.DATA_PROPERTY.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLDataProperty(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test
+    public void shouldReadOWLAnnotationProperty() {
+        when(dbObject.get("type")).thenReturn(EntityType.ANNOTATION_PROPERTY.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLAnnotationProperty(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test
+    public void shouldReadOWLNamedIndividual() {
+        when(dbObject.get("type")).thenReturn(EntityType.NAMED_INDIVIDUAL.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLNamedIndividual(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test
+    public void shouldReadOWLDatatype() {
+        when(dbObject.get("type")).thenReturn(EntityType.DATATYPE.getName());
+        OWLEntity entity = converter.convert(dbObject);
+        assertThat(entity.isOWLDatatype(), is(true));
+        assertThat(entity.getIRI(), is(iri));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionOnMissingIRIPropertyValue() {
+        when(dbObject.get("type")).thenReturn(EntityType.CLASS.getName());
+        when(dbObject.get("iri")).thenReturn(null);
+        converter.convert(dbObject);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionOnMissingTypePropertyValue() {
+        converter.convert(dbObject);
+    }
+}
---
+++ src/test/java/edu/stanford/bmir/protege/web/server/persistence/OWLEntityWriteConverter_TestCase.java
@@ -0,0 +1,91 @@
+package edu.stanford.bmir.protege.web.server.persistence;
+
+import com.mongodb.DBObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.EntityType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 27 Jul 16
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OWLEntityWriteConverter_TestCase {
+
+    private OWLEntityWriteConverter converter;
+
+    @Mock
+    private OWLEntity entity;
+
+    private IRI iri = IRI.create("http://the.entity.iri");
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new OWLEntityWriteConverter();
+        when(entity.getIRI()).thenReturn(iri);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLClass() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.CLASS);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("Class"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLObjectProperty() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.OBJECT_PROPERTY);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("ObjectProperty"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLDataProperty() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.DATA_PROPERTY);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("DataProperty"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLAnnotationProperty() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.ANNOTATION_PROPERTY);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("AnnotationProperty"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLNamedIndividual() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.NAMED_INDIVIDUAL);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("NamedIndividual"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldWriteOWLDatatype() {
+        when(entity.getEntityType()).thenReturn((EntityType) EntityType.DATATYPE);
+        DBObject dbObject = converter.convert(entity);
+        assertThat(dbObject.get("type"), is("Datatype"));
+        assertThat(dbObject.get("iri"), is(iri.toString()));
+    }
+}
