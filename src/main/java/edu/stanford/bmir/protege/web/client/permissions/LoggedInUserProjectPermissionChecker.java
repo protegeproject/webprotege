71444607f77a60e8d37f542a7e8081ae5dd47643
Matthew Horridge <matthew.horridge@stanford.edu>
2017-02-28 10:20:46 -0800

    Added docs to LoggedInUserProjectPermissionChecker

--- src/main/java/edu/stanford/bmir/protege/web/client/permissions/LoggedInUserProjectPermissionChecker.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/permissions/LoggedInUserProjectPermissionChecker.java
@@ -1,9 +1,11 @@
 package edu.stanford.bmir.protege.web.client.permissions;
 
 import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceCallback;
+import edu.stanford.bmir.protege.web.client.project.ActiveProjectManager;
 import edu.stanford.bmir.protege.web.shared.access.ActionId;
 import edu.stanford.bmir.protege.web.shared.access.BuiltInAction;
 
+import javax.annotation.Nonnull;
 import java.util.function.Consumer;
 
 /**
@@ -13,9 +15,35 @@ import java.util.function.Consumer;
  */
 public interface LoggedInUserProjectPermissionChecker {
 
-    void hasPermission(ActionId actionId, DispatchServiceCallback<Boolean> callback);
+    /**
+     * Checks that the logged in user has permission to execute the specified
+     * action on the current active project.  The current active project is defined by the
+     * {@link ActiveProjectManager}.
+     * @param actionId The {@link ActionId} to test for execute permission.
+     * @param callback A callback for receiving the result of the query.
+     */
+    void hasPermission(@Nonnull ActionId actionId,
+                       @Nonnull DispatchServiceCallback<Boolean> callback);
 
-    void hasPermission(ActionId action, Consumer<Boolean> callback);
+    /**
+     * Checks that the logged in user has permission to execute the specified
+     * action on the current active project.  The current active project is defined by the
+     * {@link ActiveProjectManager}.
+     * @param actionId The {@link ActionId} to test for execute permission.
+     * @param callback A callback for receiving the result of the query.
+     */
+    void hasPermission(@Nonnull ActionId actionId,
+                       @Nonnull Consumer<Boolean> callback);
 
-    void hasPermission(BuiltInAction action, Consumer<Boolean> callback);
+    /**
+     * Checks that the logged in user has the specified permission to execute the specified
+     * action on the current active project.  The current active project is defined by the
+     * {@link ActiveProjectManager}.
+     * @param builtInAction The {@link BuiltInAction} to test for execute permission.  This
+     *                      is a convenience function that calls the function with the
+     *                      {@link ActionId}.
+     * @param callback A callback for receiving the result of the query.
+     */
+    void hasPermission(@Nonnull BuiltInAction builtInAction,
+                       @Nonnull Consumer<Boolean> callback);
 }
