e7928c2a3c22a0f44f54fbba427a318555fec162
Matthew Horridge <matthew.horridge@stanford.edu>
2017-02-28 10:31:39 -0800

    Tidy of PermissionManager

--- src/main/java/edu/stanford/bmir/protege/web/client/permissions/PermissionManager.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/permissions/PermissionManager.java
@@ -20,6 +20,7 @@ import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
 import edu.stanford.bmir.protege.web.shared.user.UserIdProjectIdKey;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Optional;
@@ -43,7 +44,7 @@ public class PermissionManager implements HasDispose {
 
     private final LoggedInUserProvider loggedInUserProvider;
 
-    private final Multimap<UserIdProjectIdKey, ActionId> actionCache = HashMultimap.create();
+    private final Multimap<UserIdProjectIdKey, ActionId> permittedActionCache = HashMultimap.create();
 
     @Inject
     public PermissionManager(EventBus eventBus, DispatchServiceManager dispatchServiceManager, ActiveProjectManager activeProjectManager, LoggedInUserProvider loggedInUserProvider) {
@@ -60,38 +61,44 @@ public class PermissionManager implements HasDispose {
      * current project on the event bus.
      */
     public void firePermissionsChanged() {
-        actionCache.clear();
+        permittedActionCache.clear();
         final UserId userId = loggedInUserProvider.getCurrentUserId();
         final Optional<ProjectId> projectId = activeProjectManager.getActiveProjectId();
         if (!projectId.isPresent()) {
             return;
         }
-        dispatchServiceManager.execute(new GetPermissionsAction(projectId.get(), userId), new DispatchServiceCallback<GetPermissionsResult>() {
-            @Override
-            public void handleSuccess(GetPermissionsResult result) {
-                UserIdProjectIdKey key = new UserIdProjectIdKey(userId, projectId.get());
-                actionCache.putAll(key, result.getAllowedActions());
-                GWT.log("[PermissionManager] Firing permissions changed for project: " + projectId);
-                eventBus.fireEventFromSource(new PermissionsChangedEvent(projectId.get()).asGWTEvent(), projectId.get());
-            }
+        final ProjectId theProjectId = projectId.get();
+        dispatchServiceManager.execute(new GetPermissionsAction(projectId.get(), userId), result -> {
+            UserIdProjectIdKey key = new UserIdProjectIdKey(userId, theProjectId);
+            permittedActionCache.putAll(key, result.getAllowedActions());
+            GWT.log("[PermissionManager] Firing permissions changed for project: " + projectId);
+            eventBus.fireEventFromSource(new PermissionsChangedEvent(theProjectId).asGWTEvent(), theProjectId);
         });
 
     }
 
-    public void hasPermissionForProject(UserId userId,
-                                        ActionId actionId,
-                                        ProjectId projectId,
-                                        DispatchServiceCallback<Boolean> callback) {
+    /**
+     * Determines if the specified user has permission to execute the specified action on the
+     * specified project.
+     * @param userId The {@link UserId} to test for.
+     * @param actionId The {@link ActionId} to test for.
+     * @param projectId The {@link ProjectId} to test for.
+     * @param callback A callback for receiving the result.
+     */
+    public void hasPermissionForProject(@Nonnull UserId userId,
+                                        @Nonnull ActionId actionId,
+                                        @Nonnull ProjectId projectId,
+                                        @Nonnull DispatchServiceCallback<Boolean> callback) {
         final UserIdProjectIdKey key = new UserIdProjectIdKey(userId, projectId);
-        if(actionCache.containsKey(key)) {
-            callback.onSuccess(actionCache.get(key).contains(actionId));
+        if(permittedActionCache.containsKey(key)) {
+            callback.onSuccess(permittedActionCache.get(key).contains(actionId));
             return;
         }
         dispatchServiceManager.execute(new GetPermissionsAction(projectId, userId),
                                        new DispatchServiceCallback<GetPermissionsResult>() {
                                            @Override
                                            public void handleSuccess(GetPermissionsResult result) {
-                                               actionCache.putAll(key, result.getAllowedActions());
+                                               permittedActionCache.putAll(key, result.getAllowedActions());
                                                callback.onSuccess(result.getAllowedActions().contains(actionId));
                                            }
 
