bb291ce96a861c4bd3303af35b4bc297c4f0914f
Matthew Horridge <matthew.horridge@stanford.edu>
2017-02-28 17:37:25 -0800

    Show progress monitor when loading projects

--- src/main/java/edu/stanford/bmir/protege/web/client/ui/projectmanager/LoadProjectRequestHandlerImpl.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/ui/projectmanager/LoadProjectRequestHandlerImpl.java
@@ -1,8 +1,13 @@
 package edu.stanford.bmir.protege.web.client.ui.projectmanager;
 
 import com.google.gwt.place.shared.PlaceController;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchService;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
+import edu.stanford.bmir.protege.web.client.dispatch.ProgressDisplay;
+import edu.stanford.bmir.protege.web.client.ui.library.progress.ProgressMonitor;
 import edu.stanford.bmir.protege.web.shared.perspective.PerspectiveId;
 import edu.stanford.bmir.protege.web.shared.place.ProjectViewPlace;
+import edu.stanford.bmir.protege.web.shared.project.LoadProjectAction;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 
 import javax.inject.Inject;
@@ -16,13 +21,21 @@ public class LoadProjectRequestHandlerImpl implements LoadProjectRequestHandler 
 
     private PlaceController placeController;
 
+    private DispatchServiceManager dispatchServiceManager;
+
     @Inject
-    public LoadProjectRequestHandlerImpl(PlaceController placeController) {
+    public LoadProjectRequestHandlerImpl(PlaceController placeController, DispatchServiceManager dispatchServiceManager) {
         this.placeController = placeController;
+        this.dispatchServiceManager = dispatchServiceManager;
     }
 
     @Override
     public void handleProjectLoadRequest(ProjectId projectId) {
-        placeController.goTo(ProjectViewPlace.builder(projectId, new PerspectiveId("Classes")).build());
+        ProgressMonitor.get().showProgressMonitor("Loading project", "Please wait");
+        dispatchServiceManager.execute(new LoadProjectAction(projectId), result -> {
+            placeController.goTo(ProjectViewPlace.builder(projectId, new PerspectiveId("Classes")).build());
+            ProgressMonitor.get().hideProgressMonitor();
+        });
+
     }
 }
