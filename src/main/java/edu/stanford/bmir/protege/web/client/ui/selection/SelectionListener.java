45d4064fc6694c897f2250e4288cd81bc78c55db
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-03 14:50:30 -0800

    Removed unused selectable package

--- src/main/java/edu/stanford/bmir/protege/web/client/ui/selection/SelectionListener.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.bmir.protege.web.client.ui.selection;
-
-/**
- * Listener interface for the generic {@link Selectable} interface.
- */
-public interface SelectionListener {
-
-    void selectionChanged(SelectionEvent event);
-}
