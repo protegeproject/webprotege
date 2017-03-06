d88316f06438298c41dd04c663f063ba354d481b
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-05 16:36:29 -0800

    Check for download permission before providing download

--- src/main/java/edu/stanford/bmir/protege/web/server/filedownload/FileDownloadServlet.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/filedownload/FileDownloadServlet.java
@@ -1,13 +1,18 @@
 package edu.stanford.bmir.protege.web.server.filedownload;
 
+import edu.stanford.bmir.protege.web.server.access.AccessManager;
+import edu.stanford.bmir.protege.web.server.access.ProjectResource;
+import edu.stanford.bmir.protege.web.server.access.Subject;
 import edu.stanford.bmir.protege.web.server.inject.ApplicationName;
 import edu.stanford.bmir.protege.web.server.project.Project;
-import edu.stanford.bmir.protege.web.server.project.ProjectManager;
 import edu.stanford.bmir.protege.web.server.project.ProjectDetailsManager;
+import edu.stanford.bmir.protege.web.server.project.ProjectManager;
 import edu.stanford.bmir.protege.web.server.session.WebProtegeSession;
 import edu.stanford.bmir.protege.web.server.session.WebProtegeSessionImpl;
+import edu.stanford.bmir.protege.web.shared.access.BuiltInAction;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import edu.stanford.bmir.protege.web.shared.revision.RevisionNumber;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -24,14 +29,17 @@ import java.io.IOException;
  * Bio-Medical Informatics Research Group<br>
  * Date: 06/06/2012
  * <p>
- *     A servlet which allows ontologies to be downloaded from WebProtege.  See {@link OWLAPIProjectDownloader} for
- *     the piece of machinery that actually does the processing of request parameters and the downloading.
+ * A servlet which allows ontologies to be downloaded from WebProtege.  See {@link OWLAPIProjectDownloader} for
+ * the piece of machinery that actually does the processing of request parameters and the downloading.
  * </p>
  */
 public class FileDownloadServlet extends HttpServlet {
 
     @Nonnull
     private final ProjectManager projectManager;
+
+    @Nonnull
+    private final AccessManager accessManager;
 
     @Nonnull
     private final ProjectDetailsManager projectDetailsManager;
@@ -43,23 +51,37 @@ public class FileDownloadServlet extends HttpServlet {
     public FileDownloadServlet(
             @Nonnull ProjectManager projectManager,
             @Nonnull ProjectDetailsManager projectDetailsManager,
+            @Nonnull AccessManager accessManager,
             @Nonnull @ApplicationName String applicationName) {
         this.projectManager = projectManager;
         this.projectDetailsManager = projectDetailsManager;
+        this.accessManager = accessManager;
         this.applicationName = applicationName;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebProtegeSession webProtegeSession = new WebProtegeSessionImpl(req.getSession());
+        UserId userId = webProtegeSession.getUserInSession();
         FileDownloadParameters downloadParameters = new FileDownloadParameters(req);
-        if(downloadParameters.isProjectDownload()) {
+        if (!accessManager.hasPermission(Subject.forUser(userId),
+                                         new ProjectResource(downloadParameters.getProjectId()),
+                                         BuiltInAction.DOWNLOAD_PROJECT.getActionId())) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        if (downloadParameters.isProjectDownload()) {
             ProjectId projectId = downloadParameters.getProjectId();
             RevisionNumber revisionNumber = downloadParameters.getRequestedRevision();
             DownloadFormat format = downloadParameters.getFormat();
             String displayName = projectDetailsManager.getProjectDetails(projectId).getDisplayName();
             WebProtegeSession session = new WebProtegeSessionImpl(req.getSession());
             Project project = projectManager.getProject(projectId, session.getUserInSession());
-            OWLAPIProjectDownloader downloader = new OWLAPIProjectDownloader(displayName, project, revisionNumber, format, applicationName);
+            OWLAPIProjectDownloader downloader = new OWLAPIProjectDownloader(displayName,
+                                                                             project,
+                                                                             revisionNumber,
+                                                                             format,
+                                                                             applicationName);
             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(resp.getOutputStream());
             downloader.writeProject(resp, bufferedOutputStream);
             bufferedOutputStream.flush();
