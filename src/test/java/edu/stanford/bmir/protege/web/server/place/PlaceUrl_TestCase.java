8a2131d6b5d8bd73a4403e43a42c923ef3649c35
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-11 09:05:38 -0800

    Improved PlaceUrl and tests

--- src/test/java/edu/stanford/bmir/protege/web/server/place/PlaceUrl_TestCase.java
+++ src/test/java/edu/stanford/bmir/protege/web/server/place/PlaceUrl_TestCase.java
@@ -2,10 +2,13 @@ package edu.stanford.bmir.protege.web.server.place;
 
 import edu.stanford.bmir.protege.web.server.perspective.EntityTypePerspectiveMapper;
 import edu.stanford.bmir.protege.web.shared.DataFactory;
+import edu.stanford.bmir.protege.web.shared.app.ApplicationScheme;
+import edu.stanford.bmir.protege.web.shared.perspective.PerspectiveId;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.semanticweb.owlapi.model.EntityType;
@@ -13,8 +16,10 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
@@ -27,53 +32,64 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class PlaceUrl_TestCase {
 
-    private PlaceUrl placeUrl;
+    public static final String THE_APPLICATION_HOST = "the.application.host";
 
-    private EntityTypePerspectiveMapper typeMapper;
+    public static final String THE_APPLICATION_PATH = "/the/application/path";
+
+    public static final String THE_APPLICATION_NAME = "the.application.name";
+
+    public static final String EXPECTED_URL_BASE = "https://the.application.host/the/application/path";
+
+    private PlaceUrl placeUrl;
 
     private ProjectId projectId = ProjectId.get(UUID.randomUUID().toString());
 
     private IRI entityIri = IRI.create("http://the.ontology/entity");
 
-
     private EntityType<OWLClass> entityType = EntityType.CLASS;
 
     private OWLEntity entity = DataFactory.getOWLEntity(entityType, entityIri);
 
+    @Mock
+    private EntityTypePerspectiveMapper typeMapper;
+
     @Before
     public void setUp() throws Exception {
-        typeMapper = new EntityTypePerspectiveMapper();
-        placeUrl = new PlaceUrl("the.application.host",
-                                "/the/application/path",
-                                "the.application.name", typeMapper);
+        when(typeMapper.getPerspectiveId(Matchers.any())).thenReturn(new PerspectiveId("TheClassPerspective"));
+        when(typeMapper.getDefaultPerspectiveId()).thenReturn(new PerspectiveId("TheDefaultPerspective"));
+        placeUrl = new PlaceUrl(ApplicationScheme.HTTPS,
+                                THE_APPLICATION_HOST,
+                                Optional.empty(),
+                                THE_APPLICATION_PATH,
+                                THE_APPLICATION_NAME, typeMapper);
     }
 
     @Test
     public void shouldBuildApplicationUrl() {
         String url = placeUrl.getApplicationUrl();
-        assertThat(url, is("http://the.application.host/the/application/path"));
+        assertThat(url, is(EXPECTED_URL_BASE));
     }
 
     @Test
     public void shouldBuildApplicationAnchor() {
         String anchor = placeUrl.getApplicationAnchor();
-        assertThat(anchor, is("<a href=\"the.application.name\">http://the.application.host/the/application/path</a>"));
+        assertThat(anchor, is("<a href=\"the.application.name\">" + EXPECTED_URL_BASE + "</a>"));
     }
 
     @Test
     public void shouldBuildProjectUrl() {
         String url = placeUrl.getProjectUrl(projectId);
-        assertThat(url, is("http://the.application.host/the/application/path#ProjectViewPlace:/projects/"
+        assertThat(url, is(EXPECTED_URL_BASE + "#ProjectViewPlace:/projects/"
                                    + projectId.getId()
-                                   + "/edit/Classes"));
+                                   + "/edit/TheDefaultPerspective"));
     }
 
     @Test
     public void shouldBuildEntityUrl() {
 
         String url = placeUrl.getEntityUrl(projectId, entity);
-        assertThat(url, is("http://the.application.host/the/application/path#ProjectViewPlace:/projects/"
+        assertThat(url, is(EXPECTED_URL_BASE + "#ProjectViewPlace:/projects/"
                                    + projectId.getId()
-                                   + "/edit/Classes?selection=Class(%3C" + entityIri.toString() + "%3E)"));
+                                   + "/edit/TheClassPerspective?selection=Class(%3C" + entityIri.toString() + "%3E)"));
     }
 }
