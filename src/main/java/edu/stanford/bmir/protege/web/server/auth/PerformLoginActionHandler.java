ab2657c8be4f3d841f2a6817edabcb22c7c12a56
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-12 10:28:52 -0700

    Record login time

--- src/main/java/edu/stanford/bmir/protege/web/server/auth/PerformLoginActionHandler.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/auth/PerformLoginActionHandler.java
@@ -6,6 +6,7 @@ import edu.stanford.bmir.protege.web.server.dispatch.RequestValidator;
 import edu.stanford.bmir.protege.web.server.dispatch.validators.NullValidator;
 import edu.stanford.bmir.protege.web.server.logging.WebProtegeLogger;
 import edu.stanford.bmir.protege.web.server.session.WebProtegeSession;
+import edu.stanford.bmir.protege.web.server.user.UserActivityManager;
 import edu.stanford.bmir.protege.web.shared.auth.AuthenticationResponse;
 import edu.stanford.bmir.protege.web.shared.auth.PerformLoginAction;
 import edu.stanford.bmir.protege.web.shared.auth.PerformLoginResult;
@@ -19,9 +20,13 @@ import javax.inject.Inject;
  */
 public class PerformLoginActionHandler extends AuthenticatedActionHandler<PerformLoginAction, PerformLoginResult> {
 
+    private final UserActivityManager activityManager;
+
     @Inject
-    public PerformLoginActionHandler(ChapSessionManager chapSessionManager, AuthenticationManager authenticationManager, ChapResponseChecker chapResponseChecker, WebProtegeLogger logger) {
+    public PerformLoginActionHandler(UserActivityManager activityManager,
+                                     ChapSessionManager chapSessionManager, AuthenticationManager authenticationManager, ChapResponseChecker chapResponseChecker, WebProtegeLogger logger) {
         super(chapSessionManager, authenticationManager, chapResponseChecker, logger);
+        this.activityManager = activityManager;
     }
 
     @Override
@@ -38,6 +43,7 @@ public class PerformLoginActionHandler extends AuthenticatedActionHandler<Perfor
     protected PerformLoginResult executeAuthenticatedAction(PerformLoginAction action, ExecutionContext executionContext) {
         WebProtegeSession session = executionContext.getSession();
         session.setUserInSession(action.getUserId());
+        activityManager.setLastLogin(action.getUserId(), System.currentTimeMillis());
         return new PerformLoginResult(AuthenticationResponse.SUCCESS);
     }
 
