361349628f33fa58c3e03f1969214e1f5a4921a7
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-19 13:13:24 -0700

    Add validation to placeholder text box

--- src/main/java/edu/stanford/bmir/protege/web/client/library/text/PlaceholderTextBox.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/library/text/PlaceholderTextBox.java
@@ -1,8 +1,12 @@
 package edu.stanford.bmir.protege.web.client.library.text;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.ui.TextBox;
 import edu.stanford.bmir.protege.web.client.library.common.HasPlaceholder;
+
+import javax.annotation.Nullable;
 
 /**
  * Author: Matthew Horridge<br>
@@ -14,11 +18,18 @@ public class PlaceholderTextBox extends TextBox implements HasPlaceholder {
 
     private static final String ATTRIBUTE_NAME = "placeholder";
 
+    public static final String INVALID_VALUE_BORDER_COLOR = "red";
+
+    @Nullable
+    private RegExp regExp = null;
+
     public PlaceholderTextBox() {
+        addValueChangeHandler(event -> doValidation());
     }
 
     public PlaceholderTextBox(Element element) {
         super(element);
+        addValueChangeHandler(event -> doValidation());
     }
 
     @Override
@@ -29,5 +40,32 @@ public class PlaceholderTextBox extends TextBox implements HasPlaceholder {
     @Override
     public void setPlaceholder(String placeholder) {
         getElement().setAttribute("placeholder", placeholder);
+    }
+
+    public void setValidation(String regexp) {
+        this.regExp = RegExp.compile(regexp);
+    }
+
+    private void doValidation() {
+        if(regExp != null) {
+            GWT.log("Performing validation");
+            regExp.setLastIndex(0);
+            if(regExp.test(getValue())) {
+                GWT.log("Value is valid");
+                removeErrorStyle();
+            }
+            else {
+                GWT.log("Value is not valid");
+                applyErrorStyle();
+            }
+        }
+    }
+
+    private void applyErrorStyle() {
+        getElement().getStyle().setBorderColor(INVALID_VALUE_BORDER_COLOR);
+    }
+
+    private void removeErrorStyle() {
+        getElement().getStyle().clearBorderColor();
     }
 }
