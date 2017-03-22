5468445c9d4e50cc704e13632201dbd98a9f6b0c
Matthew Horridge <matthew.horridge@stanford.edu>
2017-03-22 13:44:23 -0700

    Added specific portlet presenter for editing classes

--- src/main/java/edu/stanford/bmir/protege/web/client/editor/ClassEditorPortletPresenter.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/editor/ClassEditorPortletPresenter.java
@@ -6,16 +6,12 @@ import edu.stanford.bmir.protege.web.shared.event.WebProtegeEventBus;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import edu.stanford.bmir.protege.web.shared.selection.SelectionModel;
 import edu.stanford.webprotege.shared.annotations.Portlet;
-import org.semanticweb.owlapi.model.EntityType;
-import org.semanticweb.owlapi.model.OWLEntity;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
-import java.util.Optional;
-
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.semanticweb.owlapi.model.EntityType.NAMED_INDIVIDUAL;
+import static org.semanticweb.owlapi.model.EntityType.CLASS;
 
 /**
  * Matthew Horridge
@@ -23,25 +19,25 @@ import static org.semanticweb.owlapi.model.EntityType.NAMED_INDIVIDUAL;
  * 22 Mar 2017
  */
 @Portlet(
-        id = "portlet.individualeditor",
-        title = "Individual Editor",
-        tooltip = "Provides an editor that allows individual descriptions to be edited"
+        id = "portlet.classeditor",
+        title = "Class Editor",
+        tooltip = "Provides an editor that allows class descriptions to be edited"
 )
-public class NamedIndividualEditorPortletPresenter extends AbstractWebProtegePortletPresenter {
+public class ClassEditorPortletPresenter extends AbstractWebProtegePortletPresenter {
 
     private final EditorPortletPresenter editorPresenter;
 
     @Inject
-    public NamedIndividualEditorPortletPresenter(@Nonnull SelectionModel selectionModel,
-                                                 @Nonnull ProjectId projectId,
-                                                 @Nonnull EditorPortletPresenter editorPresenter) {
+    public ClassEditorPortletPresenter(@Nonnull SelectionModel selectionModel,
+                                       @Nonnull ProjectId projectId,
+                                       @Nonnull EditorPortletPresenter editorPresenter) {
         super(selectionModel, projectId);
         this.editorPresenter = checkNotNull(editorPresenter);
     }
 
     @Override
     public void startPortlet(PortletUi portletUi, WebProtegeEventBus eventBus) {
-        editorPresenter.setDisplayedTypes(NAMED_INDIVIDUAL);
+        editorPresenter.setDisplayedTypes(CLASS);
         editorPresenter.start(portletUi, eventBus);
     }
 }
