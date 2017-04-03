4314b1c075cb0c8fffb2dfdde65e1f4e49a3e6e9
Matthew Horridge <matthew.horridge@stanford.edu>
2017-04-03 15:39:17 -0700

    Implemented autocompletion for '@' user mentions.  We might want to consider restricting the list of possible completions.

---
+++ src/main/java/edu/stanford/bmir/protege/web/client/issues/CommentAutoCompleter.java
@@ -0,0 +1,95 @@
+package edu.stanford.bmir.protege.web.client.issues;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
+import edu.stanford.bmir.gwtcodemirror.client.AutoCompletionCallback;
+import edu.stanford.bmir.gwtcodemirror.client.AutoCompletionChoice;
+import edu.stanford.bmir.gwtcodemirror.client.AutoCompletionResult;
+import edu.stanford.bmir.gwtcodemirror.client.EditorPosition;
+import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
+import edu.stanford.bmir.protege.web.shared.itemlist.GetUserIdCompletionsAction;
+import edu.stanford.bmir.protege.web.shared.user.UserId;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 3 Apr 2017
+ */
+public class CommentAutoCompleter {
+
+    private static final int NAME_GROUP = 1;
+
+    private final RegExp nameRegExp = RegExp.compile("@([^ ]*)$" );
+
+    private final DispatchServiceManager dispatchServiceManager;
+
+    @Inject
+    public CommentAutoCompleter(@Nonnull DispatchServiceManager dispatchServiceManager) {
+        this.dispatchServiceManager = checkNotNull(dispatchServiceManager);
+    }
+
+    public void handleAutocomplete(@Nonnull String query,
+                                   @Nonnull EditorPosition caretPosition,
+                                   int caretPos,
+                                   @Nonnull AutoCompletionCallback callback) {
+        String upToCarent = query.substring(0, caretPos);
+        MatchResult result = nameRegExp.exec(upToCarent);
+        if (result == null) {
+            callback.completionsReady(new AutoCompletionResult());
+            return;
+        }
+        String matchedPartialName = result.getGroup(NAME_GROUP);
+
+        EditorPosition replaceTextFrom = new EditorPosition(
+                caretPosition.getLineNumber(),
+                caretPosition.getColumnNumber() - result
+                        .getGroup(0)
+                        .length());
+
+        dispatchServiceManager.execute(new GetUserIdCompletionsAction(matchedPartialName),
+                                       userIdsResult -> {
+                                           List<AutoCompletionChoice> suggestions = new ArrayList<>();
+                                           List<UserId> possibleUserIdCompletions = userIdsResult.getPossibleItemCompletions();
+                                           for (UserId userId : possibleUserIdCompletions) {
+                                               String userName = userId.getUserName();
+                                               String replacement = getReplacementStringFromUserName(userName);
+                                               EditorPosition replaceTextTo = new EditorPosition(
+                                                       caretPosition.getLineNumber(),
+                                                       caretPosition.getColumnNumber()
+                                               );
+                                               suggestions.add(
+                                                       new AutoCompletionChoice(replacement,
+                                                                                userName,
+                                                                                "" ,
+                                                                                replaceTextFrom,
+                                                                                replaceTextTo)
+                                               );
+                                           }
+                                           callback.completionsReady(new AutoCompletionResult(suggestions,
+                                                                                              replaceTextFrom));
+                                       });
+    }
+
+    /**
+     * Gets a replacement string that will generate a valid @mention for a user name.
+     * @param userName The user name.
+     * @return The replacement string.
+     */
+    private static String getReplacementStringFromUserName(@Nonnull String userName) {
+        if (userName.contains(" " )) {
+            return  "@{" + userName + "}";
+        }
+        else {
+            return  "@" + userName;
+        }
+    }
+}
--- src/main/java/edu/stanford/bmir/protege/web/client/issues/CommentEditorViewImpl.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/issues/CommentEditorViewImpl.java
@@ -2,15 +2,22 @@ package edu.stanford.bmir.protege.web.client.issues;
 
 import com.google.common.base.Optional;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Focusable;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.*;
+import edu.stanford.bmir.gwtcodemirror.client.*;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Matthew Horridge
@@ -19,34 +26,45 @@ import javax.inject.Inject;
  */
 public class CommentEditorViewImpl extends Composite implements CommentEditorView {
 
+    private final CommentAutoCompleter commentAutoCompleter;
+
     interface CommentEditorViewImplUiBinder extends UiBinder<HTMLPanel, CommentEditorViewImpl> {
 
     }
 
     private static CommentEditorViewImplUiBinder ourUiBinder = GWT.create(CommentEditorViewImplUiBinder.class);
 
-    @UiField
-    TextArea bodyField;
+    @UiField(provided = true)
+    GWTCodeMirror bodyField;
 
     @Inject
-    public CommentEditorViewImpl() {
+    public CommentEditorViewImpl(CommentAutoCompleter commentAutoCompleter) {
+        this.commentAutoCompleter = commentAutoCompleter;
+        bodyField = new GWTCodeMirror("gfm");
+        bodyField.setAutoCompletionHandler(this::handleAutocomplete);
+        bodyField.setLineNumbersVisible(false);
+        bodyField.addAutoCompleteCharacter('@');
         initWidget(ourUiBinder.createAndBindUi(this));
+    }
+
+    private void handleAutocomplete(String query, EditorPosition caretPosition, int caretPos, AutoCompletionCallback callback) {
+        commentAutoCompleter.handleAutocomplete(query, caretPosition, caretPos, callback);
     }
 
     @Override
     public void clear() {
-        bodyField.setText("");
+        bodyField.setValue("");
     }
 
     @Override
     public void setBody(@Nonnull String body) {
-        this.bodyField.setText(body);
+        Scheduler.get().scheduleDeferred(() -> bodyField.setValue(body));
     }
 
     @Nonnull
     @Override
     public String getBody() {
-        return bodyField.getText().trim();
+        return bodyField.getValue().trim();
     }
 
     @Override
