591d76a2a40a6dd389adcefdcf6dbcdfe1bc26a1
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-07 13:03:09 -0700

    Added tokeniser for ProjectSettingsPlace plus tests

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/projectsettings/ProjectSettingsPlaceTokenizer_TestCase.java
@@ -0,0 +1,61 @@
+package edu.stanford.bmir.protege.web.shared.projectsettings;
+
+import edu.stanford.bmir.protege.web.shared.place.ProjectSettingsPlace;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2017
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProjectSettingsPlaceTokenizer_TestCase {
+
+    private static final String PROJECT_ID = "12345678-1234-1234-1234-123456789abc";
+
+    private ProjectSettingsPlaceTokenizer tokenizer;
+
+    @Mock
+    private ProjectSettingsPlace place;
+
+    @Mock
+    private ProjectId projectId;
+
+    @Before
+    public void setUp() {
+        when(projectId.getId()).thenReturn(PROJECT_ID);
+        tokenizer = new ProjectSettingsPlaceTokenizer();
+        when(place.getProjectId()).thenReturn(projectId);
+    }
+
+    @Test
+    public void shouldTokenizePlace() {
+        String token = tokenizer.getToken(place);
+        assertThat(token, is("projects/"+PROJECT_ID+"/settings"));
+    }
+
+    @Test
+    public void shouldGetPlaceFromToken() {
+        ProjectSettingsPlace p = tokenizer.getPlace("projects/"+PROJECT_ID+"/settings");
+        assertThat(p, is(not(nullValue())));
+        assertThat(p.getProjectId().getId(), is(PROJECT_ID));
+    }
+
+    @Test
+    public void shouldNotGetPlaceFromMalformedProjectId() {
+        ProjectSettingsPlace p = tokenizer.getPlace("projects/MALFORMED/settings");
+        assertThat(p, is(nullValue()));
+    }
+}
