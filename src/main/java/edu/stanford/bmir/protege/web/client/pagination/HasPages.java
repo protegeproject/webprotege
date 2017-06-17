d4a1f9e8dd3116fc6da1c6cfeeb940132ae8bb79
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-17 14:57:16 -0700

    Extracted interface from PaginatorPresenter

---
+++ src/main/java/edu/stanford/bmir/protege/web/client/pagination/HasPages.java
@@ -0,0 +1,19 @@
+package edu.stanford.bmir.protege.web.client.pagination;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 17 Jun 2017
+ */
+public interface HasPages {
+
+    void setPageNumberChangedHandler(@Nonnull HasPagination.PageNumberChangedHandler pageNumberChangedHandler);
+
+    int getPageNumber();
+
+    void setPageNumber(int pageNumber);
+
+    void setPageCount(int pageCount);
+}
