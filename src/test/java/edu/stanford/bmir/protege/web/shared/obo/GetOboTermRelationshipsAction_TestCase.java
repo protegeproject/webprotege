6f4b014cb16c5bbdf60339230dabd93be7436180
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-24 09:55:31 -0700

    Tests for GetOboTermRelationshipsAction

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/obo/GetOboTermRelationshipsAction_TestCase.java
@@ -0,0 +1,92 @@
+
+package edu.stanford.bmir.protege.web.shared.obo;
+
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.OWLClass;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetOboTermRelationshipsAction_TestCase {
+
+    private GetOboTermRelationshipsAction getOboTermRelationshipsAction;
+    @Mock
+    private ProjectId projectId;
+    @Mock
+    private OWLClass entity;
+
+    @Before
+    public void setUp() {
+        getOboTermRelationshipsAction = new GetOboTermRelationshipsAction(projectId, entity);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_projectId_IsNull() {
+        new GetOboTermRelationshipsAction(null, entity);
+    }
+
+    @Test
+    public void shouldReturnSupplied_projectId() {
+        assertThat(getOboTermRelationshipsAction.getProjectId(), is(this.projectId));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_entity_IsNull() {
+        new GetOboTermRelationshipsAction(projectId, null);
+    }
+
+    @Test
+    public void shouldReturnSupplied_entity() {
+        assertThat(getOboTermRelationshipsAction.getEntity(), is(this.entity));
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(getOboTermRelationshipsAction, is(getOboTermRelationshipsAction));
+    }
+
+    @Test
+    @SuppressWarnings("ObjectEqualsNull")
+    public void shouldNotBeEqualToNull() {
+        assertThat(getOboTermRelationshipsAction.equals(null), is(false));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(getOboTermRelationshipsAction, is(new GetOboTermRelationshipsAction(projectId, entity)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_projectId() {
+        assertThat(getOboTermRelationshipsAction, is(not(new GetOboTermRelationshipsAction(Mockito.mock(ProjectId.class), entity))));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_entity() {
+        assertThat(getOboTermRelationshipsAction, is(not(new GetOboTermRelationshipsAction(projectId, Mockito.mock(OWLClass.class)))));
+    }
+
+    @Test
+    public void shouldBeEqualToOtherHashCode() {
+        assertThat(getOboTermRelationshipsAction.hashCode(), is(new GetOboTermRelationshipsAction(projectId, entity).hashCode()));
+    }
+
+    @Test
+    public void shouldImplementToString() {
+        assertThat(getOboTermRelationshipsAction.toString(), startsWith("GetOboTermRelationshipsAction"));
+    }
+
+}
