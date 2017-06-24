283d9cfd29ffeded2bb1d8e3634da1ea53bc83c0
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-24 09:40:58 -0700

    Added tests for GetOboTermSynonymsAction

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/obo/GetOboTermSynonymsAction_TestCase.java
@@ -0,0 +1,92 @@
+
+package edu.stanford.bmir.protege.web.shared.obo;
+
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetOboTermSynonymsAction_TestCase {
+
+    private GetOboTermSynonymsAction getOboTermSynonymsAction;
+    @Mock
+    private ProjectId projectId;
+    @Mock
+    private OWLEntity entity;
+
+    @Before
+    public void setUp() {
+        getOboTermSynonymsAction = new GetOboTermSynonymsAction(projectId, entity);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_projectId_IsNull() {
+        new GetOboTermSynonymsAction(null, entity);
+    }
+
+    @Test
+    public void shouldReturnSupplied_projectId() {
+        assertThat(getOboTermSynonymsAction.getProjectId(), is(this.projectId));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_entity_IsNull() {
+        new GetOboTermSynonymsAction(projectId, null);
+    }
+
+    @Test
+    public void shouldReturnSupplied_entity() {
+        assertThat(getOboTermSynonymsAction.getEntity(), is(this.entity));
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(getOboTermSynonymsAction, is(getOboTermSynonymsAction));
+    }
+
+    @Test
+    @SuppressWarnings("ObjectEqualsNull")
+    public void shouldNotBeEqualToNull() {
+        assertThat(getOboTermSynonymsAction.equals(null), is(false));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(getOboTermSynonymsAction, is(new GetOboTermSynonymsAction(projectId, entity)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_projectId() {
+        assertThat(getOboTermSynonymsAction, is(not(new GetOboTermSynonymsAction(Mockito.mock(ProjectId.class), entity))));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_entity() {
+        assertThat(getOboTermSynonymsAction, is(not(new GetOboTermSynonymsAction(projectId, Mockito.mock(OWLEntity.class)))));
+    }
+
+    @Test
+    public void shouldBeEqualToOtherHashCode() {
+        assertThat(getOboTermSynonymsAction.hashCode(), is(new GetOboTermSynonymsAction(projectId, entity).hashCode()));
+    }
+
+    @Test
+    public void shouldImplementToString() {
+        assertThat(getOboTermSynonymsAction.toString(), startsWith("GetOboTermSynonymsAction"));
+    }
+
+}
