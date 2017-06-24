2de7b67c28d7c5335cf839f0bb97780a07de6d43
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-24 09:56:54 -0700

    Tests for GetOboTermRelationshipsResult

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/obo/GetOboTermRelationshipsResult_TestCase.java
@@ -0,0 +1,72 @@
+
+package edu.stanford.bmir.protege.web.shared.obo;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetOboTermRelationshipsResult_TestCase {
+
+    private GetOboTermRelationshipsResult getOboTermRelationshipsResult;
+    @Mock
+    private OBOTermRelationships relationships;
+
+    @Before
+    public void setUp() {
+        getOboTermRelationshipsResult = new GetOboTermRelationshipsResult(relationships);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_relationships_IsNull() {
+        new GetOboTermRelationshipsResult(null);
+    }
+
+    @Test
+    public void shouldReturnSupplied_relationships() {
+        assertThat(getOboTermRelationshipsResult.getRelationships(), is(this.relationships));
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(getOboTermRelationshipsResult, is(getOboTermRelationshipsResult));
+    }
+
+    @Test
+    @SuppressWarnings("ObjectEqualsNull")
+    public void shouldNotBeEqualToNull() {
+        assertThat(getOboTermRelationshipsResult.equals(null), is(false));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(getOboTermRelationshipsResult, is(new GetOboTermRelationshipsResult(relationships)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_relationships() {
+        assertThat(getOboTermRelationshipsResult, is(not(new GetOboTermRelationshipsResult(Mockito.mock(OBOTermRelationships.class)))));
+    }
+
+    @Test
+    public void shouldBeEqualToOtherHashCode() {
+        assertThat(getOboTermRelationshipsResult.hashCode(), is(new GetOboTermRelationshipsResult(relationships).hashCode()));
+    }
+
+    @Test
+    public void shouldImplementToString() {
+        assertThat(getOboTermRelationshipsResult.toString(), startsWith("GetOboTermRelationshipsResult"));
+    }
+
+}
