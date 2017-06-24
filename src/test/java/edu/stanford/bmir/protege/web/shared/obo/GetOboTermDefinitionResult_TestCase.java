e72fd5482f5c72011ba823b7c7ec8ea0f9707519
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-23 17:46:36 -0700

    Added tests for SetOboTermDefinitionAction

---
+++ src/test/java/edu/stanford/bmir/protege/web/shared/obo/GetOboTermDefinitionResult_TestCase.java
@@ -0,0 +1,72 @@
+
+package edu.stanford.bmir.protege.web.shared.obo;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetOboTermDefinitionResult_TestCase {
+
+    private GetOboTermDefinitionResult getOboTermDefinitionResult;
+    @Mock
+    private OBOTermDefinition definition;
+
+    @Before
+    public void setUp() {
+        getOboTermDefinitionResult = new GetOboTermDefinitionResult(definition);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionIf_definition_IsNull() {
+        new GetOboTermDefinitionResult(null);
+    }
+
+    @Test
+    public void shouldReturnSupplied_definition() {
+        assertThat(getOboTermDefinitionResult.getDefinition(), is(this.definition));
+    }
+
+    @Test
+    public void shouldBeEqualToSelf() {
+        assertThat(getOboTermDefinitionResult, is(getOboTermDefinitionResult));
+    }
+
+    @Test
+    @SuppressWarnings("ObjectEqualsNull")
+    public void shouldNotBeEqualToNull() {
+        assertThat(getOboTermDefinitionResult.equals(null), is(false));
+    }
+
+    @Test
+    public void shouldBeEqualToOther() {
+        assertThat(getOboTermDefinitionResult, is(new GetOboTermDefinitionResult(definition)));
+    }
+
+    @Test
+    public void shouldNotBeEqualToOtherThatHasDifferent_definition() {
+        assertThat(getOboTermDefinitionResult, is(not(new GetOboTermDefinitionResult(Mockito.mock(OBOTermDefinition.class)))));
+    }
+
+    @Test
+    public void shouldBeEqualToOtherHashCode() {
+        assertThat(getOboTermDefinitionResult.hashCode(), is(new GetOboTermDefinitionResult(definition).hashCode()));
+    }
+
+    @Test
+    public void shouldImplementToString() {
+        assertThat(getOboTermDefinitionResult.toString(), startsWith("GetOboTermDefinitionResult"));
+    }
+
+}
