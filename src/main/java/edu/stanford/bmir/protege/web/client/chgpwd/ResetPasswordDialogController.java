eb5ef70e2435a5e279e4121d18819b26cf1f5c8e
Matthew Horridge <matthew.horridge@stanford.edu>
2017-11-19 09:43:34 -0800

    Changed button names on password dialog

--- src/main/java/edu/stanford/bmir/protege/web/client/chgpwd/ResetPasswordDialogController.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/chgpwd/ResetPasswordDialogController.java
@@ -1,21 +1,35 @@
 package edu.stanford.bmir.protege.web.client.chgpwd;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Widget;
+import edu.stanford.bmir.protege.web.client.Messages;
+import edu.stanford.bmir.protege.web.client.library.dlg.DialogButton;
 import edu.stanford.bmir.protege.web.client.library.dlg.HasRequestFocus;
+import edu.stanford.bmir.protege.web.client.library.dlg.WebProtegeDialogController;
 import edu.stanford.bmir.protege.web.client.library.dlg.WebProtegeOKCancelDialogController;
 import edu.stanford.bmir.protege.web.shared.chgpwd.ResetPasswordData;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import java.util.Optional;
+
+import static edu.stanford.bmir.protege.web.client.library.dlg.DialogButton.CANCEL;
+import static java.util.Arrays.asList;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 01/10/2014
  */
-public class ResetPasswordDialogController extends WebProtegeOKCancelDialogController<ResetPasswordData> {
+public class ResetPasswordDialogController extends WebProtegeDialogController<ResetPasswordData> {
 
     private ResetPasswordView view;
 
-    public ResetPasswordDialogController(ResetPasswordView view) {
-        super("Reset password");
+    @Inject
+    public ResetPasswordDialogController(ResetPasswordView view, Messages messages) {
+        super("Reset password",
+              asList(CANCEL, DialogButton.get(messages.password_resetPassword())),
+              CANCEL,
+              DialogButton.get(messages.password_resetPassword()));
         this.view = view;
     }
 
@@ -26,7 +40,7 @@ public class ResetPasswordDialogController extends WebProtegeOKCancelDialogContr
 
     @Nonnull
     @Override
-    public java.util.Optional<HasRequestFocus> getInitialFocusable() {
+    public Optional<HasRequestFocus> getInitialFocusable() {
         return view.getInitialFocusable();
     }
 
