027652761cb0ea66cd07cc4a7a6b423632cf5eb4
Matthew Horridge <matthew.horridge@stanford.edu>
2017-12-17 11:16:50 +0000

    Added Nonnull annotations to ChangeApplicationResult

--- src/main/java/edu/stanford/bmir/protege/web/server/change/ChangeApplicationResult.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/change/ChangeApplicationResult.java
@@ -1,14 +1,18 @@
 package edu.stanford.bmir.protege.web.server.change;
 
+import com.google.common.collect.ImmutableList;
 import edu.stanford.bmir.protege.web.server.owlapi.RenameMap;
 import edu.stanford.bmir.protege.web.shared.HasSubject;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Author: Matthew Horridge<br>
@@ -18,30 +22,39 @@ import java.util.Optional;
  */
 public class ChangeApplicationResult<S> implements HasSubject<Optional<S>> {
 
-    private RenameMap renameMap;
+    @Nonnull
+    private final RenameMap renameMap;
 
-    private List<OWLOntologyChange> changeList;
+    @Nonnull
+    private final List<OWLOntologyChange> changeList;
 
-    private S subject;
+    @Nonnull
+    private final S subject;
 
-    public ChangeApplicationResult(S subject, List<OWLOntologyChange> changeList, RenameMap renameMap) {
-        this.subject = subject;
-        this.changeList = new ArrayList<>(changeList);
-        this.renameMap = renameMap;
+    public ChangeApplicationResult(@Nonnull S subject,
+                                   @Nonnull List<OWLOntologyChange> changeList,
+                                   @Nonnull RenameMap renameMap) {
+        this.subject = checkNotNull(subject);
+        this.changeList = ImmutableList.copyOf(checkNotNull(changeList));
+        this.renameMap = checkNotNull(renameMap);
     }
 
+    @Nonnull
     public RenameMap getRenameMap() {
         return renameMap;
     }
 
+    @Nonnull
     public List<OWLOntologyChange> getChangeList() {
         return Collections.unmodifiableList(changeList);
     }
 
+    @Nonnull
     public <E extends OWLEntity> E getRenamedEntity(E entity) {
         return renameMap.getRenamedEntity(entity);
     }
 
+    @Nonnull
     @Override
     public Optional<S> getSubject() {
         return Optional.of(subject);
