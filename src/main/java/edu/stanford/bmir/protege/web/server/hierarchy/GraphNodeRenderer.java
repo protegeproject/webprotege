7e70eb214ec1e04f22b66721795f7901f3659a8d
Matthew Horridge <matthew.horridge@stanford.edu>
2017-12-19 15:46:02 +0000

    Bit of tidying

--- src/main/java/edu/stanford/bmir/protege/web/server/hierarchy/GraphNodeRenderer.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/hierarchy/GraphNodeRenderer.java
@@ -5,6 +5,7 @@ import edu.stanford.protege.gwt.graphtree.shared.graph.GraphNode;
 import org.semanticweb.owlapi.model.OWLEntity;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -16,11 +17,19 @@ public class GraphNodeRenderer {
     @Nonnull
     private final EntityHierarchyNodeRenderer renderer;
 
+    @Inject
     public GraphNodeRenderer(@Nonnull EntityHierarchyNodeRenderer renderer) {
         this.renderer = checkNotNull(renderer);
     }
 
-    public GraphNode<EntityHierarchyNode> toGraphNode(@Nonnull OWLEntity entity, HierarchyProvider<OWLEntity> hierarchyProvider) {
+    /**
+     * Render the specified entity into a {@link GraphNode} whose user object
+     * is and {@link EntityHierarchyNode}.
+     * @param entity The entity to be rendered.
+     * @param hierarchyProvider A hierarchy that is used to provide information.
+     */
+    public GraphNode<EntityHierarchyNode> toGraphNode(@Nonnull OWLEntity entity,
+                                                      @Nonnull HierarchyProvider<OWLEntity> hierarchyProvider) {
         return new GraphNode<>(renderer.render(entity), hierarchyProvider.getChildren(entity).isEmpty());
     }
 }
