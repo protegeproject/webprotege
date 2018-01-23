d2191c8b155613be31fa049dd3f348cda194282e
Matthew Horridge <matthew.horridge@stanford.edu>
2018-01-22 17:20:22 -0800

    Moved DispatchService to shared package

--- src/main/java/edu/stanford/bmir/protege/web/server/dispatch/impl/DispatchServiceImpl.java
+++ src/main/java/edu/stanford/bmir/protege/web/server/dispatch/impl/DispatchServiceImpl.java
@@ -1,7 +1,7 @@
 package edu.stanford.bmir.protege.web.server.dispatch.impl;
 
 import edu.stanford.bmir.protege.web.shared.dispatch.ActionExecutionException;
-import edu.stanford.bmir.protege.web.client.dispatch.DispatchService;
+import edu.stanford.bmir.protege.web.shared.dispatch.DispatchService;
 import edu.stanford.bmir.protege.web.server.app.WebProtegeRemoteServiceServlet;
 import edu.stanford.bmir.protege.web.server.dispatch.DispatchServiceExecutor;
 import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
