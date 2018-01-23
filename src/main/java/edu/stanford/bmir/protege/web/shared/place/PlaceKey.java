02960e7a15fcf9a8400cc0e9bbe2a4f307174071
Matthew Horridge <matthew.horridge@stanford.edu>
2018-01-22 16:52:56 -0800

    Moved PlaceKey to shared package

--- src/main/java/edu/stanford/bmir/protege/web/shared/place/PlaceKey.java
+++ src/main/java/edu/stanford/bmir/protege/web/shared/place/PlaceKey.java
@@ -1,4 +1,4 @@
-package edu.stanford.bmir.protege.web.client.place;
+package edu.stanford.bmir.protege.web.shared.place;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 16/05/2014
