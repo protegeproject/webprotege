710c42655595a78e262c12e6419220aa16f528d4
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-11 13:39:20 -0700

    Adding an enum to describe the different possible treatments of entities that are merged into other entities

---
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/entity/MergedEntityTreatment.java
@@ -0,0 +1,13 @@
+package edu.stanford.bmir.protege.web.shared.entity;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 9 Mar 2018
+ */
+public enum MergedEntityTreatment {
+
+    DELETE_MERGED_ENTITY,
+
+    DEPRECATE_MERGED_ENTITY
+}
