863389222aaeb173afb44cdcebb649256805518f
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-11 13:48:47 -0700

    Made MergedEntityTreatment implement IsSerializable

--- webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/entity/MergedEntityTreatment.java
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/entity/MergedEntityTreatment.java
@@ -1,11 +1,13 @@
 package edu.stanford.bmir.protege.web.shared.entity;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 9 Mar 2018
  */
-public enum MergedEntityTreatment {
+public enum MergedEntityTreatment implements IsSerializable {
 
     DELETE_MERGED_ENTITY,
 
