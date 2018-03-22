c774eb101ff0c6b6e3e625d5d055e77868aad069
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-22 15:49:14 -0700

    Added a tokenizer for project tags places

---
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/place/ProjectTagsPlaceTokenizer.java
@@ -0,0 +1,52 @@
+package edu.stanford.bmir.protege.web.shared.place;
+
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 22 Mar 2018
+ */
+public class ProjectTagsPlaceTokenizer implements WebProtegePlaceTokenizer<ProjectTagsPlace> {
+
+    private static final String PROJECTS = "projects/";
+
+    private static final String TAGS = "/tags";
+
+    private static RegExp regExp = RegExp.compile(PROJECTS + "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})" + TAGS);
+
+
+    @Override
+    public boolean matches(String token) {
+        return regExp.test(token);
+    }
+
+    @Override
+    public Class<ProjectTagsPlace> getPlaceClass() {
+        return ProjectTagsPlace.class;
+    }
+
+    @Override
+    public ProjectTagsPlace getPlace(String token) {
+        MatchResult matchResult = regExp.exec(token);
+        if(matchResult == null) {
+            return null;
+        }
+        String projectIdString = matchResult.getGroup(1);
+        if(ProjectId.isWelFormedProjectId(projectIdString)) {
+            ProjectId projectId = ProjectId.get(projectIdString);
+            return new ProjectTagsPlace(projectId);
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public String getToken(ProjectTagsPlace place) {
+        return PROJECTS + place.getProjectId().getId() + TAGS;
+    }
+
+}
---
+++ webprotege-shared/src/test/java/edu/stanford/bmir/protege/web/shared/place/ProjectTagsPlaceTokenizer_TestCase.java
@@ -0,0 +1,43 @@
+package edu.stanford.bmir.protege.web.shared.place;
+
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 22 Mar 2018
+ */
+public class ProjectTagsPlaceTokenizer_TestCase {
+
+    private static final String PROJECT_ID = "12345678-1234-1234-1234-123456789abc";
+
+    private static final String PLACE_TOKEN = "projects/" + PROJECT_ID + "/tags";
+
+    private ProjectTagsPlaceTokenizer tokenizer;
+
+    @Before
+    public void setUp() throws Exception {
+        tokenizer = new ProjectTagsPlaceTokenizer();
+    }
+
+    @Test
+    public void shouldParsePlaceToken() {
+        ProjectTagsPlace place = tokenizer.getPlace(PLACE_TOKEN);
+        assertThat(place, is(not(nullValue())));
+        assertThat(place.getProjectId().getId(), is(PROJECT_ID));
+    }
+
+    @Test
+    public void shouldGenerateToken() {
+        ProjectTagsPlace place = new ProjectTagsPlace(ProjectId.get(PROJECT_ID));
+        String token = tokenizer.getToken(place);
+        assertThat(token, is(PLACE_TOKEN));
+    }
+}
