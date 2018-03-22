e131c57504c4b2d5336aa8f1ac3cb1799f1cbb41
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-22 14:56:37 -0700

    Added a dedicated view for the editor portlet

--- webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/editor/EditorPortletView.java
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/editor/EditorPortletView.java
@@ -1,9 +1,20 @@
 package edu.stanford.bmir.protege.web.client.editor;
 
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.IsWidget;
+
+import javax.annotation.Nonnull;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 22 Mar 2018
  */
-public interface EditorPortletView {
+public interface EditorPortletView extends IsWidget {
+
+    @Nonnull
+    AcceptsOneWidget getTagListViewContainer();
+
+    @Nonnull
+    AcceptsOneWidget getEditorViewContainer();
 }
--- webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/editor/EditorPortletViewImpl.java
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/editor/EditorPortletViewImpl.java
@@ -2,20 +2,47 @@ package edu.stanford.bmir.protege.web.client.editor;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 22 Mar 2018
  */
-public class EditorPortletViewImpl extends Composite {
-    interface EditorPortletViewImplUiBinder extends UiBinder<com.google.gwt.user.client.ui.HTMLPanel, edu.stanford.bmir.protege.web.client.editor.EditorPortletViewImpl> {
+public class EditorPortletViewImpl extends Composite implements EditorPortletView {
+
+    interface EditorPortletViewImplUiBinder extends UiBinder<HTMLPanel, EditorPortletViewImpl> {
     }
 
     private static EditorPortletViewImplUiBinder ourUiBinder = GWT.create(EditorPortletViewImplUiBinder.class);
 
+    @UiField
+    SimplePanel tagListViewContainer;
+
+    @UiField
+    SimplePanel editorViewContainer;
+
+    @Inject
     public EditorPortletViewImpl() {
         initWidget(ourUiBinder.createAndBindUi(this));
+    }
+
+    @Nonnull
+    @Override
+    public AcceptsOneWidget getTagListViewContainer() {
+        return tagListViewContainer;
+    }
+
+    @Nonnull
+    @Override
+    public AcceptsOneWidget getEditorViewContainer() {
+        return editorViewContainer;
     }
 }
