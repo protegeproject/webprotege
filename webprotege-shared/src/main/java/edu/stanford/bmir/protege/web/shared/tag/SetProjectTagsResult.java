49ebb6e6955af117885c35c35e29fe76eb2da33d
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-25 11:51:31 -0700

    Added an action, result and action handler for setting project tags

---
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/tag/SetProjectTagsResult.java
@@ -0,0 +1,9 @@
+package edu.stanford.bmir.protege.web.shared.tag;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 24 Mar 2018
+ */
+public class SetProjectTagsResult {
+}
