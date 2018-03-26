cc7d7aed76a79b1170a6d77ddba51d3235e0f96a
Matthew Horridge <matthew.horridge@stanford.edu>
2018-03-26 15:01:08 -0700

    Bit of tyding with imports static

---
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/tag/DeleteProjectTagConfirmationPrompt.java
@@ -0,0 +1,9 @@
+package edu.stanford.bmir.protege.web.client.tag;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 26 Mar 2018
+ */
+public class DeleteProjectTagConfirmation {
+}
