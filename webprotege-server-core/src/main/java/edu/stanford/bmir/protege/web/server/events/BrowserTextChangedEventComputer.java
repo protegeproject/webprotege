5112b62780762e088359531194675284f27dae8b
Matthew Horridge <matthew.horridge@stanford.edu>
2018-04-09 15:35:21 -0700

    Tidied BrowserTextChangedComputer

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/events/BrowserTextChangedEventComputer.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/events/BrowserTextChangedEventComputer.java
@@ -4,19 +4,22 @@ import com.google.common.collect.Maps;
 import edu.stanford.bmir.protege.web.server.change.ChangeApplicationResult;
 import edu.stanford.bmir.protege.web.server.change.HasGetChangeSubjects;
 import edu.stanford.bmir.protege.web.server.revision.Revision;
+import edu.stanford.bmir.protege.web.server.shortform.DictionaryManager;
 import edu.stanford.bmir.protege.web.shared.event.BrowserTextChangedEvent;
 import edu.stanford.bmir.protege.web.shared.event.ProjectEvent;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import org.semanticweb.owlapi.model.HasContainsEntityInSignature;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
-import org.semanticweb.owlapi.util.ShortFormProvider;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @author Matthew Horridge,
@@ -26,52 +29,53 @@ import java.util.Set;
  */
 public class BrowserTextChangedEventComputer implements EventTranslator {
 
-    private Map<OWLEntity, String> shortFormMap = Maps.newHashMap();
+    private final Map<OWLEntity, String> shortFormMap = Maps.newHashMap();
 
     private final ProjectId projectId;
 
-    private final ShortFormProvider shortFormProvider;
+    @Nonnull
+    private final DictionaryManager dictionaryManager;
 
-    private final HasGetChangeSubjects hasChangeSubject;
+    private final HasGetChangeSubjects changeSubjectsProvider;
 
-    private final HasContainsEntityInSignature hasContainsEntityInSignature;
+    private final HasContainsEntityInSignature signature;
 
     @Inject
-    public BrowserTextChangedEventComputer(ProjectId projectId,
-                                           ShortFormProvider shortFormProvider,
-                                           HasGetChangeSubjects hasChangeSubject,
-                                           HasContainsEntityInSignature hasContainsEntityInSignature) {
-        this.projectId = projectId;
-        this.shortFormProvider = shortFormProvider;
-        this.hasChangeSubject = hasChangeSubject;
-        this.hasContainsEntityInSignature = hasContainsEntityInSignature;
+    public BrowserTextChangedEventComputer(@Nonnull ProjectId projectId,
+                                           @Nonnull DictionaryManager dictionaryManager,
+                                           @Nonnull HasGetChangeSubjects changeSubjectsProvider,
+                                           @Nonnull HasContainsEntityInSignature signature) {
+        this.projectId = checkNotNull(projectId);
+        this.dictionaryManager = checkNotNull(dictionaryManager);
+        this.changeSubjectsProvider = checkNotNull(changeSubjectsProvider);
+        this.signature = checkNotNull(signature);
     }
 
     @Override
     public void prepareForOntologyChanges(List<OWLOntologyChange> submittedChanges) {
-        for(OWLOntologyChange change : submittedChanges) {
-            for(OWLEntity entity : hasChangeSubject.getChangeSubjects(change)) {
-                if(hasContainsEntityInSignature.containsEntityInSignature(entity)) {
-                    String shortForm = shortFormProvider.getShortForm(entity);
-                    shortFormMap.put(entity, shortForm);
-                }
-            }
-        }
+        submittedChanges.stream()
+                        .flatMap(change -> changeSubjectsProvider.getChangeSubjects(change).stream())
+                        // The changes might only be extending the signature, so check for this.
+                        // If this is the case, then there will not be any existing short form.
+                        .filter(signature::containsEntityInSignature)
+                        .forEach(entity -> {
+                            String shortForm = dictionaryManager.getShortForm(entity);
+                            shortFormMap.put(entity, shortForm);
+                        });
     }
 
     @Override
     public void translateOntologyChanges(Revision revision, ChangeApplicationResult<?> changes, List<ProjectEvent<?>> projectEventList) {
         Set<OWLEntity> processedEntities = new HashSet<>();
-        for(OWLOntologyChange change : changes.getChangeList()) {
-            for(OWLEntity entity : hasChangeSubject.getChangeSubjects(change)) {
-                if(processedEntities.add(entity)) {
-                    String shortForm = shortFormProvider.getShortForm(entity);
-                    String oldShortForm = shortFormMap.get(entity);
-                    if(oldShortForm == null || !shortForm.equals(oldShortForm)) {
-                        projectEventList.add(new BrowserTextChangedEvent(entity, shortForm, projectId));
-                    }
-                }
-            }
-        }
+        changes.getChangeList().stream()
+               .flatMap(change -> changeSubjectsProvider.getChangeSubjects(change).stream())
+               .distinct()
+               .forEach(entity -> {
+                   String shortForm = dictionaryManager.getShortForm(entity);
+                   String oldShortForm = shortFormMap.get(entity);
+                   if(oldShortForm == null || !shortForm.equals(oldShortForm)) {
+                       projectEventList.add(new BrowserTextChangedEvent(entity, shortForm, projectId));
+                   }
+               });
     }
 }
