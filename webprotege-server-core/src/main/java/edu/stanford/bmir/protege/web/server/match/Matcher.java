9cd7c7cd340be75a989d48791deb5782292c266b
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-07 16:45:01 -0700

    Added initial matcher interface

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/Matcher.java
@@ -0,0 +1,13 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2018
+ */
+public interface Matcher<T> {
+
+    boolean matches(@Nonnull T value);
+}
