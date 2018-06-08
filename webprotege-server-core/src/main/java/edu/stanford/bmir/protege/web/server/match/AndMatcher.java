3e5f7201ea98963cf317c9322ddcd45c0774362d
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-07 17:55:35 -0700

    Added AndMatcher

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/AndMatcher.java
@@ -0,0 +1,28 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2018
+ */
+public class AndMatcher<T> implements Matcher<T> {
+
+    @Nonnull
+    private final ImmutableList<Matcher<T>> matchers;
+
+    public AndMatcher(@Nonnull ImmutableList<Matcher<T>> matchers) {
+        this.matchers = checkNotNull(matchers);
+    }
+
+    @Override
+    public boolean matches(@Nonnull T value) {
+        return matchers.stream()
+                       .allMatch(m -> m.matches(value));
+    }
+}
