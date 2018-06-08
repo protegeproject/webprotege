434d1d14464040fa3c3a050df8e4fe93a825b4ac
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-07 17:17:40 -0700

    Added Annotation matcher

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/AnnotationMatcher.java
@@ -0,0 +1,33 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2018
+ */
+public class AnnotationMatcher implements Matcher<OWLAnnotation> {
+
+    @Nonnull
+    private final Matcher<OWLAnnotationProperty> propertyMatcher;
+
+    @Nonnull
+    private final Matcher<OWLAnnotationValue> annotationValueMatcher;
+
+    public AnnotationMatcher(@Nonnull Matcher<OWLAnnotationProperty> propertyMatcher,
+                             @Nonnull Matcher<OWLAnnotationValue> annotationValueMatcher) {
+        this.propertyMatcher = propertyMatcher;
+        this.annotationValueMatcher = annotationValueMatcher;
+    }
+
+    @Override
+    public boolean matches(@Nonnull OWLAnnotation value) {
+        return propertyMatcher.matches(value.getProperty())
+                && annotationValueMatcher.matches(value.getValue());
+    }
+}
