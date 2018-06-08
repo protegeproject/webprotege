32b5758dbf24434ba9a91d3f7e88e18395610cfe
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-07 17:16:40 -0700

    Added matcher for literal annotation values

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/LiteralAnnotationValueMatcher.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/LiteralAnnotationValueMatcher.java
@@ -1,10 +1,28 @@
 package edu.stanford.bmir.protege.web.server.match;
 
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+import org.semanticweb.owlapi.model.OWLLiteral;
+
+import javax.annotation.Nonnull;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 7 Jun 2018
  */
-public class AnnotationValueMatcher {
+public class LiteralAnnotationValueMatcher implements Matcher<OWLAnnotationValue> {
 
+    @Nonnull
+    private final Matcher<OWLLiteral> literalMatcher;
+
+    public LiteralAnnotationValueMatcher(@Nonnull Matcher<OWLLiteral> literalMatcher) {
+        this.literalMatcher = checkNotNull(literalMatcher);
+    }
+
+    @Override
+    public boolean matches(@Nonnull OWLAnnotationValue value) {
+        return value.isLiteral() && literalMatcher.matches((OWLLiteral) value);
+    }
 }
---
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/match/LiteralAnnotationValueMatcher_TestCase.java
@@ -0,0 +1,70 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
+import org.semanticweb.owlapi.model.OWLLiteral;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2018
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LiteralAnnotationValueMatcher_TestCase {
+
+    private LiteralAnnotationValueMatcher matcher;
+
+    @Mock
+    private Matcher<OWLLiteral> literalMatcher;
+
+    @Mock
+    private OWLLiteral literal;
+
+    @Mock
+    private IRI iri;
+
+    @Mock
+    private OWLAnonymousIndividual individual;
+
+    @Before
+    public void setUp() throws Exception {
+        matcher = new LiteralAnnotationValueMatcher(literalMatcher);
+
+        when(literal.isLiteral()).thenReturn(true);
+
+        when(literalMatcher.matches(literal)).thenReturn(true);
+
+    }
+
+    @Test
+    public void shouldMatchLiteral() {
+        assertThat(matcher.matches(literal), is(true));
+    }
+
+    @Test
+    public void shouldNotMatchLiteral() {
+        when(literalMatcher.matches(literal)).thenReturn(false);
+        assertThat(matcher.matches(literal), is(false));
+    }
+
+    @Test
+    public void shouldNotMatchIri() {
+        assertThat(matcher.matches(iri), is(false));
+    }
+
+    @Test
+    public void shouldNotMatchAnonymousIndividual() {
+        assertThat(matcher.matches(individual), is(false));
+    }
+}
