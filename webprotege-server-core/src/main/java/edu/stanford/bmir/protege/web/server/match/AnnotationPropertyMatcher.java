c5b170807a1904bc14e9aade18cc780cfadf2e6f
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-07 17:27:41 -0700

    Added a matcher for annotation properties

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/AnnotationPropertyMatcher.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/AnnotationPropertyMatcher.java
@@ -1,10 +1,28 @@
 package edu.stanford.bmir.protege.web.server.match;
 
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+
+import javax.annotation.Nonnull;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 7 Jun 2018
  */
-public class AnnotationPropertyMatcher {
+public class AnnotationPropertyMatcher implements Matcher<OWLAnnotationProperty> {
 
+    @Nonnull
+    private final Matcher<IRI> iriMatcher;
+
+    public AnnotationPropertyMatcher(@Nonnull Matcher<IRI> iriMatcher) {
+        this.iriMatcher = checkNotNull(iriMatcher);
+    }
+
+    @Override
+    public boolean matches(@Nonnull OWLAnnotationProperty value) {
+        return iriMatcher.matches(value.getIRI());
+    }
 }
---
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/match/AnnotationPropertyMatcher_TestCase.java
@@ -0,0 +1,52 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 7 Jun 2018
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AnnotationPropertyMatcher_TestCase {
+
+    private AnnotationPropertyMatcher matcher;
+
+    @Mock
+    private OWLAnnotationProperty property;
+
+    @Mock
+    private IRI iri;
+
+    @Mock
+    private Matcher<IRI> iriMatcher;
+
+    @Before
+    public void setUp() {
+        matcher = new AnnotationPropertyMatcher(iriMatcher);
+        when(property.getIRI()).thenReturn(iri);
+    }
+
+    @Test
+    public void shouldMatchAnnotationProperty() {
+        when(iriMatcher.matches(iri)).thenReturn(true);
+        assertThat(matcher.matches(property), is(true));
+    }
+
+    @Test
+    public void shouldNotMatchAnnotationProperty() {
+        when(iriMatcher.matches(iri)).thenReturn(false);
+        assertThat(matcher.matches(property), is(false));
+    }
+}
