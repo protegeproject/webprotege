1f5ded61a06042535328af9e84f9d724c926df85
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-12 11:10:37 -0700

    Added documentation for AnnotationCriteria

--- webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/match/criteria/AnnotationCriteria.java
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/match/criteria/AnnotationCriteria.java
@@ -11,6 +11,8 @@ import javax.annotation.Nonnull;
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 11 Jun 2018
+ *
+ * Represents criteria to match an annotation
  */
 @AutoValue
 public abstract class AnnotationCriteria {
@@ -22,6 +24,46 @@ public abstract class AnnotationCriteria {
     private static final String ANNOTATIONS = "annotations";
 
     private static final String PRESENCE = "presence";
+
+    /**
+     * Creates criteria that match an annotation based on its property, its value,
+     * its set of annotations and its presence.
+     *  @param propertyCriteria      The criteria for matching the property.
+     * @param valueCriteria         The criteria for matching the value.
+     * @param annotationSetCriteria The criteria for matching annotations on the annotation.
+     * @param presence              The presence that specifies whether the annotation should be
+*                              present or not.
+     */
+    @JsonCreator
+    @Nonnull
+    public static AnnotationCriteria get(@Nonnull @JsonProperty(PROPERTY) AnnotationPropertyCriteria propertyCriteria,
+                                         @Nonnull @JsonProperty(VALUE) AnnotationValueCriteria valueCriteria,
+                                         @Nonnull @JsonProperty(ANNOTATIONS) AnnotationSetCriteria annotationSetCriteria,
+                                         @Nonnull @JsonProperty(PRESENCE) AnnotationPresence presence) {
+        return new AutoValue_AnnotationCriteria(propertyCriteria, valueCriteria, presence, annotationSetCriteria);
+    }
+
+    /**
+     * A convenience method to create criteria that match an annotation based on its property
+     * and its value.  The annotation must be present.  Annotations on the annotation are ignored.
+     *
+     * @param propertyCriteria The criteria for matching the property.
+     * @param valueCriteria    The criteria for matching the value.
+     */
+    @Nonnull
+    public static AnnotationCriteria get(@Nonnull @JsonProperty(PROPERTY) AnnotationPropertyCriteria propertyCriteria,
+                                         @Nonnull @JsonProperty(VALUE) AnnotationValueCriteria valueCriteria) {
+        return get(propertyCriteria, valueCriteria, AnyAnnotationSetCriteria.get(), AnnotationPresence.PRESENT);
+    }
+
+    /**
+     * A convenicence method to create criteria that will match any annotation.
+     */
+    @Nonnull
+    public static AnnotationCriteria anyAnnotation() {
+        return get(AnyAnnotationPropertyCriteria.get(),
+                   AnyAnnotationValueCriteria.get());
+    }
 
     @JsonProperty(PROPERTY)
     @Nonnull
@@ -38,25 +80,4 @@ public abstract class AnnotationCriteria {
     @JsonProperty(ANNOTATIONS)
     @Nonnull
     public abstract AnnotationSetCriteria getAnnotationSetCriteria();
-
-    @JsonCreator
-    @Nonnull
-    public static AnnotationCriteria get(@Nonnull @JsonProperty(PROPERTY) AnnotationPropertyCriteria propertyCriteria,
-                                         @Nonnull @JsonProperty(VALUE) AnnotationValueCriteria valueCriteria,
-                                         @Nonnull @JsonProperty(PRESENCE) AnnotationPresence presence,
-                                         @Nonnull @JsonProperty(ANNOTATIONS) AnnotationSetCriteria annotationSetCriteria) {
-        return new AutoValue_AnnotationCriteria(propertyCriteria, valueCriteria, presence, annotationSetCriteria);
-    }
-
-    @Nonnull
-    public static AnnotationCriteria get(@Nonnull @JsonProperty(PROPERTY) AnnotationPropertyCriteria propertyCriteria,
-                                         @Nonnull @JsonProperty(VALUE) AnnotationValueCriteria valueCriteria) {
-        return get(propertyCriteria, valueCriteria, AnnotationPresence.PRESENT, AnyAnnotationSetCriteria.get());
-    }
-
-    @Nonnull
-    public static AnnotationCriteria anyAnnotation() {
-        return get(AnyAnnotationPropertyCriteria.get(),
-                   AnyAnnotationValueCriteria.get());
-    }
 }
