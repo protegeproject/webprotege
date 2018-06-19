0e468302b2f1626f8b1d9fb6fcce454057cb5ca9
Matthew Horridge <matthew.horridge@stanford.edu>
2018-06-19 13:47:51 -0700

    Added a matching engine to match entities against specified criteria

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/MatchingEngine.java
@@ -0,0 +1,31 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import com.google.common.collect.ImmutableList;
+import edu.stanford.bmir.protege.web.shared.match.criteria.Criteria;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import javax.annotation.Nonnull;
+import java.util.stream.Stream;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 19 Jun 2018
+ */
+public interface MatchingEngine {
+
+    /**
+     * Get the entities that match the specified criteria.
+     */
+    Stream<OWLEntity> match(@Nonnull Criteria criteria);
+
+    /**
+     * Gets the entities that match any of the specified criteria
+     */
+    Stream<OWLEntity> matchAny(@Nonnull ImmutableList<? extends Criteria> criteria);
+
+    /**
+     * Determines whether the specified entity matches the specified criteria
+     */
+    boolean matches(@Nonnull OWLEntity entity, @Nonnull Criteria criteria);
+}
---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/MatchingEngineImpl.java
@@ -0,0 +1,66 @@
+package edu.stanford.bmir.protege.web.server.match;
+
+import com.google.common.collect.ImmutableList;
+import edu.stanford.bmir.protege.web.server.inject.project.RootOntology;
+import edu.stanford.bmir.protege.web.shared.match.criteria.Criteria;
+import edu.stanford.bmir.protege.web.shared.match.criteria.RootCriteria;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.parameters.Imports;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 19 Jun 2018
+ */
+public class MatchingEngineImpl implements MatchingEngine {
+
+    @Nonnull
+    @RootOntology
+    private final OWLOntology rootOntology;
+
+    @Nonnull
+    private final MatcherFactory matcherFactory;
+
+    @Inject
+    public MatchingEngineImpl(@Nonnull @RootOntology OWLOntology rootOntology,
+                              @Nonnull MatcherFactory matcherFactory) {
+        this.rootOntology = checkNotNull(rootOntology);
+        this.matcherFactory = checkNotNull(matcherFactory);
+    }
+
+    @Override
+    public Stream<OWLEntity> match(@Nonnull Criteria criteria) {
+        Matcher<OWLEntity> matcher = getMatcher(criteria);
+        return rootOntology.getSignature(Imports.INCLUDED)
+                .stream()
+                .filter(matcher::matches);
+    }
+
+    @Override
+    public Stream<OWLEntity> matchAny(@Nonnull ImmutableList<? extends Criteria> criteria) {
+        ImmutableList<Matcher<OWLEntity>> matchers = criteria.stream()
+                                                            .map(this::getMatcher)
+                                                            .collect(toImmutableList());
+        OrMatcher<OWLEntity> orMatcher = new OrMatcher<>(matchers);
+        return rootOntology.getSignature(Imports.INCLUDED)
+                           .stream()
+                           .filter(orMatcher::matches);
+    }
+
+    @Override
+    public boolean matches(@Nonnull OWLEntity entity, @Nonnull Criteria criteria) {
+        return getMatcher(criteria).matches(entity);
+    }
+
+    private Matcher<OWLEntity> getMatcher(@Nonnull Criteria rootCriteria) {
+        return matcherFactory.getMatcher((RootCriteria) rootCriteria);
+    }
+}
