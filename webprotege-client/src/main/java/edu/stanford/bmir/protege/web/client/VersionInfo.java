56ddc89143509ea9ca0bc6d599ea596c4bf7bc35
Matthew Horridge <matthew.horridge@stanford.edu>
2018-08-28 18:49:07 -0700

    Closes #512

---
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/VersionInfo.java
@@ -0,0 +1,17 @@
+package edu.stanford.bmir.protege.web.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 28 Aug 2018
+ */
+public interface VersionInfo extends Constants {
+
+    @Key("version")
+    String versionString();
+
+    @Key("build.timestamp")
+    String buildTimestamp();
+}
