70c30f44826d59be99b18e7fe65805c30d957116
Matthew Horridge <matthew.horridge@stanford.edu>
2018-09-18 11:57:58 -0700

    Added a basic individuals index.  This is not an efficient implementation – no caching is used

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/individuals/IndividualsIndex.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/individuals/IndividualsIndex.java
@@ -1,6 +1,7 @@
 package edu.stanford.bmir.protege.web.server.individuals;
 
 import edu.stanford.bmir.protege.web.shared.individuals.InstanceRetrievalMode;
+import edu.stanford.bmir.protege.web.shared.inject.ProjectSingleton;
 import edu.stanford.bmir.protege.web.shared.pagination.PageRequest;
 import org.semanticweb.owlapi.model.OWLClass;
 
@@ -11,14 +12,29 @@ import javax.annotation.Nonnull;
  * Stanford Center for Biomedical Informatics Research
  * 14 Sep 2018
  */
+@ProjectSingleton
 public interface IndividualsIndex {
 
+    /**
+     * Get individuals that have a display name that matches the specified search string
+     * @param searchString The search string.  Spaces separate multiple words.
+     * @param pageRequest A page request that determines the individuals to be retrieved.
+     */
     @Nonnull
     IndividualsQueryResult getIndividuals(@Nonnull String searchString,
                                           @Nonnull PageRequest pageRequest);
 
+    /**
+     * Get individuals that are either directly or indirect an instance of the specified type.  The individuals
+     * will be filtered by the specified search string so that only individuals whose display name matches the
+     * specified string will be returned.
+     * @param searchString The search string.  Spaces separate multiple words.
+     * @param pageRequest A page request that determines the individuals to be retrieved.
+     * @param type The type for the individuals to be retrieved.
+     * @param mode The mode of retrieval.
+     */
     @Nonnull
-    IndividualsQueryResult getIndividuals(@Nonnull OWLClass byType,
+    IndividualsQueryResult getIndividuals(@Nonnull OWLClass type,
                                           @Nonnull InstanceRetrievalMode mode,
                                           @Nonnull String searchString,
                                           @Nonnull PageRequest pageRequest);
--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/individuals/IndividualsIndexImpl.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/individuals/IndividualsIndexImpl.java
@@ -1,9 +1,7 @@
 package edu.stanford.bmir.protege.web.server.individuals;
 
-import com.google.common.base.Stopwatch;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
-import com.google.common.collect.Streams;
 import edu.stanford.bmir.protege.web.server.hierarchy.ClassHierarchyProvider;
 import edu.stanford.bmir.protege.web.server.shortform.DictionaryManager;
 import edu.stanford.bmir.protege.web.server.shortform.Scanner;
@@ -14,18 +12,13 @@ import edu.stanford.bmir.protege.web.shared.individuals.InstanceRetrievalMode;
 import edu.stanford.bmir.protege.web.shared.pagination.Page;
 import edu.stanford.bmir.protege.web.shared.pagination.PageRequest;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
@@ -33,7 +26,6 @@ import java.util.stream.Stream;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static edu.stanford.bmir.protege.web.server.pagination.PageCollector.toPage;
 import static edu.stanford.bmir.protege.web.shared.individuals.InstanceRetrievalMode.ALL_INSTANCES;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /**
  * Matthew Horridge
@@ -58,11 +50,13 @@ public class IndividualsIndexImpl implements IndividualsIndex {
     @Nonnull
     private final ClassHierarchyProvider classHierarchyProvider;
 
-    private final SetMultimap<OWLClass, OWLNamedIndividual> individualsByClass = HashMultimap.create();
+    private final SetMultimap<OWLClass, OWLClass> classAssertionClassesByAncestors = HashMultimap.create();
 
-    private final SetMultimap<OWLClass, OWLClass> clsesBySubClass = HashMultimap.create();
+    private final SetMultimap<OWLClass, OWLClass> ancestorsByClassAssertionClasses = HashMultimap.create();
 
     private boolean builtIndex = false;
+
+    private boolean bound = false;
 
     @Inject
     public IndividualsIndexImpl(@Nonnull ProjectId projectId, @Nonnull OWLOntology rootOntology, @Nonnull DictionaryManager dictionaryManager, @Nonnull ClassHierarchyProvider classHierarchyProvider) {
@@ -72,52 +66,10 @@ public class IndividualsIndexImpl implements IndividualsIndex {
         this.classHierarchyProvider = checkNotNull(classHierarchyProvider);
     }
 
-    private void ensureIndex() {
-        try {
-            lock.lock();
-            if (builtIndex) {
-                return;
-            }
-            Stopwatch stopwatch = Stopwatch.createStarted();
-            logger.info("{} Building individuals indexes", projectId);
-            individualsByClass.clear();
-            clsesBySubClass.clear();
-            for (OWLOntology ontology : rootOntology.getImportsClosure()) {
-                for (OWLNamedIndividual individual : ontology.getIndividualsInSignature()) {
-                    if (!individual.isAnonymous()) {
-                        Set<OWLClassAssertionAxiom> axioms = ontology.getClassAssertionAxioms(individual);
-                        if (!axioms.isEmpty()) {
-                            axioms
-                                    .stream()
-                                    .filter(ax -> !ax.getClassExpression().isAnonymous())
-                                    .forEach(ax -> individualsByClass.put(ax.getClassExpression().asOWLClass(),
-                                                                          ax.getIndividual().asOWLNamedIndividual()));
-                        }
-                        else {
-                            individualsByClass.put(DataFactory.getOWLThing(),
-                                                   individual);
-                        }
-                    }
-                }
-            }
-            for (OWLClass cls : individualsByClass.keySet()) {
-                classHierarchyProvider.getParents(cls)
-                                      .forEach(par -> clsesBySubClass.put(par, cls));
-            }
-            stopwatch.stop();
-            logger.info("{} Built individuals indexes in {} ms", projectId, stopwatch.elapsed(MILLISECONDS));
-            builtIndex = true;
-        } finally {
-            lock.unlock();
-        }
-    }
-
-
     @Nonnull
     @Override
     public IndividualsQueryResult getIndividuals(@Nonnull String searchString,
                                                  @Nonnull PageRequest pageRequest) {
-        ensureIndex();
         return getIndividuals(DataFactory.getOWLThing(),
                               ALL_INSTANCES,
                               searchString,
@@ -130,47 +82,59 @@ public class IndividualsIndexImpl implements IndividualsIndex {
                                                  @Nonnull InstanceRetrievalMode mode,
                                                  @Nonnull String search,
                                                  @Nonnull PageRequest pageRequest) {
-        ensureIndex();
         List<SearchString> searchStrings = SearchString.parseMultiWordSearchString(search);
-        Stream<OWLNamedIndividual> individuals;
-        Stream<OWLNamedIndividual> direct = individualsByClass.get(type).stream();
-        if (mode == ALL_INSTANCES) {
-            Stream<OWLNamedIndividual> indirect = clsesBySubClass.get(type)
-                                                                 .stream()
-                                                                 .flatMap(cls -> individualsByClass.get(cls).stream());
 
-            individuals = Streams.concat(direct, indirect);
+        Stream<OWLClass> direct = Stream.of(type);
+        Stream<OWLClass> ancestors;
+        if (mode == ALL_INSTANCES) {
+            // This potentially needs caching
+            ancestors = classHierarchyProvider.getDescendants(type).stream();
         }
         else {
-            individuals = direct;
+            ancestors = Stream.empty();
         }
+        Stream<OWLClass> types = Stream.concat(direct, ancestors);
+        Stream<OWLNamedIndividual> individuals =
+                types.flatMap(t -> rootOntology.getImportsClosure()
+                        .stream()
+                        .flatMap(o -> o.getClassAssertionAxioms(t).stream()))
+                        .map(OWLClassAssertionAxiom::getIndividual)
+                        .filter(OWLIndividual::isNamed)
+                        .map(OWLIndividual::asOWLNamedIndividual);
         Counter counter = new Counter();
-        Optional<Page<OWLNamedIndividual>> page = individuals.peek(i -> counter.increment()).filter(i -> {
-            if (searchStrings.isEmpty()) {
-                return true;
-            }
-            String shortForm = dictionaryManager.getShortForm(i);
-            Scanner scanner = new Scanner(shortForm, shortForm);
-            for (SearchString searchString : searchStrings) {
-                int index = scanner.indexOf(searchString, 0);
-                if (index == -1) {
-                    return false;
-                }
-            }
-            return true;
-        })
-                                                             .distinct()
-                                                             .map(i -> new IndividualRendering(i, dictionaryManager.getShortForm(i)
-                                                                                                                   .toLowerCase()))
-                                                             .sorted()
-                                                             .map(IndividualRendering::getIndividual)
-                                                             .collect(toPage(pageRequest.getPageNumber(),
-                                                                             pageRequest.getPageSize()));
+        Optional<Page<OWLNamedIndividual>> page = individuals
+                .peek(ind -> counter.increment())
+                .filter(ind -> matchesSearchStrings(ind, searchStrings))
+                .distinct()
+                .map(this::toIndividualRendering)
+                .sorted()
+                .map(IndividualRendering::getIndividual)
+                .collect(toPage(pageRequest.getPageNumber(),
+                                pageRequest.getPageSize()));
 
         return new IndividualsQueryResult(page.orElse(Page.emptyPage()),
                                           counter.getCounter());
     }
 
+    private IndividualRendering toIndividualRendering(OWLNamedIndividual ind) {
+        return new IndividualRendering(ind, dictionaryManager.getShortForm(ind).toLowerCase());
+    }
+
+    private boolean matchesSearchStrings(@Nonnull OWLNamedIndividual i,
+                                         @Nonnull List<SearchString> searchStrings) {
+        if (searchStrings.isEmpty()) {
+            return true;
+        }
+        String shortForm = dictionaryManager.getShortForm(i);
+        Scanner scanner = new Scanner(shortForm, shortForm);
+        for (SearchString searchString : searchStrings) {
+            int index = scanner.indexOf(searchString, 0);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private static class IndividualRendering implements Comparable<IndividualRendering> {
 
