0a630aa30ddfe56d29d96429c93e33de404496eb
Matthew Horridge <matthew.horridge@stanford.edu>
2018-11-04 13:14:24 -0800

    Tidying and optimisation

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/revision/RevisionStoreImpl.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/revision/RevisionStoreImpl.java
@@ -8,14 +8,12 @@ import edu.stanford.bmir.protege.web.server.inject.project.ChangeHistoryFile;
 import edu.stanford.bmir.protege.web.shared.project.ProjectId;
 import edu.stanford.bmir.protege.web.shared.revision.RevisionNumber;
 import edu.stanford.bmir.protege.web.shared.user.UserId;
-import org.semanticweb.binaryowl.BinaryOWLMetadata;
 import org.semanticweb.binaryowl.BinaryOWLOntologyChangeLog;
 import org.semanticweb.binaryowl.change.OntologyChangeRecordList;
 import org.semanticweb.binaryowl.chunk.SkipSetting;
 import org.semanticweb.owlapi.change.*;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,19 +23,17 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static edu.stanford.bmir.protege.web.server.revision.RevisionSerializationVocabulary.*;
 
 /**
  * Matthew Horridge
@@ -50,20 +46,19 @@ public class RevisionStoreImpl implements RevisionStore {
 
     private final ExecutorService changeSerializationExucutor = Executors.newSingleThreadExecutor();
 
-    private ImmutableList<Revision> revisions = ImmutableList.of();
-
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     private final Lock readLock = readWriteLock.readLock();
 
     private final Lock writeLock = readWriteLock.writeLock();
 
-
     private final ProjectId projectId;
 
     private final OWLDataFactory dataFactory;
 
     private final File changeHistoryFile;
+
+    private ImmutableList<Revision> revisions = ImmutableList.of();
 
 
     @Inject
@@ -90,19 +85,28 @@ public class RevisionStoreImpl implements RevisionStore {
         }
     }
 
-    @Nonnull
-    @Override
-    public RevisionNumber getCurrentRevisionNumber() {
+    private int getRevisionIndexForRevision(RevisionNumber revision) {
         try {
             readLock.lock();
-            if (revisions.isEmpty()) {
-                return RevisionNumber.getRevisionNumber(0);
+            if(revisions.isEmpty()) {
+                return -1;
             }
-            return revisions.get(revisions.size() - 1).getRevisionNumber();
+            if(revision.isHead()) {
+                return revisions.size() - 1;
+            }
+            var firstRevision = revisions.get(0);
+            if(revision.compareTo(firstRevision.getRevisionNumber()) < 0) {
+                return -1;
+            }
+            var lastRevision = revisions.get(revisions.size() - 1);
+            if(lastRevision.getRevisionNumber() == revision) {
+                return revisions.size() - 1;
+            }
+            var dummyRevision = Revision.createEmptyRevisionWithRevisionNumber(revision);
+            return Collections.binarySearch(revisions, dummyRevision);
         } finally {
             readLock.unlock();
         }
-
     }
 
     @Nonnull
@@ -122,21 +126,56 @@ public class RevisionStoreImpl implements RevisionStore {
         try {
             writeLock.lock();
             if(revision.getRevisionNumber().compareTo(getCurrentRevisionNumber()) <= 0) {
-                throw new IllegalArgumentException(String.format(
-                        "Revision number (%d) must be greater than the current revision number (%d)",
-                        revision.getRevisionNumber().getValue(),
-                        getCurrentRevisionNumber().getValue()
-                ));
+                throw new IllegalArgumentException(String.format("Revision number (%d) must be greater than the current revision number (%d)", revision
+                        .getRevisionNumber()
+                        .getValue(), getCurrentRevisionNumber().getValue()));
             }
-            ImmutableList.Builder<Revision> extendedList = ImmutableList.builder();
-            extendedList.addAll(revisions);
-            extendedList.add(revision);
-            revisions = extendedList.build();
+            var extendedListBuilder = ImmutableList.<Revision>builder();
+            extendedListBuilder.addAll(revisions);
+            extendedListBuilder.add(revision);
+            revisions = extendedListBuilder.build();
             persistChanges(revision);
         } finally {
             writeLock.unlock();
         }
 
+    }
+
+    @Nonnull
+    @Override
+    public RevisionNumber getCurrentRevisionNumber() {
+        try {
+            readLock.lock();
+            if(revisions.isEmpty()) {
+                return RevisionNumber.getRevisionNumber(0);
+            }
+            return revisions.get(revisions.size() - 1).getRevisionNumber();
+        } finally {
+            readLock.unlock();
+        }
+
+    }
+
+    private void persistChanges(Revision revision) {
+        try {
+            writeLock.lock();
+            var revisionSerializationTask = new RevisionSerializationTask(changeHistoryFile, revision);
+            if(revisions.size() != 1) {
+                changeSerializationExucutor.submit(revisionSerializationTask);
+            }
+            else {
+                // Save immediately
+                try {
+                    logger.info("{} Saving first revision of project", projectId);
+                    revisionSerializationTask.call();
+                } catch(IOException e) {
+                    logger.error("{} An error occurred whilst saving the first revision of the project.  Cause: {}.", projectId, e
+                            .getMessage(), e);
+                }
+            }
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void load() {
@@ -146,37 +185,35 @@ public class RevisionStoreImpl implements RevisionStore {
                 changeHistoryFile.getParentFile().mkdirs();
                 return;
             }
-            final ImmutableList.Builder<Revision> revisionsBuilder = ImmutableList.builder();
+            var revisionsBuilder = ImmutableList.<Revision>builder();
+            var axiomInterner = Interners.<OWLAxiom>newStrongInterner();
+            var metadataInterner = Interners.<String>newStrongInterner();
+            var userIdInterner = Interners.<UserId>newStrongInterner();
+
             try {
                 logger.info("{} Loading change history", projectId);
-                Stopwatch stopwatch = Stopwatch.createStarted();
-                BinaryOWLOntologyChangeLog changeLog = new BinaryOWLOntologyChangeLog();
-                final BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(changeHistoryFile));
-                final Interner<OWLAxiom> axiomInterner = Interners.newStrongInterner();
-                final Interner<String> metadataInterner = Interners.newStrongInterner();
-                changeLog.readChanges(inputStream, dataFactory, (list, skipSetting, l) -> {
+                var stopwatch = Stopwatch.createStarted();
+                var changeLog = new BinaryOWLOntologyChangeLog();
+                var inputStream = new BufferedInputStream(new FileInputStream(changeHistoryFile));
+                changeLog.readChanges(inputStream, dataFactory, (changeRecordList, skipSetting, l) -> {
+                    var metadata = changeRecordList.getMetadata();
+                    var userName = metadataInterner.intern(metadata.getStringAttribute(USERNAME_METADATA_ATTRIBUTE.getVocabularyName(), ""));
+                    var revisionNumberValue = metadata.getLongAttribute(REVISION_META_DATA_ATTRIBUTE.getVocabularyName(), 0L);
+                    var revisionNumber = RevisionNumber.getRevisionNumber(revisionNumberValue);
+                    var description = metadata.getStringAttribute(DESCRIPTION_META_DATA_ATTRIBUTE.getVocabularyName(), "");
+                    var userId = userIdInterner.intern(UserId.getUserId(userName));
 
-                    BinaryOWLMetadata metadata = list.getMetadata();
-                    String userName = metadataInterner.intern(metadata.getStringAttribute(RevisionSerializationVocabulary.USERNAME_METADATA_ATTRIBUTE.getVocabularyName(), ""));
-                    Long revisionNumberValue = metadata.getLongAttribute(RevisionSerializationVocabulary.REVISION_META_DATA_ATTRIBUTE.getVocabularyName(), 0L);
-                    RevisionNumber revisionNumber = RevisionNumber.getRevisionNumber(revisionNumberValue);
-
-                    String description = metadataInterner.intern(metadata.getStringAttribute(RevisionSerializationVocabulary.DESCRIPTION_META_DATA_ATTRIBUTE.getVocabularyName(), ""));
-
-//                        RevisionType type = RevisionType.valueOf(metadata.getStringAttribute(RevisionSerializationVocabulary.REVISION_TYPE_META_DATA_ATTRIBUTE.getVocabularyName(), RevisionType.EDIT.name()));
-
-                    final UserId userId = UserId.getUserId(userName);
-                    final List<OWLOntologyChangeRecord> changeRecords = internChangeRecords(list, axiomInterner);
-
-                    Revision revision = new Revision(userId, revisionNumber, ImmutableList.copyOf(changeRecords), list.getTimestamp(), description);
+                    var internedChangeRecords = internChangeRecords(changeRecordList, axiomInterner);
+                    var revision = new Revision(userId, revisionNumber, internedChangeRecords, changeRecordList.getTimestamp(), description);
                     revisionsBuilder.add(revision);
                 }, SkipSetting.SKIP_NONE);
                 inputStream.close();
                 stopwatch.stop();
                 revisions = revisionsBuilder.build();
-                logger.info("{} Change history loading complete.  Loaded {} revisions in {} ms.", projectId, revisions.size(), stopwatch.elapsed(TimeUnit.MILLISECONDS));
+                logger.info("{} Change history loading complete.  Loaded {} revisions in {} ms.", projectId, revisions.size(), stopwatch
+                        .elapsed(TimeUnit.MILLISECONDS));
 
-            } catch (Exception e) {
+            } catch(Exception e) {
                 logger.error("{} Failed to load change history for project.  Cause: {}", projectId, e.getMessage(), e);
             }
         } finally {
@@ -186,103 +223,82 @@ public class RevisionStoreImpl implements RevisionStore {
 
     }
 
-
-    private int getRevisionIndexForRevision(RevisionNumber revision) {
-        try {
-            readLock.lock();
-            if (revisions.isEmpty()) {
-                return -1;
+    @Nonnull
+    private static ImmutableList<OWLOntologyChangeRecord> internChangeRecords(OntologyChangeRecordList list,
+                                                                              final Interner<OWLAxiom> axiomInterner) {
+        var changeRecords = list.getChangeRecords();
+        var internedChangeRecordsListBuilder = ImmutableList.<OWLOntologyChangeRecord>builder();
+        var changeDataInterner = new ChangeDataInterner(axiomInterner);
+        for(var chanceRecord : changeRecords) {
+            var ontologyId = chanceRecord.getOntologyID();
+            var changeData = chanceRecord.getData();
+            var internedChangeData = changeData.accept(changeDataInterner);
+            if(internedChangeData == changeData) {
+                internedChangeRecordsListBuilder.add(chanceRecord);
             }
-            if (revision.isHead()) {
-                return revisions.size() - 1;
+            else {
+                OWLOntologyChangeRecord rec = new OWLOntologyChangeRecord(ontologyId, internedChangeData);
+                internedChangeRecordsListBuilder.add(rec);
             }
-            Revision firstRevision = revisions.get(0);
-            if (revision.compareTo(firstRevision.getRevisionNumber()) < 0) {
-                return -1;
-            }
-            Revision lastRevision = revisions.get(revisions.size() - 1);
-            if (lastRevision.getRevisionNumber() == revision) {
-                return revisions.size() - 1;
-            }
-            Revision dummy = Revision.createEmptyRevisionWithRevisionNumber(revision);
-            return Collections.binarySearch(revisions, dummy);
-        } finally {
-            readLock.unlock();
         }
+        return internedChangeRecordsListBuilder.build();
     }
 
-    private List<OWLOntologyChangeRecord> internChangeRecords(OntologyChangeRecordList list, final Interner<OWLAxiom> axiomInterner) {
-        List<OWLOntologyChangeRecord> result = new ArrayList<>();
-        List<OWLOntologyChangeRecord> records = list.getChangeRecords();
+    private static class ChangeDataInterner implements OWLOntologyChangeDataVisitor<OWLOntologyChangeData, RuntimeException> {
 
-        for (OWLOntologyChangeRecord record : records) {
-            OWLOntologyID id = record.getOntologyID();
-            OWLOntologyChangeData changeData = record.getData();
-            OWLOntologyChangeData internedData = changeData.accept(new OWLOntologyChangeDataVisitor<OWLOntologyChangeData, RuntimeException>() {
-                @Override
-                public OWLOntologyChangeData visit(AddAxiomData data) throws RuntimeException {
-                    final OWLAxiom ax = axiomInterner.intern(data.getAxiom());
-                    if (ax != null) {
-                        return new AddAxiomData(ax);
-                    } else {
-                        return data;
-                    }
-                }
+        @Nonnull
+        private final Interner<OWLAxiom> axiomInterner;
 
-                @Override
-                public OWLOntologyChangeData visit(RemoveAxiomData data) throws RuntimeException {
-                    return data;
-                }
-
-                @Override
-                public OWLOntologyChangeData visit(AddOntologyAnnotationData data) throws RuntimeException {
-                    return data;
-                }
-
-                @Override
-                public OWLOntologyChangeData visit(RemoveOntologyAnnotationData data) throws RuntimeException {
-                    return data;
-                }
-
-                @Override
-                public OWLOntologyChangeData visit(SetOntologyIDData data) throws RuntimeException {
-                    return data;
-                }
-
-                @Override
-                public OWLOntologyChangeData visit(AddImportData data) throws RuntimeException {
-                    return data;
-                }
-
-                @Override
-                public OWLOntologyChangeData visit(RemoveImportData data) throws RuntimeException {
-                    return data;
-                }
-            });
-            OWLOntologyChangeRecord rec = new OWLOntologyChangeRecord(id, internedData);
-            result.add(rec);
-
+        public ChangeDataInterner(@Nonnull Interner<OWLAxiom> axiomInterner) {
+            this.axiomInterner = checkNotNull(axiomInterner);
         }
-        return result;
-    }
 
-    private void persistChanges(Revision revision) {
-        try {
-            writeLock.lock();
-            RevisionSerializationTask revisionSerializationTask = new RevisionSerializationTask(changeHistoryFile, revision);
-            if (revisions.size() != 1) {
-                changeSerializationExucutor.submit(revisionSerializationTask);
-            } else {
-                // Save immediately
-                try {
-                    logger.info("{} Saving first revision of project", projectId);
-                    revisionSerializationTask.call();
-                } catch (IOException e) {
-                    logger.error("{} An error occurred whilst saving the first revision of the project.  Cause: {}.", projectId, e.getMessage(), e);
-                }
+        @Nonnull
+        @Override
+        public OWLOntologyChangeData visit(AddAxiomData data) throws RuntimeException {
+            final OWLAxiom ax = axiomInterner.intern(data.getAxiom());
+            if(ax != null) {
+                return new AddAxiomData(ax);
             }
-        } finally {
-            writeLock.unlock();
+            else {
+                return data;
+            }
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(RemoveAxiomData data) throws RuntimeException {
+            final OWLAxiom ax = axiomInterner.intern(data.getAxiom());
+            if(ax != null) {
+                return new AddAxiomData(ax);
+            }
+            else {
+                return data;
+            }
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(AddOntologyAnnotationData data) throws RuntimeException {
+            return data;
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(RemoveOntologyAnnotationData data) throws RuntimeException {
+            return data;
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(SetOntologyIDData data) throws RuntimeException {
+            return data;
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(AddImportData data) throws RuntimeException {
+            return data;
+        }
+
+        @Override
+        public OWLOntologyChangeData visit(RemoveImportData data) throws RuntimeException {
+            return data;
         }
     }
 }
