7848e3a43c8ffb36a5976a0079d321e5c5916149
Matthew Horridge <matthew.horridge@stanford.edu>
2018-11-21 15:50:17 -0800

    Use custom DI for OWL API manager because we want one lock per manager

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/owlapi/WebProtegeOWLManager.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/owlapi/WebProtegeOWLManager.java
@@ -3,6 +3,15 @@ package edu.stanford.bmir.protege.web.server.owlapi;
 import org.semanticweb.binaryowl.owlapi.BinaryOWLOntologyDocumentParserFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLOntologyFactoryImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLOntologyManagerImpl;
+import uk.ac.manchester.cs.owl.owlapi.concurrent.ConcurrentOWLOntologyBuilder;
+import uk.ac.manchester.cs.owl.owlapi.concurrent.NoOpReadWriteLock;
+import uk.ac.manchester.cs.owl.owlapi.concurrent.NonConcurrentOWLOntologyBuilder;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Author: Matthew Horridge<br>
@@ -13,18 +22,25 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 public class WebProtegeOWLManager {
 
     public static OWLOntologyManager createConcurrentOWLOntologyManager() {
-        OWLOntologyManager manager = OWLManager.createConcurrentOWLOntologyManager();
-        augmentManager(manager);
+        var dataFactory = new OWLDataFactoryImpl();
+        var readWriteLock = new ReentrantReadWriteLock();
+        var manager = new OWLOntologyManagerImpl(dataFactory, readWriteLock);
+        augmentManager(manager, readWriteLock);
         return manager;
     }
 
     public static OWLOntologyManager createOWLOntologyManager() {
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        augmentManager(manager);
+        var manager = OWLManager.createOWLOntologyManager();
+        augmentManager(manager, new NoOpReadWriteLock());
         return manager;
     }
 
-    private static void augmentManager(OWLOntologyManager manager) {
+    private static void augmentManager(OWLOntologyManager manager,
+                                       ReadWriteLock readWriteLock) {
+        var nonConcurrentBuilder = new NonConcurrentOWLOntologyBuilder();
+        var concurrentBuilder = new ConcurrentOWLOntologyBuilder(nonConcurrentBuilder, readWriteLock);
+        var ontologyFactory = new OWLOntologyFactoryImpl(concurrentBuilder);
+        manager.getOntologyFactories().add(ontologyFactory);
         manager.getOntologyParsers().add(new BinaryOWLOntologyDocumentParserFactory());
         manager.getOntologyStorers().add(new BinaryOWLStorerFactory());
 
