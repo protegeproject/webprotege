a541da00271fc2ebfa5e97d5c046fc5e8f3d1be0
Matthew Horridge <matthew.horridge@stanford.edu>
2018-12-10 19:18:54 -0800

    Added structured descriptions for Added/Removed Individual Type

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/change/description/AddedIndividualType.java
@@ -0,0 +1,41 @@
+package edu.stanford.bmir.protege.web.server.change.description;
+
+import com.google.auto.value.AutoValue;
+import edu.stanford.bmir.protege.web.server.owlapi.OWLObjectStringFormatter;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2018-12-10
+ */
+@AutoValue
+public abstract class AddedIndividualType implements StructuredChangeDescription {
+
+    @Nonnull
+    public static AddedIndividualType get(@Nonnull OWLNamedIndividual individual,
+                                          @Nonnull OWLClass type) {
+        return new AutoValue_AddedIndividualType(individual, type);
+    }
+
+    @Nonnull
+    public abstract OWLNamedIndividual getIndividual();
+
+    @Nonnull
+    public abstract OWLClass getType();
+
+    @Nonnull
+    @Override
+    public String getTypeName() {
+        return "AddedIndividualType";
+    }
+
+    @Nonnull
+    @Override
+    public String formatDescription(@Nonnull OWLObjectStringFormatter formatter) {
+        return formatter.formatString("Added %s as a type to %s", getType(), getIndividual());
+    }
+}
---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/change/description/RemovedIndividualType.java
@@ -0,0 +1,41 @@
+package edu.stanford.bmir.protege.web.server.change.description;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import edu.stanford.bmir.protege.web.server.owlapi.OWLObjectStringFormatter;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2018-12-10
+ */
+@AutoValue
+public abstract class RemovedIndividualType implements StructuredChangeDescription {
+
+    public static RemovedIndividualType get(@Nonnull OWLNamedIndividual individual,
+                                            @Nonnull OWLClass type) {
+        return new AutoValue_RemovedIndividualType(individual, type);
+    }
+
+    @Nonnull
+    public abstract OWLNamedIndividual getIndividual();
+
+    @Nonnull
+    public abstract OWLClass getType();
+
+    @Nonnull
+    @Override
+    public String getTypeName() {
+        return "RemovedIndividualType";
+    }
+
+    @Nonnull
+    @Override
+    public String formatDescription(@Nonnull OWLObjectStringFormatter formatter) {
+        return formatter.formatString("Removed %s as a type from %s", getType(), getIndividual());
+    }
+}
