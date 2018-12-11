f7a08ab3ae2cdeedbb48e579973945cb2cf41906
Matthew Horridge <matthew.horridge@stanford.edu>
2018-12-10 17:41:41 -0800

    Added structured description for entity deprecation

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/change/description/DeprecatedEntities.java
@@ -0,0 +1,37 @@
+package edu.stanford.bmir.protege.web.server.change.description;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import edu.stanford.bmir.protege.web.server.owlapi.OWLObjectStringFormatter;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2018-12-10
+ */
+@AutoValue
+public abstract class DeprecatedEntities implements StructuredChangeDescription {
+
+    public static DeprecatedEntities get(@Nonnull ImmutableSet<IRI> entities) {
+        return new AutoValue_DeprecatedEntities(entities);
+    }
+
+    @Nonnull
+    public abstract ImmutableSet<IRI> getEntities();
+
+    @Nonnull
+    @Override
+    public String getTypeName() {
+        return "DeprecatedEntities";
+    }
+
+    @Nonnull
+    @Override
+    public String formatDescription(@Nonnull OWLObjectStringFormatter formatter) {
+        return formatter.formatString("Deprecated %s", getEntities());
+    }
+}
