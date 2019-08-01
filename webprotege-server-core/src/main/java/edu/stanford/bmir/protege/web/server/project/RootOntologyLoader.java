03aa6a3396c35bd7397497a6300d8d8fd15bc544
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-01 16:40:34 -0700

    Tidying of root ontology loader

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/RootOntologyLoader.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/project/RootOntologyLoader.java
@@ -1,13 +1,11 @@
 package edu.stanford.bmir.protege.web.server.project;
 
 import edu.stanford.bmir.protege.web.server.owlapi.WebProtegeOWLManager;
-import org.semanticweb.binaryowl.owlapi.BinaryOWLOntologyDocumentParserFactory;
 import org.semanticweb.owlapi.functional.renderer.FunctionalSyntaxStorerFactory;
 import org.semanticweb.owlapi.manchestersyntax.renderer.ManchesterSyntaxStorerFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.owlxml.renderer.OWLXMLStorerFactory;
 import org.semanticweb.owlapi.rdf.rdfxml.renderer.RDFXMLStorerFactory;
 
@@ -32,30 +30,21 @@ public class RootOntologyLoader {
     public OWLOntology loadRootOntology() {
         // The delegate - we use the concurrent ontology manager
         OWLOntologyManager delegateManager = WebProtegeOWLManager.createConcurrentOWLOntologyManager();
-        // We only support the binary format for speed
         delegateManager.getOntologyStorers().add(new RDFXMLStorerFactory());
         delegateManager.getOntologyStorers().add(new OWLXMLStorerFactory());
         delegateManager.getOntologyStorers().add(new FunctionalSyntaxStorerFactory());
         delegateManager.getOntologyStorers().add(new ManchesterSyntaxStorerFactory());
 
-        delegateManager.getOntologyParsers().add(new BinaryOWLOntologyDocumentParserFactory());
-
-
         // The wrapper manager
         ProjectOWLOntologyManager manager = new ProjectOWLOntologyManager();
         manager.setDelegate(delegateManager);
-        int threadPriority = Thread.currentThread().getPriority();
         try {
-            Thread.currentThread().setPriority(3);
             OWLOntology rootOntology = projectOntologyManagerLoader.createProjectOntologiesInManager(manager.getDelegate());
             manager.sealDelegate();
             return rootOntology;
         }
         catch (OWLOntologyCreationException e) {
             throw new RuntimeException("Failed to load project: " + e.getMessage(), e);
-        }
-        finally {
-            Thread.currentThread().setPriority(threadPriority);
         }
     }
 
