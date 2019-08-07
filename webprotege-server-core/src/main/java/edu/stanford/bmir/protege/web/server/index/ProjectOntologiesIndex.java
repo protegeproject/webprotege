40cfef47804dff3ee8f02bcfebdf767ede401d47
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-06 17:49:32 -0700

    Added index for project ontologies

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/ProjectOntologiesIndex.java
@@ -0,0 +1,30 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-06
+ */
+public interface ProjectOntologiesIndex {
+
+    /**
+     * Gets the ontology Ids of project ontologies
+     * @return A stream of ids that represent the ids of project ontologies
+     */
+    @Nonnull
+    Stream<OWLOntologyID> getOntologyIds();
+
+    /**
+     * Gets the ontology with the specified ontology id.
+     * @param ontologyId The ontology id.
+     */
+    @Nonnull
+    Optional<OWLOntology> getOntology(@Nonnull OWLOntologyID ontologyId);
+}
---
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/index/ProjectOntologiesIndexImpl_TestCase.java
@@ -0,0 +1,69 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-06
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProjectOntologiesIndexImpl_TestCase {
+
+    @Mock
+    private OWLOntologyManager manager;
+
+    @Mock
+    private OWLOntology rootOntology;
+
+    @Mock
+    private OWLOntologyID rootOntologyId;
+
+    private ProjectOntologiesIndexImpl impl;
+
+    @Before
+    public void setUp() {
+        when(rootOntology.getOntologyID()).thenReturn(rootOntologyId);
+        when(rootOntology.getOWLOntologyManager()).thenReturn(manager);
+        when(manager.getOntologies()).thenReturn(Collections.singleton(rootOntology));
+        when(manager.getOntology(rootOntologyId)).thenReturn(rootOntology);
+        impl = new ProjectOntologiesIndexImpl(rootOntology);
+    }
+
+    @Test
+    public void shouldReturnStreamOfRootOntologyId() {
+        var ontologyIdStream = impl.getOntologyIds();
+        var ontologyIds = ontologyIdStream.collect(Collectors.toSet());
+        assertThat(ontologyIds, contains(rootOntologyId));
+    }
+
+    @Test
+    public void shouldGetRootOntology() {
+        var ontology = impl.getOntology(rootOntologyId);
+        assertThat(ontology, is(Optional.of(rootOntology)));
+    }
+
+    @Test
+    public void shouldNotReturnNonExistentOntology() {
+        var ontology = impl.getOntology(mock(OWLOntologyID.class));
+        assertThat(ontology.isEmpty(), is(true));
+    }
+}
