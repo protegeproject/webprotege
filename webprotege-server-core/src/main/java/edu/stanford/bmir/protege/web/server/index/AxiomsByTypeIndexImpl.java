59c72e68f4ced4342d0d2101c259923c41e10185
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-08 09:30:40 -0700

    Added AxiomsByTypeIndex, implementation and tests

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/AxiomsByTypeIndexImpl.java
@@ -0,0 +1,51 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import edu.stanford.bmir.protege.web.shared.inject.ProjectSingleton;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-07
+ */
+@ProjectSingleton
+public class AxiomsByTypeIndexImpl implements AxiomsByTypeIndex {
+
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    private final Lock writeLock = readWriteLock.writeLock();
+
+    @Nonnull
+    private final ProjectOntologiesIndex projectOntologiesIndex;
+
+    @Inject
+    public AxiomsByTypeIndexImpl(@Nonnull ProjectOntologiesIndex projectOntologiesIndex) {
+        this.projectOntologiesIndex = checkNotNull(projectOntologiesIndex);
+    }
+
+    @Override
+    public <T extends OWLAxiom> Stream<T> getAxiomsByType(AxiomType<T> axiomType,
+                                                          OWLOntologyID ontologyId) {
+        checkNotNull(axiomType);
+        checkNotNull(ontologyId);
+        try {
+            writeLock.lock();
+            return projectOntologiesIndex.getOntology(ontologyId)
+                    .stream()
+                    .flatMap(ont -> ont.getAxioms(axiomType).stream());
+        } finally {
+            writeLock.unlock();
+        }
+    }
+}
---
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/index/AxiomsByTypeIndexImpl_TestCase.java
@@ -0,0 +1,80 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-07
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AxiomsByTypeIndexImpl_TestCase {
+
+    private AxiomsByTypeIndexImpl impl;
+
+    @Mock
+    private ProjectOntologiesIndex projectOntologiesIndex;
+
+    @Mock
+    private OWLOntology ontology;
+
+    @Mock
+    private OWLOntologyID ontologyId;
+
+    @Mock
+    private OWLSubClassOfAxiom axiom;
+
+    @Before
+    public void setUp() {
+        impl = new AxiomsByTypeIndexImpl(projectOntologiesIndex);
+        when(projectOntologiesIndex.getOntologyIds()).thenReturn(Stream.of(ontologyId));
+        when(projectOntologiesIndex.getOntology(any())).thenReturn(Optional.empty());
+        when(projectOntologiesIndex.getOntology(ontologyId)).thenReturn(Optional.of(ontology));
+        when(ontology.getAxioms(any(AxiomType.class))).thenReturn(Collections.emptySet());
+        when(ontology.getAxioms(AxiomType.SUBCLASS_OF)).thenReturn(Collections.singleton(axiom));
+    }
+
+    @Test
+    public void shouldGetSubClassOfAxiom() {
+        var axiomsStream = impl.getAxiomsByType(AxiomType.SUBCLASS_OF, ontologyId);
+        var axioms = axiomsStream.collect(toSet());
+        assertThat(axioms, hasItem(axiom));
+    }
+
+    @Test
+    public void shouldReturnEmptyStreamIfForUnknownOntologyId() {
+        var axiomsStream = impl.getAxiomsByType(AxiomType.SUBCLASS_OF, mock(OWLOntologyID.class));
+        var axioms = axiomsStream.collect(toSet());
+        assertThat(axioms.isEmpty(), is(true));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeIfAxiomTypeIsNull() {
+        impl.getAxiomsByType(null, ontologyId);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeIfOntologyIdIsNull() {
+        impl.getAxiomsByType(AxiomType.SUBCLASS_OF, null);
+    }
+}
