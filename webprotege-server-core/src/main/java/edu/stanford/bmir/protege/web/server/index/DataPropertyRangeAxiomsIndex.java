b556ab4b6ddfda350e4afbe55a16a140504f8b39
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-10 11:26:26 -0700

    Added DataPropertyRangeAxiomsIndex, impl and tests

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/DataPropertyRangeAxiomsIndex.java
@@ -0,0 +1,20 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import java.util.stream.Stream;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-10
+ */
+public interface DataPropertyRangeAxiomsIndex {
+
+    @Nonnull
+    Stream<OWLDataPropertyRangeAxiom> getDataPropertyRangeAxioms(@Nonnull OWLDataProperty dataProperty,
+                                                                 @Nonnull OWLOntologyID ontologyID);
+}
---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/DataPropertyRangeAxiomsIndexImpl.java
@@ -0,0 +1,38 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-10
+ */
+public class DataPropertyRangeAxiomsIndexImpl implements DataPropertyRangeAxiomsIndex {
+
+    @Nonnull
+    private final OntologyIndex ontologyIndex;
+
+    @Inject
+    public DataPropertyRangeAxiomsIndexImpl(@Nonnull OntologyIndex ontologyIndex) {
+        this.ontologyIndex = checkNotNull(ontologyIndex);
+    }
+
+    @Nonnull
+    @Override
+    public Stream<OWLDataPropertyRangeAxiom> getDataPropertyRangeAxioms(@Nonnull OWLDataProperty dataProperty,
+                                                                        @Nonnull OWLOntologyID ontologyID) {
+        checkNotNull(dataProperty);
+        checkNotNull(ontologyID);
+        return ontologyIndex.getOntology(ontologyID)
+                .stream()
+                .flatMap(ont -> ont.getDataPropertyRangeAxioms(dataProperty).stream());
+    }
+}
