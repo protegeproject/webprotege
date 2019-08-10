5abf1d19556bbccfe6025210bc655f214a2b44fa
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-10 10:41:26 -0700

    Added ObjectPropertyRange axioms index, implementation and tests

---
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/index/ObjectPropertyRangeAxiomsIndexImpl_TestCase.java
@@ -0,0 +1,87 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.*;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-10
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ObjectPropertyRangeAxiomsIndexImpl_TestCase {
+
+    private ObjectPropertyRangeAxiomsIndexImpl impl;
+
+    @Mock
+    private OntologyIndex ontologyIndex;
+
+    @Mock
+    private OWLOntology ontology;
+
+    @Mock
+    private OWLOntologyID ontologyID;
+
+    @Mock
+    private OWLObjectProperty property;
+
+    @Mock
+    private OWLObjectPropertyRangeAxiom axiom;
+
+    @Before
+    public void setUp() {
+        when(ontologyIndex.getOntology(any()))
+                .thenReturn(Optional.empty());
+        when(ontologyIndex.getOntology(ontologyID))
+                .thenReturn(Optional.of(ontology));
+        when(ontology.getObjectPropertyRangeAxioms(property))
+                .thenReturn(Collections.singleton(axiom));
+        impl = new ObjectPropertyRangeAxiomsIndexImpl(ontologyIndex);
+    }
+
+    @Test
+    public void shouldGetObjectPropertyRangeAxiomForProperty() {
+        var axioms = impl.getObjectPropertyRangeAxioms(property, ontologyID).collect(toSet());
+        assertThat(axioms, hasItem(axiom));
+    }
+
+    @Test
+    public void shouldGetEmptySetForUnknownOntologyId() {
+        var axioms = impl.getObjectPropertyRangeAxioms(property, mock(OWLOntologyID.class)).collect(toSet());
+        assertThat(axioms.isEmpty(), is(true));
+    }
+
+    @Test
+    public void shouldGetEmptySetForUnknownClass() {
+        var axioms = impl.getObjectPropertyRangeAxioms(mock(OWLObjectProperty.class), ontologyID).collect(toSet());
+        assertThat(axioms.isEmpty(), is(true));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeForNullOntologyId() {
+        impl.getObjectPropertyRangeAxioms(property, null);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeForNullProperty() {
+        impl.getObjectPropertyRangeAxioms(null, ontologyID);
+    }
+    
+}
