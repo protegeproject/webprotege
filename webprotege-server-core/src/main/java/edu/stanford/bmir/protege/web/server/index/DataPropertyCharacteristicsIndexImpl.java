23321391c5ca9ea86238a4bb2e1b70e6cedb4908
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-10 11:25:49 -0700

    Added DataPropertyCharacteristicsIndex, impl and tests

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/DataPropertyCharacteristicsIndexImpl.java
@@ -0,0 +1,35 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-10
+ */
+public class DataPropertyCharacteristicsIndexImpl implements DataPropertyCharacteristicsIndex {
+
+    @Nonnull
+    private final OntologyIndex ontologyIndex;
+
+    @Inject
+    public DataPropertyCharacteristicsIndexImpl(@Nonnull OntologyIndex ontologyIndex) {
+        this.ontologyIndex = checkNotNull(ontologyIndex);
+    }
+
+    @Override
+    public boolean isFunctional(@Nonnull OWLDataProperty dataProperty,
+                                @Nonnull OWLOntologyID ontologyId) {
+        return ontologyIndex.getOntology(ontologyId)
+                .stream()
+                .flatMap(ont -> ont.getFunctionalDataPropertyAxioms(dataProperty).stream())
+                .findFirst()
+                .isPresent();
+    }
+}
