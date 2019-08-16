38cf8dfe2378c50e5cbafe913abb55d3101733ed
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-16 11:09:30 -0700

    Removed direct ontology access in MachingEngineImpl

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/MatchingEngineImpl.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/MatchingEngineImpl.java
@@ -1,12 +1,10 @@
 package edu.stanford.bmir.protege.web.server.match;
 
 import com.google.common.collect.ImmutableList;
-import edu.stanford.bmir.protege.web.server.inject.project.RootOntology;
+import edu.stanford.bmir.protege.web.server.index.ProjectSignatureIndex;
 import edu.stanford.bmir.protege.web.shared.match.criteria.Criteria;
 import edu.stanford.bmir.protege.web.shared.match.criteria.RootCriteria;
 import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.parameters.Imports;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -23,25 +21,23 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 public class MatchingEngineImpl implements MatchingEngine {
 
     @Nonnull
-    @RootOntology
-    private final OWLOntology rootOntology;
+    private final ProjectSignatureIndex projectSignatureIndex;
 
     @Nonnull
     private final MatcherFactory matcherFactory;
 
     @Inject
-    public MatchingEngineImpl(@Nonnull @RootOntology OWLOntology rootOntology,
+    public MatchingEngineImpl(@Nonnull ProjectSignatureIndex projectSignatureIndex,
                               @Nonnull MatcherFactory matcherFactory) {
-        this.rootOntology = checkNotNull(rootOntology);
+        this.projectSignatureIndex = projectSignatureIndex;
         this.matcherFactory = checkNotNull(matcherFactory);
     }
 
     @Override
     public Stream<OWLEntity> match(@Nonnull Criteria criteria) {
         Matcher<OWLEntity> matcher = getMatcher(criteria);
-        return rootOntology.getSignature(Imports.INCLUDED)
-                           .stream()
-                           .filter(matcher::matches);
+        return projectSignatureIndex.getSignature()
+                                    .filter(matcher::matches);
     }
 
     @Override
@@ -50,9 +46,8 @@ public class MatchingEngineImpl implements MatchingEngine {
                                                              .map(this::getMatcher)
                                                              .collect(toImmutableList());
         OrMatcher<OWLEntity> orMatcher = new OrMatcher<>(matchers);
-        return rootOntology.getSignature(Imports.INCLUDED)
-                           .stream()
-                           .filter(orMatcher::matches);
+        return projectSignatureIndex.getSignature()
+                                    .filter(orMatcher::matches);
     }
 
     @Override
