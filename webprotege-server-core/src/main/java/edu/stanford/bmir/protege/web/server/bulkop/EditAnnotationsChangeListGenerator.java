a00c51df112c4b8a6db170276ea3510220fa83fa
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-16 11:26:17 -0700

    Removed direct access from EditAnnotationsChangeListGenerator

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/bulkop/EditAnnotationsChangeListGenerator.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/bulkop/EditAnnotationsChangeListGenerator.java
@@ -3,10 +3,9 @@ package edu.stanford.bmir.protege.web.server.bulkop;
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import com.google.common.collect.ImmutableSet;
-import edu.stanford.bmir.protege.web.server.change.ChangeApplicationResult;
-import edu.stanford.bmir.protege.web.server.change.ChangeGenerationContext;
-import edu.stanford.bmir.protege.web.server.change.ChangeListGenerator;
-import edu.stanford.bmir.protege.web.server.change.OntologyChangeList;
+import edu.stanford.bmir.protege.web.server.change.*;
+import edu.stanford.bmir.protege.web.server.index.AnnotationAssertionAxiomsBySubjectIndex;
+import edu.stanford.bmir.protege.web.server.index.ProjectOntologiesIndex;
 import edu.stanford.bmir.protege.web.server.owlapi.RenameMap;
 import edu.stanford.bmir.protege.web.shared.bulkop.NewAnnotationData;
 import edu.stanford.bmir.protege.web.shared.bulkop.Operation;
@@ -30,13 +29,19 @@ public class EditAnnotationsChangeListGenerator implements ChangeListGenerator<B
     private final OWLDataFactory dataFactory;
 
     @Nonnull
+    private final ProjectOntologiesIndex projectOntologiesIndex;
+
+    @Nonnull
+    private final AnnotationAssertionAxiomsBySubjectIndex annotationAssertionsIndex;
+
+    @Nonnull
+    private final OntologyChangeFactory ontologyChangeFactory;
+
+    @Nonnull
     private final ImmutableSet<OWLEntity> entities;
 
     @Nonnull
     private final Operation operation;
-
-    @Nonnull
-    private final OWLOntology rootOntology;
 
     @Nonnull
     private final Optional<OWLAnnotationProperty> property;
@@ -57,18 +62,23 @@ public class EditAnnotationsChangeListGenerator implements ChangeListGenerator<B
 
     @AutoFactory
     public EditAnnotationsChangeListGenerator(@Provided @Nonnull OWLDataFactory dataFactory,
+                                              @Nonnull ProjectOntologiesIndex projectOntologiesIndex,
+                                              @Nonnull AnnotationAssertionAxiomsBySubjectIndex annotationAssertionsIndex,
+                                              @Nonnull OntologyChangeFactory ontologyChangeFactory,
                                               @Nonnull ImmutableSet<OWLEntity> entities,
                                               @Nonnull Operation operation,
-                                              @Provided @Nonnull OWLOntology rootOntology,
                                               @Nonnull Optional<OWLAnnotationProperty> matchProperty,
                                               @Nonnull Optional<String> matchLexicalValue,
-                                              boolean regEx, @Nonnull Optional<String> matchLangTag,
+                                              boolean regEx,
+                                              @Nonnull Optional<String> matchLangTag,
                                               @Nonnull NewAnnotationData newAnnotationData,
                                               @Nonnull String commitMessage) {
         this.dataFactory = checkNotNull(dataFactory);
+        this.projectOntologiesIndex = projectOntologiesIndex;
+        this.annotationAssertionsIndex = annotationAssertionsIndex;
+        this.ontologyChangeFactory = ontologyChangeFactory;
         this.entities = checkNotNull(entities);
         this.operation = checkNotNull(operation);
-        this.rootOntology = checkNotNull(rootOntology);
         this.property = checkNotNull(matchProperty);
         this.valueExpression = checkNotNull(matchLexicalValue);
         this.matchLang = checkNotNull(matchLangTag);
@@ -79,10 +89,10 @@ public class EditAnnotationsChangeListGenerator implements ChangeListGenerator<B
 
     @Override
     public OntologyChangeList<Boolean> generateChanges(ChangeGenerationContext context) {
-        OntologyChangeList.Builder<Boolean> builder = OntologyChangeList.builder();
+        var builder = OntologyChangeList.<Boolean>builder();
         String p;
         if(valueExpression.isPresent()) {
-            if (regEx) {
+            if(regEx) {
                 p = valueExpression.get();
             }
             else {
@@ -92,35 +102,44 @@ public class EditAnnotationsChangeListGenerator implements ChangeListGenerator<B
         else {
             p = ".*";
         }
-        Pattern pattern = Pattern.compile(p, Pattern.DOTALL);
-        for(OWLOntology ontology : rootOntology.getImportsClosure()) {
-            for(OWLEntity entity : entities) {
-                for(OWLAnnotationAssertionAxiom ax : ontology.getAnnotationAssertionAxioms(entity.getIRI())) {
-                    processAnnotationAssertion(ontology, entity, ax, pattern, builder);
-                }
-            }
-        }
+        var pattern = Pattern.compile(p, Pattern.DOTALL);
+        projectOntologiesIndex.getOntologyIds()
+                              .forEach(ontId -> {
+                                  entities.forEach(entity -> {
+                                      annotationAssertionsIndex.getAxiomsForSubject(entity.getIRI(), ontId)
+                                                               .forEach(ax -> processAnnotationAssertion(ontId,
+                                                                                                         entity,
+                                                                                                         ax,
+                                                                                                         pattern,
+                                                                                                         builder));
+                                  });
+                              });
         return builder.build(true);
     }
 
-    private void processAnnotationAssertion(OWLOntology ontology, OWLEntity entity, OWLAnnotationAssertionAxiom ax, Pattern pattern, OntologyChangeList.Builder<Boolean> builder) {
-        if (!ax.getValue().isLiteral()) {
+    private void processAnnotationAssertion(OWLOntologyID ontologyId,
+                                            OWLEntity entity,
+                                            OWLAnnotationAssertionAxiom ax,
+                                            Pattern pattern,
+                                            OntologyChangeList.Builder<Boolean> builder) {
+        if(!ax.getValue()
+              .isLiteral()) {
             return;
         }
-        OWLLiteral literal = (OWLLiteral) ax.getValue();
-        if (!matchesProperty(ax)) {
+        var literal = (OWLLiteral) ax.getValue();
+        if(!matchesProperty(ax)) {
             return;
         }
-        Matcher matcher = pattern.matcher(literal.getLiteral());
+        var matcher = pattern.matcher(literal.getLiteral());
         if(!matcher.find()) {
             return;
         }
         if(!matchesLangTag(literal)) {
             return;
         }
-        OWLAnnotationProperty newProperty = getNewProperty(ax);
-        String newLexicalValue = getNewLexicalValue(literal, matcher);
-        String newLangTag = getNewLangTag(literal);
+        var newProperty = getNewProperty(ax);
+        var newLexicalValue = getNewLexicalValue(literal, matcher);
+        var newLangTag = getNewLangTag(literal);
         OWLLiteral newLiteral;
         if(newLangTag.isEmpty()) {
             newLiteral = dataFactory.getOWLLiteral(newLexicalValue, literal.getDatatype());
@@ -128,40 +147,43 @@ public class EditAnnotationsChangeListGenerator implements ChangeListGenerator<B
         else {
             newLiteral = dataFactory.getOWLLiteral(newLexicalValue, newLangTag);
         }
-        OWLAxiom newAx = dataFactory.getOWLAnnotationAssertionAxiom(newProperty,
-                                                                    entity.getIRI(),
-                                                                    newLiteral,
-                                                                    ax.getAnnotations());
-        if (newAnnotationData.equals(ax)) {
-            return;
+        var newAx = dataFactory.getOWLAnnotationAssertionAxiom(newProperty,
+                                                               entity.getIRI(),
+                                                               newLiteral,
+                                                               ax.getAnnotations());
+        if(operation == Operation.REPLACE || operation == Operation.DELETE) {
+            builder.add(ontologyChangeFactory.createRemoveAxiom(ontologyId, ax));
         }
-        if (operation == Operation.REPLACE || operation == Operation.DELETE) {
-            builder.removeAxiom(ontology, ax);
-        }
-        if (operation == Operation.REPLACE || operation == Operation.AUGMENT) {
-            builder.addAxiom(ontology, newAx);
+        if(operation == Operation.REPLACE || operation == Operation.AUGMENT) {
+            builder.add(ontologyChangeFactory.createAddAxiom(ontologyId, newAx));
         }
     }
 
-    private String getNewLangTag(OWLLiteral literal) {
-        return newAnnotationData.getLanguageTag().orElse(literal.getLang());
-    }
-
-    private String getNewLexicalValue(OWLLiteral literal, Matcher matcher) {
-        return newAnnotationData.getValue().map(lv -> regEx ? matcher.replaceAll(lv) : lv).orElse(literal.getLiteral());
-    }
-
-    private OWLAnnotationProperty getNewProperty(OWLAnnotationAssertionAxiom ax) {
-        return newAnnotationData.getProperty().orElse(ax.getProperty());
+    private Boolean matchesProperty(OWLAnnotationAssertionAxiom ax) {
+        return property.map(prop -> ax.getProperty()
+                                      .equals(prop))
+                       .orElse(true);
     }
 
     private Boolean matchesLangTag(OWLLiteral val) {
-        return matchLang.map(lang -> lang.equalsIgnoreCase(val.getLang())).orElse(true);
+        return matchLang.map(lang -> lang.equalsIgnoreCase(val.getLang()))
+                        .orElse(true);
     }
 
+    private OWLAnnotationProperty getNewProperty(OWLAnnotationAssertionAxiom ax) {
+        return newAnnotationData.getProperty()
+                                .orElse(ax.getProperty());
+    }
 
-    private Boolean matchesProperty(OWLAnnotationAssertionAxiom ax) {
-        return property.map(prop -> ax.getProperty().equals(prop)).orElse(true);
+    private String getNewLexicalValue(OWLLiteral literal, Matcher matcher) {
+        return newAnnotationData.getValue()
+                                .map(lv -> regEx ? matcher.replaceAll(lv) : lv)
+                                .orElse(literal.getLiteral());
+    }
+
+    private String getNewLangTag(OWLLiteral literal) {
+        return newAnnotationData.getLanguageTag()
+                                .orElse(literal.getLang());
     }
 
     @Override
