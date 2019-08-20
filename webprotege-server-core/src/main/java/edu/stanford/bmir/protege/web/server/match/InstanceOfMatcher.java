f0077f1bdcf8f7d6b93a594c6def3a9142ee3190
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-19 18:34:41 -0700

    Removed direct ontology access from InstanceOfMatcher

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/InstanceOfMatcher.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/match/InstanceOfMatcher.java
@@ -3,12 +3,15 @@ package edu.stanford.bmir.protege.web.server.match;
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import edu.stanford.bmir.protege.web.server.hierarchy.ClassHierarchyProvider;
+import edu.stanford.bmir.protege.web.server.index.ClassAssertionAxiomsByClassIndex;
+import edu.stanford.bmir.protege.web.server.index.ProjectOntologiesIndex;
+import edu.stanford.bmir.protege.web.server.index.ProjectSignatureByTypeIndex;
 import edu.stanford.bmir.protege.web.shared.match.criteria.HierarchyFilterType;
 import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.model.parameters.Imports;
 
 import javax.annotation.Nonnull;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.stream.Collectors.toSet;
@@ -25,7 +28,13 @@ public class InstanceOfMatcher implements Matcher<OWLEntity> {
     private final ClassHierarchyProvider hierarchyProvider;
 
     @Nonnull
-    private final OWLOntology rootOntology;
+    private final ProjectOntologiesIndex projectOntologiesIndex;
+
+    @Nonnull
+    private final ClassAssertionAxiomsByClassIndex classAssertionsByClass;
+
+    @Nonnull
+    private final ProjectSignatureByTypeIndex projectSignatureByType;
 
     @Nonnull
     private final OWLClass target;
@@ -38,11 +47,15 @@ public class InstanceOfMatcher implements Matcher<OWLEntity> {
     private Set<OWLNamedIndividual> instances;
 
     public InstanceOfMatcher(@Nonnull @Provided ClassHierarchyProvider hierarchyProvider,
-                             @Nonnull @Provided OWLOntology rootOntology,
+                             @Nonnull @Provided ProjectOntologiesIndex projectOntologiesIndex,
+                             @Nonnull @Provided ClassAssertionAxiomsByClassIndex classAssertionsByClass,
+                             @Nonnull @Provided ProjectSignatureByTypeIndex projectSignatureByType,
                              @Nonnull OWLClass target,
                              @Nonnull HierarchyFilterType filterType) {
         this.hierarchyProvider = checkNotNull(hierarchyProvider);
-        this.rootOntology = rootOntology;
+        this.projectOntologiesIndex = checkNotNull(projectOntologiesIndex);
+        this.classAssertionsByClass = checkNotNull(classAssertionsByClass);
+        this.projectSignatureByType = checkNotNull(projectSignatureByType);
         this.target = checkNotNull(target);
         this.filterType = checkNotNull(filterType);
     }
@@ -50,25 +63,24 @@ public class InstanceOfMatcher implements Matcher<OWLEntity> {
     @SuppressWarnings("SuspiciousMethodCalls")
     @Override
     public boolean matches(@Nonnull OWLEntity value) {
-        if (!initialised) {
-            if (filterType == HierarchyFilterType.DIRECT) {
-                instances = rootOntology.getImportsClosure().stream()
-                                        .flatMap(ont -> ont.getClassAssertionAxioms(target).stream())
-                                        .map(OWLClassAssertionAxiom::getIndividual)
-                                        .filter(OWLIndividual::isNamed)
-                                        .map(ind -> (OWLNamedIndividual) ind)
-                                        .collect(toSet());
+        if(!initialised) {
+            if(filterType == HierarchyFilterType.DIRECT) {
+                instances = getClassAssertionAxioms(target)
+                                                  .map(OWLClassAssertionAxiom::getIndividual)
+                                                  .filter(OWLIndividual::isNamed)
+                                                  .map(ind -> (OWLNamedIndividual) ind)
+                                                  .collect(toSet());
             }
             else {
                 if(target.isOWLThing()) {
-                    instances = rootOntology.getIndividualsInSignature(Imports.INCLUDED);
+                    instances = projectSignatureByType.getSignature(EntityType.NAMED_INDIVIDUAL)
+                                                      .collect(toSet());
                 }
                 else {
                     Set<OWLClass> clses = hierarchyProvider.getDescendants(target);
                     clses.add(target);
                     instances = clses.stream()
-                                     .flatMap(cls -> rootOntology.getImportsClosure().stream()
-                                                                 .flatMap(ont -> ont.getClassAssertionAxioms(cls).stream()))
+                                     .flatMap(this::getClassAssertionAxioms)
                                      .map(OWLClassAssertionAxiom::getIndividual)
                                      .filter(OWLIndividual::isNamed)
                                      .map(ind -> (OWLNamedIndividual) ind)
@@ -78,5 +90,12 @@ public class InstanceOfMatcher implements Matcher<OWLEntity> {
             initialised = true;
         }
         return instances.contains(value);
+    }
+
+    private Stream<OWLClassAssertionAxiom> getClassAssertionAxioms(OWLClass cls) {
+        return projectOntologiesIndex.getOntologyIds()
+                                     .flatMap(ontId -> classAssertionsByClass.getClassAssertionAxioms(
+                                             cls,
+                                             ontId));
     }
 }
