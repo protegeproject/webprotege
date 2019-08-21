ea1174ebb67056cd158c9706349b7046c0137fa6
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-20 20:29:42 -0700

    Added empty injectable constructor to NullHttpLinkRenderer

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/mansyntax/render/NullHttpLinkRenderer.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/mansyntax/render/NullHttpLinkRenderer.java
@@ -2,12 +2,18 @@ package edu.stanford.bmir.protege.web.server.mansyntax.render;
 
 import org.semanticweb.owlapi.model.OWLLiteral;
 
+import javax.inject.Inject;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 27/01/15
  */
 public class NullHttpLinkRenderer implements HttpLinkRenderer {
+
+    @Inject
+    public NullHttpLinkRenderer() {
+    }
 
     @Override
     public boolean isLink(OWLLiteral literal) {
