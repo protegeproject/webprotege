7a7bde933b145ab74c627573eb0a93ee8bca4f7f
Matthew Horridge <matthew.horridge@stanford.edu>
2019-08-24 08:23:52 -0700

    Added DisjointDataPropertiesAxiomsIndex, implementation and tests

---
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/DisjointDataPropertiesAxiomsIndex.java
@@ -0,0 +1,20 @@
+package edu.stanford.bmir.protege.web.server.index;
+
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDisjointDataPropertiesAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import java.util.stream.Stream;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2019-08-23
+ */
+public interface DisjointDataPropertiesAxiomsIndex {
+
+    @Nonnull
+    Stream<OWLDisjointDataPropertiesAxiom> getDisjointDataPropertiesAxioms(@Nonnull OWLDataProperty subject,
+                                                                           @Nonnull OWLOntologyID ontologyId);
+}
