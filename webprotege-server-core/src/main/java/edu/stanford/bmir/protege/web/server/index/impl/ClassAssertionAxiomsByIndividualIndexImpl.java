6d614ce09cef4d1a1874685da1ce35a8a2234f05
Matthew Horridge <matthew.horridge@stanford.edu>
2019-09-07 14:55:08 -0700

    Reimplemented ClassAssertionAxiomsByIndividualIndexImpl

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/impl/ClassAssertionAxiomsByIndividualIndexImpl.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/impl/ClassAssertionAxiomsByIndividualIndexImpl.java
@@ -1,5 +1,11 @@
 package edu.stanford.bmir.protege.web.server.index.impl;
 
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import edu.stanford.bmir.protege.web.server.change.AxiomChange;
+import edu.stanford.bmir.protege.web.server.change.OntologyChange;
 import edu.stanford.bmir.protege.web.server.index.ClassAssertionAxiomsByIndividualIndex;
 import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLIndividual;
@@ -7,6 +13,10 @@ import org.semanticweb.owlapi.model.OWLOntologyID;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -16,13 +26,23 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * Stanford Center for Biomedical Informatics Research
  * 2019-08-10
  */
-public class ClassAssertionAxiomsByIndividualIndexImpl implements ClassAssertionAxiomsByIndividualIndex {
+public class ClassAssertionAxiomsByIndividualIndexImpl implements ClassAssertionAxiomsByIndividualIndex, RequiresOntologyChangeNotification {
 
-    private final OntologyIndex ontologyIndex;
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = readWriteLock.readLock();
+
+    private final Lock writeLock = readWriteLock.writeLock();
+
+    private final AxiomChangeHandler axiomChangeHandler = new AxiomChangeHandler();
+
+    private final Multimap<Key, OWLClassAssertionAxiom> index = MultimapBuilder.hashKeys()
+                                                                               .arrayListValues()
+                                                                               .build();
 
     @Inject
-    public ClassAssertionAxiomsByIndividualIndexImpl(@Nonnull OntologyIndex ontologyIndex) {
-        this.ontologyIndex = checkNotNull(ontologyIndex);
+    public ClassAssertionAxiomsByIndividualIndexImpl() {
+        axiomChangeHandler.setAxiomChangeConsumer(this::handleOntologyChange);
     }
 
     @Override
@@ -30,8 +50,53 @@ public class ClassAssertionAxiomsByIndividualIndexImpl implements ClassAssertion
                                                                   @Nonnull OWLOntologyID ontologyID) {
         checkNotNull(individual);
         checkNotNull(ontologyID);
-        return ontologyIndex.getOntology(ontologyID)
-                .stream()
-                .flatMap(ont -> ont.getClassAssertionAxioms(individual).stream());
+        var key = Key.get(ontologyID, individual);
+        try {
+            readLock.lock();
+            return ImmutableList.copyOf(index.get(key)).stream();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public void handleOntologyChanges(@Nonnull List<OntologyChange> changes) {
+        try {
+            writeLock.lock();
+            axiomChangeHandler.handleOntologyChanges(changes);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    private void handleOntologyChange(@Nonnull AxiomChange change) {
+        var axiom = change.getAxiom();
+        if(!(axiom instanceof OWLClassAssertionAxiom)) {
+            return;
+        }
+        var classAssertionAxiom = (OWLClassAssertionAxiom) axiom;
+        var key = Key.get(change.getOntologyId(),
+                          classAssertionAxiom.getIndividual());
+        if(change.isAddAxiom()) {
+            index.put(key, classAssertionAxiom);
+        }
+        else {
+            index.remove(key, classAssertionAxiom);
+        }
+    }
+
+
+    @AutoValue
+    public static abstract class Key {
+
+        public static Key get(@Nonnull OWLOntologyID ontologyId,
+                              @Nonnull OWLIndividual individual) {
+            return new AutoValue_ClassAssertionAxiomsByIndividualIndexImpl_Key(ontologyId,
+                                                                               individual);
+        }
+
+        public abstract OWLOntologyID getOntologyId();
+
+        public abstract OWLIndividual getIndividual();
     }
 }
