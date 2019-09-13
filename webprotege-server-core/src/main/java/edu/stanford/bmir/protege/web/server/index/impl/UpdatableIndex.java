9fcfd156cfc157dc2857d43b5bb2a10db5612913
Matthew Horridge <matthew.horridge@stanford.edu>
2019-09-13 11:32:47 -0700

    Added javadoc to UpdatableIndex

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/impl/UpdatableIndex.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/index/impl/UpdatableIndex.java
@@ -9,8 +9,14 @@ import javax.annotation.Nonnull;
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 2019-08-09
+ *
+ * An index that needs to be updated with ontology changes in order to
+ * build or maintain it.
  */
 public interface UpdatableIndex extends Index {
 
+    /**
+     * Apply the specified list of changes to update this index.
+     */
     void applyChanges(@Nonnull ImmutableList<OntologyChange> changes);
 }
