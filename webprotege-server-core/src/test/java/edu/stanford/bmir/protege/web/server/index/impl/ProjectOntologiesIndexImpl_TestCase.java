25c14ac98f105544473c92433c827db08f32f3ed
Matthew Horridge <matthew.horridge@stanford.edu>
2019-09-13 10:43:39 -0700

    Updated ProjectOntologiesIndexImpl_TestCase

--- webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/index/impl/ProjectOntologiesIndexImpl_TestCase.java
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/index/impl/ProjectOntologiesIndexImpl_TestCase.java
@@ -1,21 +1,22 @@
 package edu.stanford.bmir.protege.web.server.index.impl;
 
-import edu.stanford.bmir.protege.web.server.index.impl.ProjectOntologiesIndexImpl;
+import com.google.common.collect.ImmutableList;
+import dagger.Module;
+import edu.stanford.bmir.protege.web.server.change.AddAxiomChange;
+import edu.stanford.bmir.protege.web.server.change.RemoveAxiomChange;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyID;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLAxiom;
 
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Matthew Horridge
@@ -26,29 +27,29 @@ import static org.mockito.Mockito.when;
 public class ProjectOntologiesIndexImpl_TestCase {
 
     @Mock
-    private OWLOntologyManager manager;
-
-    @Mock
-    private OWLOntology rootOntology;
-
-    @Mock
     private OWLOntologyID rootOntologyId;
 
     private ProjectOntologiesIndexImpl impl;
 
+    @Mock
+    private OWLAxiom axiom;
+
     @Before
     public void setUp() {
-        when(rootOntology.getOntologyID()).thenReturn(rootOntologyId);
-        when(rootOntology.getOWLOntologyManager()).thenReturn(manager);
-        when(manager.getOntologies()).thenReturn(Collections.singleton(rootOntology));
-        when(manager.getOntology(rootOntologyId)).thenReturn(rootOntology);
         impl = new ProjectOntologiesIndexImpl();
+        impl.applyChanges(ImmutableList.of(AddAxiomChange.of(rootOntologyId, axiom)));
     }
 
     @Test
-    public void shouldReturnStreamOfRootOntologyId() {
+    public void shouldReturnStreamOfReferencedOntologyId() {
         var ontologyIdStream = impl.getOntologyIds();
         var ontologyIds = ontologyIdStream.collect(Collectors.toSet());
         assertThat(ontologyIds, contains(rootOntologyId));
+    }
+
+    @Test
+    public void shouldOnlyContainReferencedOntologies() {
+        impl.applyChanges(ImmutableList.of(RemoveAxiomChange.of(rootOntologyId, axiom)));
+        assertThat(impl.getOntologyIds().count(), is(0L));
     }
 }
