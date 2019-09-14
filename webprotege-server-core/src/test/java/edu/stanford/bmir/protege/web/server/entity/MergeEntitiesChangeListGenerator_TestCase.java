b76f56144225d0eb97c0fdaf0d027d1948183315
Matthew Horridge <matthew.horridge@stanford.edu>
2019-09-13 17:26:36 -0700

    Fixed merge entitied test case

--- webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/entity/MergeEntitiesChangeListGenerator_TestCase.java
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/entity/MergeEntitiesChangeListGenerator_TestCase.java
@@ -1,6 +1,8 @@
 package edu.stanford.bmir.protege.web.server.entity;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import edu.stanford.bmir.protege.web.server.change.AddAxiomChange;
 import edu.stanford.bmir.protege.web.server.change.ChangeGenerationContext;
 import edu.stanford.bmir.protege.web.server.change.OntologyChange;
 import edu.stanford.bmir.protege.web.server.change.OntologyChangeList;
@@ -21,13 +23,13 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.vocab.SKOSVocabulary;
-
-import java.util.stream.Stream;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 import static edu.stanford.bmir.protege.web.shared.entity.MergedEntityTreatment.DELETE_MERGED_ENTITY;
 import static edu.stanford.bmir.protege.web.shared.entity.MergedEntityTreatment.DEPRECATE_MERGED_ENTITY;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Class;
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.*;
@@ -46,11 +48,12 @@ public class MergeEntitiesChangeListGenerator_TestCase {
     @Mock
     private EntityDiscussionThreadRepository discussionThreadRepo;
 
-    private OWLOntology rootOntology;
-
     private OWLDataFactory dataFactory;
 
     private ImmutableSet<OWLEntity> sourceEntities;
+
+    @Mock
+    private OWLOntologyID ontologyId;
 
     private OWLClass targetEntity;
 
@@ -70,22 +73,29 @@ public class MergeEntitiesChangeListGenerator_TestCase {
 
     private OWLClass clsC;
 
-    private OWLOntologyManager manager;
-
     private OWLClass sourceEntity;
 
     private EntityRenamer entityRenamer;
 
-    private DefaultOntologyIdManager defaultOntologyIdManager;
+    private DefaultOntologyIdManagerImpl defaultOntologyIdManager;
 
-    private ProjectOntologiesIndex projectOntologiesIndex;
+    private ProjectOntologiesIndexImpl projectOntologiesIndex;
 
-    private AnnotationAssertionAxiomsBySubjectIndex annotationAssertionsIndex;
+    private AnnotationAssertionAxiomsBySubjectIndexImpl annotationAssertionsIndex;
+
+    private AxiomsByEntityReferenceIndexImpl axiomsByEntityReference;
+
+    private AxiomsByReferenceIndexImpl axiomsByReferenceIndex;
+
+    private OntologyAxiomsIndexImpl ontologyAxiomsIndex;
+
+    private AnnotationAxiomsByIriReferenceIndexImpl axiomsByIriReference;
+
+    private AxiomsByTypeIndexImpl axiomsByTypeIndex;
 
     @Before
     public void setUp() throws Exception {
-        manager = OWLManager.createOWLOntologyManager();
-        dataFactory = manager.getOWLDataFactory();
+        dataFactory = new OWLDataFactoryImpl();
         IRI iriA = IRI.create("http://ontology.org/A");
         IRI iriB = IRI.create("http://ontology.org/B");
         IRI iriC = IRI.create("http://ontology.org/C");
@@ -100,25 +110,35 @@ public class MergeEntitiesChangeListGenerator_TestCase {
         hello = Literal("Hello", "en");
         bonjour = Literal("Bonjour", "fr");
         hi = Literal("hi", "en");
-        rootOntology = Ontology(manager,
-                                SubClassOf(sourceEntity, clsC),
-                                SubClassOf(targetEntity, clsC),
-                                AnnotationAssertion(rdfsLabel, sourceEntity.getIRI(), hello),
-                                AnnotationAssertion(skosPrefLabel, sourceEntity.getIRI(), bonjour),
-                                AnnotationAssertion(rdfsComment, sourceEntity.getIRI(), hi));
-        var projectOntologiesIndex = new ProjectOntologiesIndexImpl();
+        var ontologyChanges = ImmutableList.<OntologyChange>of(
+                AddAxiomChange.of(ontologyId, SubClassOf(sourceEntity, clsC)),
+                AddAxiomChange.of(ontologyId, SubClassOf(targetEntity, clsC)),
+                AddAxiomChange.of(ontologyId, AnnotationAssertion(rdfsLabel, sourceEntity.getIRI(), hello)),
+                AddAxiomChange.of(ontologyId, AnnotationAssertion(skosPrefLabel, sourceEntity.getIRI(), bonjour)),
+                AddAxiomChange.of(ontologyId, AnnotationAssertion(rdfsComment, sourceEntity.getIRI(), hi)));
+        projectOntologiesIndex = new ProjectOntologiesIndexImpl();
         defaultOntologyIdManager = new DefaultOntologyIdManagerImpl(projectOntologiesIndex);
-        this.projectOntologiesIndex = projectOntologiesIndex;
         annotationAssertionsIndex = new AnnotationAssertionAxiomsBySubjectIndexImpl();
-        var axiomsByEntityReference = new AxiomsByEntityReferenceIndexImpl(dataFactory);
-        var axiomsByIriReference = new AnnotationAxiomsByIriReferenceIndexImpl();
-        var axiomsByReferenceIndex = new AxiomsByReferenceIndexImpl(axiomsByEntityReference,
-                                                                    axiomsByIriReference);
+        axiomsByEntityReference = new AxiomsByEntityReferenceIndexImpl(dataFactory);
+        axiomsByIriReference = new AnnotationAxiomsByIriReferenceIndexImpl();
+        axiomsByReferenceIndex = new AxiomsByReferenceIndexImpl(axiomsByEntityReference,
+                                                                axiomsByIriReference);
+        axiomsByTypeIndex = new AxiomsByTypeIndexImpl();
+        ontologyAxiomsIndex = new OntologyAxiomsIndexImpl(axiomsByTypeIndex);
 
         entityRenamer = new EntityRenamer(dataFactory,
                                           this.projectOntologiesIndex,
                                           axiomsByReferenceIndex);
 
+        applyChanges(ontologyChanges);
+    }
+
+    private void applyChanges(ImmutableList<OntologyChange> changes) {
+        projectOntologiesIndex.applyChanges(changes);
+        axiomsByEntityReference.applyChanges(changes);
+        axiomsByIriReference.applyChanges(changes);
+        axiomsByTypeIndex.applyChanges(changes);
+        annotationAssertionsIndex.applyChanges(changes);
     }
 
     private void createGeneratorAndApplyChanges(MergedEntityTreatment treatment) {
@@ -134,42 +154,42 @@ public class MergeEntitiesChangeListGenerator_TestCase {
                                                                                     projectOntologiesIndex,
                                                                                     annotationAssertionsIndex);
         OntologyChangeList<?> changeList = gen.generateChanges(new ChangeGenerationContext(UserId.getUserId("Bob")));
-        manager.applyChanges(changeList.getChanges().stream().map(OntologyChange::toOwlOntologyChangeRecord).map(chg -> chg.createOntologyChange(manager)).collect(toList()));
+        applyChanges(ImmutableList.copyOf(changeList.getChanges()));
     }
 
     @Test
     public void shouldReplaceSourceWithTarget_WithDeleteTreatment() {
         createGeneratorAndApplyChanges(DELETE_MERGED_ENTITY);
-        assertThat(rootOntology.containsEntityInSignature(sourceEntity), is(false));
-        assertThat(rootOntology.containsAxiom(SubClassOf(sourceEntity, clsC)), is(false));
-        assertThat(rootOntology.containsAxiom(SubClassOf(targetEntity, clsC)), is(true));
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(rdfsComment, sourceEntity.getIRI(), hi)), is(false));
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(rdfsComment, targetEntity.getIRI(), hi)), is(true));
+        assertThat(axiomsByEntityReference.containsEntityInOntologyAxiomsSignature(sourceEntity, ontologyId), is(false));
+        assertThat(ontologyAxiomsIndex.containsAxiom(SubClassOf(sourceEntity, clsC), ontologyId), is(false));
+        assertThat(ontologyAxiomsIndex.containsAxiom(SubClassOf(targetEntity, clsC), ontologyId), is(true));
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(rdfsComment, sourceEntity.getIRI(), hi), ontologyId), is(false));
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(rdfsComment, targetEntity.getIRI(), hi), ontologyId), is(true));
     }
 
     @Test
     public void shouldReplaceSourceRdfsLabelWithTargetAltLabel_WithDeleteTreatment() {
         createGeneratorAndApplyChanges(DELETE_MERGED_ENTITY);
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(rdfsLabel, sourceEntity.getIRI(), hello)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(rdfsLabel, sourceEntity.getIRI(), hello), ontologyId),
                    is(false));
 
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(rdfsLabel, targetEntity.getIRI(), hello)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(rdfsLabel, targetEntity.getIRI(), hello), ontologyId),
                    is(false));
 
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(skosAltLabel, targetEntity.getIRI(), hello)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(skosAltLabel, targetEntity.getIRI(), hello), ontologyId),
                    is(true));
     }
 
     @Test
     public void shouldReplaceSourceSkosPrefLabelWithTargetAltLabel_WithDeleteTreatment() {
         createGeneratorAndApplyChanges(DELETE_MERGED_ENTITY);
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(skosPrefLabel, sourceEntity.getIRI(), bonjour)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(skosPrefLabel, sourceEntity.getIRI(), bonjour), ontologyId),
                    is(false));
 
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(skosPrefLabel, targetEntity.getIRI(), bonjour)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(skosPrefLabel, targetEntity.getIRI(), bonjour), ontologyId),
                    is(false));
 
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(skosAltLabel, targetEntity.getIRI(), bonjour)),
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(skosAltLabel, targetEntity.getIRI(), bonjour), ontologyId),
                    is(true));
     }
 
@@ -177,13 +197,13 @@ public class MergeEntitiesChangeListGenerator_TestCase {
     public void shouldNotAddDeprecationWith_WithDeleteTreatment() {
         createGeneratorAndApplyChanges(DELETE_MERGED_ENTITY);
         OWLAnnotationProperty deprecated = dataFactory.getOWLDeprecated();
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(deprecated, sourceEntity.getIRI(), Literal(true))), is(false));
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(deprecated, sourceEntity.getIRI(), Literal(true)), ontologyId), is(false));
     }
 
     @Test
     public void shouldAddDeprecationWith_WithDeprecateTreatment() {
         createGeneratorAndApplyChanges(DEPRECATE_MERGED_ENTITY);
         OWLAnnotationProperty deprecated = dataFactory.getOWLDeprecated();
-        assertThat(rootOntology.containsAxiom(AnnotationAssertion(deprecated, sourceEntity.getIRI(), Literal(true))), is(true));
+        assertThat(ontologyAxiomsIndex.containsAxiom(AnnotationAssertion(deprecated, sourceEntity.getIRI(), Literal(true)), ontologyId), is(true));
     }
 }
