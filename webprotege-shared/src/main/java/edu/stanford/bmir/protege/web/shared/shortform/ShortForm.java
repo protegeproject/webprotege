ef338452575ec3401a1b106a5059bf3c57f17402
Matthew Horridge <matthew.horridge@stanford.edu>
2020-05-13 14:11:00 -0700

    Added a ShortForm class

---
+++ webprotege-shared/src/main/java/edu/stanford/bmir/protege/web/shared/shortform/ShortForm.java
@@ -0,0 +1,40 @@
+package edu.stanford.bmir.protege.web.shared.shortform;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.google.auto.value.AutoValue;
+import com.google.common.annotations.GwtCompatible;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@AutoValue
+@GwtCompatible(serializable = true)
+public abstract class ShortForm {
+
+    public static final String DICTIONARY_LANGUAGE = "dictionaryLanguage";
+
+    public static final String SHORT_FORM = "shortForm";
+
+    @Nonnull
+    protected static ShortForm get(@Nonnull @JsonProperty(DICTIONARY_LANGUAGE) DictionaryLanguage language,
+                                @Nonnull @JsonProperty(SHORT_FORM) String shortForm) {
+        return new AutoValue_ShortForm(language, shortForm);
+    }
+
+    @JsonCreator
+    @Nonnull
+    protected static ShortForm getFromJson(@Nullable @JsonProperty(DICTIONARY_LANGUAGE) DictionaryLanguage language,
+                                @Nonnull @JsonProperty(SHORT_FORM) String shortForm) {
+        return new AutoValue_ShortForm(language == null ? DictionaryLanguage.localName() : language, shortForm);
+    }
+
+    @JsonProperty(DICTIONARY_LANGUAGE)
+    @Nonnull
+    public abstract DictionaryLanguage getDictionaryLanguage();
+
+    @Nonnull
+    @JsonProperty(SHORT_FORM)
+    public abstract String getShortForm();
+}
