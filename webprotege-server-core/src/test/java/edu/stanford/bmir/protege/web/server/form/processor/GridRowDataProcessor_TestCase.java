48a974f4498284661606f0f737215d6c513095e0
Matthew Horridge <matthew.horridge@stanford.edu>
2020-05-21 14:02:41 -0700

    Added a couple of more tests related to row subject

--- webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/form/processor/GridRowDataProcessor_TestCase.java
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/server/form/processor/GridRowDataProcessor_TestCase.java
@@ -5,6 +5,7 @@ import edu.stanford.bmir.protege.web.server.form.FormFrameBuilder;
 import edu.stanford.bmir.protege.web.shared.form.FormFieldBindingMissingException;
 import edu.stanford.bmir.protege.web.shared.form.FormSubjectFactoryDescriptor;
 import edu.stanford.bmir.protege.web.shared.form.GridColumnBindingMissingException;
+import edu.stanford.bmir.protege.web.shared.form.data.FormSubject;
 import edu.stanford.bmir.protege.web.shared.form.data.GridCellData;
 import edu.stanford.bmir.protege.web.shared.form.data.GridRowData;
 import edu.stanford.bmir.protege.web.shared.form.field.GridColumnDescriptor;
@@ -13,12 +14,15 @@ import edu.stanford.bmir.protege.web.shared.form.field.OwlBinding;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
@@ -79,6 +83,31 @@ public class GridRowDataProcessor_TestCase {
                                      gridRowData);
         verify(formFrameBuilder, times(1))
                 .add(binding, rowformFrameBuilder);
+    }
+
+    @Test
+    public void shouldNotSetSubjectIfRowDoesNotHaveSubject() {
+        processor.processGridRowData(binding,
+                                     rowSubjectFactoryDescriptor,
+                                     ImmutableList.of(columnDescriptor),
+                                     formFrameBuilder,
+                                     gridRowData);
+        verify(formFrameBuilder, never()).setSubject(any());
+    }
+
+    @Test
+    public void shouldSetSubjectIfRowHasSubject() {
+        var formSubject = mock(FormSubject.class);
+        when(gridRowData.getSubject())
+                .thenReturn(Optional.of(formSubject));
+        processor.processGridRowData(binding,
+                                     rowSubjectFactoryDescriptor,
+                                     ImmutableList.of(columnDescriptor),
+                                     formFrameBuilder,
+                                     gridRowData);
+        var rowFrameCapture = ArgumentCaptor.forClass(FormFrameBuilder.class);
+        verify(formFrameBuilder, times(1)).add(eq(binding), rowFrameCapture.capture());
+        verify(rowFrameCapture.getValue(), times(1)).setSubject(formSubject);
     }
 
     @Test
