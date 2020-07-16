ed1ee0bfdad6fe6b63e238c63086ae65825840d1
Matthew Horridge <matthew.horridge@stanford.edu>
2020-07-16 16:15:08 -0700

    Check short form match bounds

--- webprotege-server-api/src/main/java/edu/stanford/bmir/protege/web/server/shortform/ShortFormMatchPosition.java
+++ webprotege-server-api/src/main/java/edu/stanford/bmir/protege/web/server/shortform/ShortFormMatchPosition.java
@@ -4,6 +4,8 @@ import com.google.auto.value.AutoValue;
 
 import javax.annotation.Nonnull;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
@@ -14,6 +16,7 @@ public abstract class ShortFormMatchPosition {
 
     @Nonnull
     public static ShortFormMatchPosition get(int start, int end) {
+        checkArgument(start <= end);
         return new AutoValue_ShortFormMatchPosition(start, end);
     }
 
