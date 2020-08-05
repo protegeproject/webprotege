d676a48f3b2a66d305458554d2e629ebbadbef03
Matthew Horridge <matthew.horridge@stanford.edu>
2020-08-05 11:32:02 -0700

    Added serialization tests for legacy serialization

--- webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/shared/shortform/DictionaryLanguage_Serialization_TestCase.java
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/shared/shortform/DictionaryLanguage_Serialization_TestCase.java
@@ -2,6 +2,7 @@ package edu.stanford.bmir.protege.web.shared.shortform;
 
 import edu.stanford.bmir.protege.web.shared.match.JsonSerializationTestUtil;
 import org.junit.Test;
+import org.semanticweb.owlapi.model.IRI;
 
 import java.io.IOException;
 
@@ -17,5 +18,27 @@ public class DictionaryLanguage_Serialization_TestCase {
     public void shouldSerializeAnnotationBasedDictionaryLanguage() throws IOException {
         var dictionaryLanguage = DictionaryLanguage.rdfsLabel("en");
         JsonSerializationTestUtil.testSerialization(dictionaryLanguage, DictionaryLanguage.class);
+    }
+
+    @Test
+    public void shouldDeserializeLocalNameLegacySerialization() throws IOException {
+        var localName = DictionaryLanguage.localName();
+        JsonSerializationTestUtil.testDeserialization("{}", localName, DictionaryLanguage.class);
+    }
+
+    @Test
+    public void shouldDeserializeAnnotationAssertionWithEmptyLanguageTagLegacySerialization() throws IOException {
+        var iriString = "http://example.org/prop";
+        var dictionaryLanguage = DictionaryLanguage.create(IRI.create(iriString), "");
+        var serialization = String.format("{\"propertyIri\":\"%s\"}", iriString);
+        JsonSerializationTestUtil.testDeserialization(serialization, dictionaryLanguage, DictionaryLanguage.class);
+    }
+
+    @Test
+    public void shouldDeserializeAnnotationAssertionWithNonEmptyLanguageTagLegacySerialization() throws IOException {
+        var iriString = "http://example.org/prop";
+        var dictionaryLanguage = DictionaryLanguage.create(IRI.create(iriString), "en");
+        var serialization = String.format("{\"propertyIri\":\"%s\", \"lang\":\"en\"}", iriString);
+        JsonSerializationTestUtil.testDeserialization(serialization, dictionaryLanguage, DictionaryLanguage.class);
     }
 }
