3fac60d08be5f2224e3cad1063b3b419a90a29fa
Matthew Horridge <matthew.horridge@stanford.edu>
2020-09-02 12:38:27 +0100

    Fix input box

--- webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/library/msgbox/InputBoxViewImpl.java
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/library/msgbox/InputBoxViewImpl.java
@@ -73,9 +73,6 @@ public class InputBoxViewImpl extends Composite implements InputBoxView, HasInit
 
     @Override
     public void setMultiline(boolean multiline) {
-        if (multiline == this.multiline) {
-            return;
-        }
         if(multiline) {
             switchToMultiline();
         }
@@ -86,8 +83,8 @@ public class InputBoxViewImpl extends Composite implements InputBoxView, HasInit
 
     private void switchToSingleLine() {
         multiline = false;
-        textArea.setVisible(true);
-        textBox.setVisible(false);
+        textBox.setVisible(true);
+        textArea.setVisible(false);
         textBox.setValue(textArea.getValue());
     }
 
