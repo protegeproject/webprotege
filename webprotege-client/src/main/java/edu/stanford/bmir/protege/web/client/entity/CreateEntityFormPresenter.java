6984d1864e4ea25a7ac522f399511c6bcde4b2b1
Matthew Horridge <matthew.horridge@stanford.edu>
2020-10-02 12:32:54 -0700

    Tidying

--- webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/entity/CreateEntityFormPresenter.java
+++ webprotege-client/src/main/java/edu/stanford/bmir/protege/web/client/entity/CreateEntityFormPresenter.java
@@ -4,12 +4,12 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.user.client.Window;
 import edu.stanford.bmir.protege.web.client.Messages;
 import edu.stanford.bmir.protege.web.client.dispatch.DispatchServiceManager;
 import edu.stanford.bmir.protege.web.client.form.FormPresenter;
 import edu.stanford.bmir.protege.web.client.form.LanguageMapCurrentLocaleMapper;
 import edu.stanford.bmir.protege.web.client.library.dlg.DialogButton;
+import edu.stanford.bmir.protege.web.client.library.modal.ModalCloser;
 import edu.stanford.bmir.protege.web.client.library.modal.ModalManager;
 import edu.stanford.bmir.protege.web.client.library.modal.ModalPresenter;
 import edu.stanford.bmir.protege.web.client.uuid.UuidV4Provider;
@@ -17,7 +17,7 @@ import edu.stanford.bmir.protege.web.shared.DataFactory;
 import edu.stanford.bmir.protege.web.shared.dispatch.actions.CreateEntityFromFormDataAction;
 import edu.stanford.bmir.protege.web.shared.entity.FreshEntityIri;
 import edu.stanford.bmir.protege.web.shared.form.FormDescriptorDto;
-import edu.stanford.bmir.protege.web.shared.form.data.FormData;
+import edu.stanford.bmir.protege.web.shared.form.ValidationStatus;
 import edu.stanford.bmir.protege.web.shared.form.data.FormDataDto;
 import edu.stanford.bmir.protege.web.shared.form.data.FormFieldDataDto;
 import edu.stanford.bmir.protege.web.shared.form.data.FormSubjectDto;
@@ -89,33 +89,23 @@ public class CreateEntityFormPresenter {
                                @Nonnull Optional<? extends OWLEntity> parentEntity,
                                @Nonnull CreateEntityPresenter.EntitiesCreatedHandler entitiesCreatedHandler,
                                @Nonnull ImmutableList<FormDescriptorDto> formDescriptorDtos) {
-        if(formDescriptorDtos.isEmpty()) {
+        if (formDescriptorDtos.isEmpty()) {
             throw new RuntimeException("No form descriptors have been specified");
         }
 
         ImmutableSet<IRI> parentEntityIris = parentEntity.map(owlEntity -> ImmutableSet.of(owlEntity.getIRI()))
-                                       .orElseGet(ImmutableSet::of);
+                                                         .orElseGet(ImmutableSet::of);
         FreshEntityIri freshEntityIri = FreshEntityIri.get("", "", uuidV4Provider.get(), parentEntityIris);
         OWLEntity entity = DataFactory.getOWLEntity(entityType, freshEntityIri.getIri());
-        FormSubjectDto subject = FormSubjectDto.getFormSubject(
-            DataFactory.getOWLEntityData(entity, ImmutableMap.of())
-        );
+        FormSubjectDto subject = FormSubjectDto.getFormSubject(DataFactory.getOWLEntityData(entity, ImmutableMap.of()));
         formPresenter.start(view.getFormsContainer());
-        ImmutableList<FormDataDto> formData = formDescriptorDtos.stream()
-                                                               .map(descriptor -> {
-                                                                   ImmutableList<FormFieldDataDto> fields = descriptor.getFields()
-                                                                                                                       .stream()
-                                                                                                                       .map(fd -> FormFieldDataDto
-                                                                                                                               .get(fd,
-                                                                                                                                    Page.emptyPage()))
-                                                                                                                       .collect(
-                                                                                                                               toImmutableList());
-                                                                   return FormDataDto.get(subject,
-                                                                                   descriptor,
-                                                                           fields,
-                                                                                   0);
-                                                               })
+        ImmutableList<FormDataDto> formData = formDescriptorDtos.stream().map(descriptor -> {
+            ImmutableList<FormFieldDataDto> fields = descriptor.getFields()
+                                                               .stream()
+                                                               .map(fd -> FormFieldDataDto.get(fd, Page.emptyPage()))
                                                                .collect(toImmutableList());
+            return FormDataDto.get(subject, descriptor, fields, 0);
+        }).collect(toImmutableList());
         formPresenter.displayForm(formData.get(0));
 
         FormDescriptorDto formDescriptor = formDescriptorDtos.get(0);
@@ -128,20 +118,30 @@ public class CreateEntityFormPresenter {
         modalPresenter.setPrimaryButton(DialogButton.CREATE);
         modalPresenter.setPrimaryButtonEnabled(false);
         modalPresenter.setButtonHandler(DialogButton.CREATE, closer -> {
-            FormData fd = formPresenter.getFormData()
-                                       .orElseGet(() -> FormData.empty(entity, formDescriptor.getFormId()));
-            dispatch.execute(new CreateEntityFromFormDataAction(projectId, entityType, freshEntityIri, fd),
-                             result -> {
-                                entitiesCreatedHandler.handleEntitiesCreated(result.getEntities());
-                                closer.closeModal();
-                             });
+            handleCreateEntityFormCloseRequest(entityType, freshEntityIri, entitiesCreatedHandler, closer);
+
         });
-        formPresenter.setFormDataChangedHandler(() -> {
-            updateModalPrimaryButtonState(modalPresenter);
-        });
+        formPresenter.setFormDataChangedHandler(() -> updateModalPrimaryButtonState(modalPresenter));
         modalManager.showModal(modalPresenter);
         Scheduler.get().scheduleDeferred(formPresenter::requestFocus);
         Scheduler.get().scheduleDeferred(() -> updateModalPrimaryButtonState(modalPresenter));
+    }
+
+    private void handleCreateEntityFormCloseRequest(@Nonnull EntityType<?> entityType,
+                                                    @Nonnull FreshEntityIri freshEntityIri,
+                                                    @Nonnull CreateEntityPresenter.EntitiesCreatedHandler entitiesCreatedHandler,
+                                                    @Nonnull ModalCloser closer) {
+        if (!formPresenter.getValidationStatus().equals(ValidationStatus.VALID)) {
+            return;
+        }
+        formPresenter.getFormData().ifPresent(formData -> {
+            dispatch.execute(new CreateEntityFromFormDataAction(projectId, entityType, freshEntityIri, formData),
+                             result -> {
+                                 entitiesCreatedHandler.handleEntitiesCreated(result.getEntities());
+                                 closer.closeModal();
+                             });
+        });
+
     }
 
     private void updateModalPrimaryButtonState(ModalPresenter modalPresenter) {
