0e6992bb4a58da26913094658c77ee528515cecc
Josef Hardi <josef.hardi@gmail.com>
2020-10-19 11:30:06 -0700

    feat: added Neo4j implementation for the ActiveLanguageManager

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/inject/Neo4jShortFormModule.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/inject/Neo4jShortFormModule.java
@@ -7,17 +7,22 @@ import edu.stanford.bmir.protege.web.server.lang.ActiveLanguagesManagerImpl;
 import edu.stanford.bmir.protege.web.server.shortform.BuiltInShortFormDictionary;
 import edu.stanford.bmir.protege.web.server.shortform.Neo4jLuceneModule;
 import edu.stanford.bmir.protege.web.server.shortform.ShortFormCache;
+import edu.stanford.owl2lpg.client.bind.lang.Neo4jActiveLanguagesManager;
+import edu.stanford.owl2lpg.client.read.lang.DictionaryLanguageAccessor;
+import edu.stanford.owl2lpg.client.read.lang.DictionaryLanguageAccessorModule;
 import org.semanticweb.owlapi.model.OWLEntityProvider;
 
 /**
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
-@Module(includes = Neo4jLuceneModule.class)
+@Module(includes = {
+    Neo4jLuceneModule.class,
+    DictionaryLanguageAccessorModule.class})
 public class Neo4jShortFormModule {
 
   @Provides
-  ActiveLanguagesManager provideActiveLanguagesManager(ActiveLanguagesManagerImpl impl) {
+  ActiveLanguagesManager provideActiveLanguagesManager(Neo4jActiveLanguagesManager impl) {
     return impl;
   }
 
